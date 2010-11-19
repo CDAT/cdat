2cc406708919203a7314802f42351e6250f0316d
David Partyka <dave.partyka@kitware.com>
2010-11-19 10:04:16 -0500

    Add a patch command to zlib.

--- CMake/External_zlib.cmake
+++ CMake/External_zlib.cmake
@@ -5,8 +5,9 @@ ExternalProject_Add(${proj}
   URL ${ZLIB_URL}/${ZLIB_GZ}
   URL_MD5 ${ZLIB_MD5}
   UPDATE_COMMAND ""
+  PATCH_COMMAND ${CMAKE_COMMAND} -E remove <SOURCE_DIR>/zconf.h
   SOURCE_DIR ${proj}
-  BINARY_DIR ${proj}-install
+  BINARY_DIR ${proj}-build
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
     ${ep_common_args}
