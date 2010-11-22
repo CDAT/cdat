e198b305964b9eaded957784831abffe9c79f45e
David Partyka <dave.partyka@kitware.com>
2010-11-22 17:12:46 -0500

    WIP more TclTk progress.

--- CMake/External_TclTk.cmake
+++ CMake/External_TclTk.cmake
@@ -1,57 +1,57 @@
 
 # tcl
 #
-set(proj "tcl-${version}")
+set(proj tcl-${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC})
 
 ExternalProject_Add(${proj}
   URL ${TCLTK_URL}/${TCL_GZ}
   URL_MD5 ${TCL_MD5}
-  INSTALL_DIR ${prefix}
+  INSTALL_DIR tcltk-install
   CONFIGURE_COMMAND <SOURCE_DIR>/unix/configure --prefix=<INSTALL_DIR>
   BUILD_COMMAND ${build_cmd_smp}
 )
 
 # tk
 #
-set(proj "tk-${version}")
+set(proj tk-${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC})
 
 ExternalProject_Add(${proj}
   URL ${TCLTK_URL}/${TK_GZ}
   URL_MD5 ${TK_MD5}
-  INSTALL_DIR ${prefix}
-  CONFIGURE_COMMAND <SOURCE_DIR>/unix/configure --prefix=<INSTALL_DIR>
+  INSTALL_DIR tcltk-install
+  CONFIGURE_COMMAND <SOURCE_DIR>/unix/configure --prefix=<INSTALL_DIR>  --with-tcl=<INSTALL_DIR>/lib
   BUILD_COMMAND ${build_cmd_smp}
-  DEPENDS tcl-${version}
+  DEPENDS tcl-${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC}
 )
 
 ExternalProject_Add_Step(${proj} symlink
-  COMMAND ${CMAKE_COMMAND} -E create_symlink "wish${version_short}" wish
-  WORKING_DIRECTORY ${prefix}/bin
-  COMMENT "Linking wish${version_short} to wish"
+  COMMAND ${CMAKE_COMMAND} -E create_symlink "wish${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC}" wish
+  WORKING_DIRECTORY tcltk-install/bin
+  COMMENT "Linking wish${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC} to wish"
   DEPENDEES install
 )
 
 # tcltk
 #
-set(proj "${name}-${version}")
+set(proj ${name}-${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC})
 
 ExternalProject_Add(${proj}
   DOWNLOAD_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
-  DEPENDS tk-${version}
+  DEPENDS tk-${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC}
 )
 
 # other projects need to know where tcltk gets installed:
 #
-set(CSE_TCLTK_HOME ${prefix} PARENT_SCOPE)
-set(CSE_TCLTK_PACKAGE "${name}-${version}" PARENT_SCOPE)
-set(CSE_TCLTK_NAME "${name}" PARENT_SCOPE)
-set(CSE_TCLTK_VERSIONXX "${version_major}.${version_minor}" PARENT_SCOPE)
+#set(CSE_TCLTK_HOME ${prefix} PARENT_SCOPE)
+#set(CSE_TCLTK_PACKAGE "${name}-${version}" PARENT_SCOPE)
+#set(CSE_TCLTK_NAME "${name}" PARENT_SCOPE)
+#set(CSE_TCLTK_VERSIONXX "${version_major}.${version_minor}" PARENT_SCOPE)
 
-set(extras
-  "setenv TCLTK_INCLUDE_DIR  $prefix/include"
-  "setenv TCLTK_LIB_DIR $prefix/lib"
-  "setenv TCL_LIBRARY $prefix/lib/tcl8.4"
-)
+#set(extras
+#  "setenv TCLTK_INCLUDE_DIR  $prefix/include"
+#  "setenv TCLTK_LIB_DIR $prefix/lib"
+#  "setenv TCL_LIBRARY $prefix/lib/tcl8.4"
+#)
