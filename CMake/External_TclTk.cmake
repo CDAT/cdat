03712f62735d0ae344b76bc7048b1b257e65a8c5
David Partyka <dave.partyka@kitware.com>
2010-11-22 13:49:47 -0500

    WIP building TclTk.

--- CMake/External_TclTk.cmake
+++ CMake/External_TclTk.cmake
@@ -1,35 +1,23 @@
-set(name "tcltk")
-set(version "8.4.19")
-string(REGEX MATCH "^[0-9]+[.][0-9]+" version_short ${version})
-set(latest 1)
-set(release 1)
-set(prefix ${CSE_RELEASE_INSTALL_DIR}/${name}-${version})
-
 
 # tcl
 #
 set(proj "tcl-${version}")
-set(tgz "${CMAKE_CURRENT_SOURCE_DIR}/tcl${version}-src.tar.gz")
-include("${CMAKE_CURRENT_SOURCE_DIR}/../../../CMakeModules/cse_macros.cmake")
-split_version(${version} version_major version_minor version_patch)
 
 ExternalProject_Add(${proj}
-  URL ${tgz}
+  URL ${TCLTK_URL}/${TCL_GZ}
+  URL_MD5 ${TCL_MD5}
   INSTALL_DIR ${prefix}
   CONFIGURE_COMMAND <SOURCE_DIR>/unix/configure --prefix=<INSTALL_DIR>
   BUILD_COMMAND ${build_cmd_smp}
 )
 
-set(CSE_TCL_NAME "tcl" PARENT_SCOPE)
-set(CSE_TCL_VERSIONXX "${version_major}.${version_minor}" PARENT_SCOPE)
-
 # tk
 #
 set(proj "tk-${version}")
-set(tgz "${CMAKE_CURRENT_SOURCE_DIR}/tk${version}-src.tar.gz")
 
 ExternalProject_Add(${proj}
-  URL ${tgz}
+  URL ${TCLTK_URL}/${TK_GZ}
+  URL_MD5 ${TK_MD5}
   INSTALL_DIR ${prefix}
   CONFIGURE_COMMAND <SOURCE_DIR>/unix/configure --prefix=<INSTALL_DIR>
   BUILD_COMMAND ${build_cmd_smp}
@@ -42,9 +30,6 @@ ExternalProject_Add_Step(${proj} symlink
   COMMENT "Linking wish${version_short} to wish"
   DEPENDEES install
 )
-
-set(CSE_TK_NAME "tk" PARENT_SCOPE)
-set(CSE_TK_VERSIONXX "${version_major}.${version_minor}" PARENT_SCOPE)
 
 # tcltk
 #
@@ -70,14 +55,3 @@ set(extras
   "setenv TCLTK_LIB_DIR $prefix/lib"
   "setenv TCL_LIBRARY $prefix/lib/tcl8.4"
 )
-
-cse_module_install(
-  "${name}" "${version}"
-  "${CSE_MODRELTOOLS_DIR}" "${extras}"
-  ${release} ${latest}
-)
-
-# tests
-#
-add_test(SmokeTest-${name}-${version}-tclsh8.4
-  "${prefix}/bin/tclsh8.4" "${CSE_SOURCE_DIR}/Tests/exit.tcl")
