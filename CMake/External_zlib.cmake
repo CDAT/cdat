109514900308e0f801edf34b1d7d8fa4260fa9a2
David Partyka <dave.partyka@kitware.com>
2010-11-21 21:22:21 -0500

    Get png to properly link to our built zlib.

--- CMake/External_zlib.cmake
+++ CMake/External_zlib.cmake
@@ -8,16 +8,20 @@ ExternalProject_Add(${proj}
   PATCH_COMMAND ${CMAKE_COMMAND} -E remove <SOURCE_DIR>/zconf.h
   SOURCE_DIR ${proj}
   BINARY_DIR ${proj}-build
+  INSTALL_DIR ${proj}-install
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
     ${ep_common_args}
     -DBUILD_SHARED_LIBS:BOOL=ON
     -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
     -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
-  INSTALL_COMMAND ""
+    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+  INSTALL_COMMAND make install
   DEPENDS
   )
 #set(ITK_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
 
-set(ZLIB_INCLUDE_DIR)
+set(ZLIB_INCLUDE_DIR ${proj}-install/include)
+set(ZLIB_LIBRARY_DIR ${proj}-install/lib)
+set(ZLIB_LIBRARY ${CMAKE_BINARY_DIR}/${proj}-install/lib/libz.so)
 
