c892a4adadff0a5512d4a1b529265cb13e4a23fa
David Partyka <dave.partyka@kitware.com>
2010-11-30 13:21:34 -0500

    Finish PyQt configure.

--- CMake/External_PyQt.cmake
+++ CMake/External_PyQt.cmake
@@ -7,29 +7,7 @@ ExternalProject_Add(${proj}
   URL ${PYQT_URL}/${PYQT_GZ_${CMAKE_PLATFORM}}
   URL_MD5 ${PYQT_MD5_${CMAKE_PLATFORM}}
   BUILD_IN_SOURCE 1
-  #CONFIGURE_COMMAND ${LIBRARY_PATH}=${PYTHON_LIBRARY_DIR}
-  #   ${PYTHON_EXECUTABLE} configure.py
-  CONFIGURE_COMMAND ${CMAKE_COMMAND}
-    -Dprefix=${prefix}
-    -DCSE_PYTHON_NAME=${CSE_PYTHON_NAME}
-    -DCSE_PYTHON_VERSIONXX=${CSE_PYTHON_VERSIONXX}
-    -Dpython=${CSE_PYTHON_HOME}
-    -Dsip=${CSE_SIP_HOME}
-    -Dqt=${CSE_QT_HOME}
-    -DCC=${CMAKE_C_COMPILER}
-    -DCXX=${CMAKE_CXX_COMPILER}
-    -P ${cfg}
+  CONFIGURE_COMMAND ${LIBRARY_PATH}=${PYTHON_LIBRARY_DIR}
+     ${PYTHON_EXECUTABLE} configure.py -q ${QT_QMAKE_EXECUTABLE}
   DEPENDS ${PyQt_DEPENDENCIES}
   )
-
-ExternalProject_Add_Step(${proj} "check-configure"
-   COMMENT "Check ${cfg}"
-   DEPENDERS configure
-   DEPENDS ${cfg}
-)
-
-#split_version(${version} version_major version_minor version_patch)
-#set(CSE_PYQT_HOME "${prefix}" PARENT_SCOPE)
-#set(CSE_PYQT_PACKAGE "${name}-${version}" PARENT_SCOPE)
-#set(CSE_PYQT_NAME "${name}" PARENT_SCOPE)
-#set(CSE_PYQT_VERSIONXX "${version_major}.${version_minor}" PARENT_SCOPE)
