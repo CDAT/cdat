2f6eda52f9d0d35aa48e4b3c12e7b150429a3f9e
David Partyka <dave.partyka@kitware.com>
2010-11-30 15:18:33 -0500

    PyQt and SIP now working on Mac.

--- CMake/External_SIP.cmake
+++ CMake/External_SIP.cmake
@@ -9,17 +9,8 @@ ExternalProject_Add(${proj}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ${LIBRARY_PATH}=${PYTHON_LIBRARY_DIR}
      ${PYTHON_EXECUTABLE} configure.py
-#    -b ${CMAKE_BINARY_DIR}/python-build/bin
-#    -d ${CMAKE_BINARY_DIR}/python-build/bin/python
-#    -e ${CMAKE_BINARY_DIR}/python-build/include
-#    -v ${CMAKE_BINARY_DIR}/python-build/share
     CC=${CMAKE_C_COMPILER}
     CXX=${CMAKE_CXX_COMPILER}
   DEPENDS python
   )
 
-#set(CSE_SIP_HOME ${prefix} PARENT_SCOPE)
-#split_version(${version} version_major version_minor version_patch)
-#set(CSE_SIP_PACKAGE "${name}-${version}" PARENT_SCOPE)
-#set(CSE_SIP_NAME "${name}" PARENT_SCOPE)
-#set(CSE_SIP_VERSIONXX "${version_major}.${version_minor}" PARENT_SCOPE)
