7a482a86a8ad779f8aef56fd9ae83f9a37e02522
David Partyka <david.partyka@kitware.com>
2011-02-06 18:38:18 -0500

    Fixup LAPACK build.

--- CMake/External_LAPACK.cmake
+++ CMake/External_LAPACK.cmake
@@ -1,14 +1,21 @@
 # The LAPACK external project for Titan
 
+set(lapack_source "${CMAKE_CURRENT_BINARY_DIR}/LAPACK")
+set(lapack_binary "${CMAKE_CURRENT_BINARY_DIR}/LAPACK-build")
+set(NUMPY_LAPACK_binary ${lapack_binary})
+
 ExternalProject_Add(LAPACK
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  SOURCE_DIR LAPACK
-  BINARY_DIR LAPACK-build
+  SOURCE_DIR ${lapack_source}
+  BINARY_DIR ${lapack_binary}
   URL ${LAPACK_URL}/${LAPACK_GZ}
   URL_MD5 ${LAPACK_MD5}
-
+  CMAKE_ARGS
+    -DCMAKE_CXX_FLAGS:STRING=${pv_tpl_cxx_flags}
+    -DCMAKE_C_FLAGS:STRING=${pv_tpl_c_flags}
+    -DBUILD_SHARED_LIBS:BOOL=ON
+    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    ${LAPACK_EXTRA_ARGS}
+  INSTALL_COMMAND ""
   )
-#list(APPEND trilinos_depends LAPACK)
-#set(trilinos_blas_args
-#  -DTPL_BLAS_LIBRARIES=${lapack_binary}/SRC/liblapack.so
-#  -DTPL_LAPACK_LIBRARIES=${lapack_binary}/BLAS/SRC/libblas.so)
+list(APPEND trilinos_depends LAPACK)
