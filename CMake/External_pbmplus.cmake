ae391f57a4ca9d13faa33da046ce0583377ba52f
David Partyka <david.partyka@kitware.com>
2011-02-07 09:42:13 -0500

    Add initial pbmplus build step.

---
+++ CMake/External_pbmplus.cmake
@@ -0,0 +1,19 @@
+
+set(pbmplus_source "${CMAKE_CURRENT_BINARY_DIR}/pbmplus")
+set(pbmplus_binary "${CMAKE_CURRENT_BINARY_DIR}/pbmplus-build")
+set(pbmplus_install "${CMAKE_CURRENT_BINARY_DIR}/pbmplus-install")
+
+ExternalProject_Add(pbmplus
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${pbmplus_source}
+  BINARY_DIR ${pbmplus_build}
+  INSTALL_DIR ${pbmplus_install}
+  URL ${PBMPLUS_URL}/${PBMPLUS_GZ}
+  URL_MD5 ${PBMPLUS_MD5}
+  BUILD_IN_SOURCE 1
+  PATCH_COMMAND ""
+  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+)
+
+set(pbmplus_DIR "${pbmplus_binary}" CACHE PATH "pbmplus binary directory" FORCE)
+mark_as_advanced(pbmplus_DIR)
