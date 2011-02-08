16c42666ece77e159861cb99bab326c440762bd7
David Partyka <david.partyka@kitware.com>
2011-02-07 20:34:58 -0500

    Remove unused macro usage.

--- CMake/External_HDF5.cmake
+++ CMake/External_HDF5.cmake
@@ -1,52 +1,20 @@
-set(name "hdf5")
-set(version "1.8.5")
-set(latest 1)
-set(release 1)
-set(prefix ${CSE_RELEASE_INSTALL_DIR}/${name}-${version})
 
 
-# Hdf5
-#
-set(proj "${name}-${version}")
-set(tgz "${CMAKE_CURRENT_SOURCE_DIR}/${proj}-patch1.tar.gz")
-set(cfg "${CMAKE_CURRENT_SOURCE_DIR}/configure.cmake")
-include("${CMAKE_CURRENT_SOURCE_DIR}/../../../../CMakeModules/cse_macros.cmake")
+set(HDF5_source "${CMAKE_CURRENT_BINARY_DIR}/HDF5")
+set(HDF5_binary "${CMAKE_CURRENT_BINARY_DIR}/HDF5-build")
+set(HDF5_install "${CMAKE_CURRENT_BINARY_DIR}/HDF5-install")
 
-ExternalProject_Add(${proj}
-  URL ${tgz}
-  CONFIGURE_COMMAND ${CMAKE_COMMAND}
-                    -Dsrc=<SOURCE_DIR>
-                    -Dprefix=${prefix}
-                    -DCFLAGS=-fno-strict-aliasing
-                    -P ${CMAKE_CURRENT_SOURCE_DIR}/configure.cmake
-  BUILD_COMMAND ${build_cmd_smp}
-  DEPENDS patch-2.5.4
+ExternalProject_Add(HDF5
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${HDF5_source}
+  BINARY_DIR ${HDF5_build}
+  INSTALL_DIR ${HDF5_install}
+  URL ${HDF5_URL}/${HDF5_GZ}
+  URL_MD5 ${HDF5_MD5}
+  BUILD_IN_SOURCE 1
+  PATCH_COMMAND ""
+  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
 )
 
-ExternalProject_Add_Step(${proj} "check-configure"
-   COMMENT "Check ${cfg}"
-   DEPENDERS configure
-   DEPENDS ${cfg}
-) 
-
-set(extras
-   "setenv HDF5_INCLUDE_DIR /usr/cta/CSE/packages/hdf5-1.8.5/include"
-   "prepend-path CMAKE_INCLUDES /usr/cta/CSE/packages/hdf5-1.8.5/include"
-   "setenv          CSE_HDF5_INCLUDE_DIR  /usr/cta/CSE/packages/hdf5-1.8.5/include"
-   "setenv          CSE_HDF5_LIB_DIR  /usr/cta/CSE/packages/hdf5-1.8.5/lib"
-)
-
-set(CSE_HDF5185_HOME ${prefix} PARENT_SCOPE)
-split_version(${version} version_major version_minor version_patch)
-set(CSE_HDF5185_PACKAGE "${name}-${version}" PARENT_SCOPE)
-set(CSE_HDF5185_NAME "${name}" PARENT_SCOPE)
-set(CSE_HDF5185_VERSIONXX "${version_major}.${version_minor}" PARENT_SCOPE)
-
-cse_module_install(
-  "${name}" "${version}"
-  "${CSE_MODRELTOOLS_DIR}" "${extras}"
-  ${release} ${latest}
-)
-
-add_test(SmokeTest-${name}-${version}
-  "${prefix}/bin/h5ls" "--version")
+set(PyOpenGL_DIR "${HDF5_binary}" CACHE PATH "HDF5 binary directory" FORCE)
+mark_as_advanced(HDF5_DIR)
