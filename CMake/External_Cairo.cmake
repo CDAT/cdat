c3c61067e7b4bd3dbeee896fba13a9c22dbef3d0
David Partyka <david.partyka@kitware.com>
2011-02-08 12:14:20 -0500

    Fix configure command for Cairo.

--- CMake/External_Cairo.cmake
+++ CMake/External_Cairo.cmake
@@ -11,7 +11,7 @@ ExternalProject_Add(Cairo
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
   #CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
-  CONFIGURE_COMMAND ${cdat_configure_command} -DINSTALL_DIR="${Cairo_install}" -DWORKING_DIR="${Cairo_source}"
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=${Cairo_install} -DWORKING_DIR=${Cairo_source} -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
   DEPENDS ${Cairo_DEPENDENCIES}
 )
 
