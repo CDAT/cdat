10e61e1baf6db8835023a5b83ea7bb76ac306441
David Partyka <david.partyka@kitware.com>
2011-02-22 12:36:39 -0500

    Fix typo in install command.

--- CMake/External_netcdf.cmake
+++ CMake/External_netcdf.cmake
@@ -1,9 +1,10 @@
 
 set(netcdf_source "${CMAKE_CURRENT_BINARY_DIR}/build/netcdf")
 set(netcdf_install "${CMAKE_CURRENT_BINARY_DIR}/Externals/NetCDF")
-set(netcdf_configure_args --enable-netcdf-4 --with-hdf5=<INSTALL_DIR>/HDF5)
+set(netcdf_configure_args "--enable-netcdf-4^^--with-hdf5=${CMAKE_CURRENT_BINARY_DIR}/Externals/HDF5")
 
 ExternalProject_Add(netcdf
+  LIST_SEPARATOR ^^
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${netcdf_source}
   INSTALL_DIR ${netcdf_install}
@@ -11,7 +12,7 @@ ExternalProject_Add(netcdf
   URL_MD5 ${NC4_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -DCONFIGURE_ARGS=${netcdf_configure_args} -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -D CONFIGURE_ARGS=${netcdf_configure_args} -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
 )
 
 set(netcdf_DIR "${netcdf_binary}" CACHE PATH "netcdf binary directory" FORCE)
