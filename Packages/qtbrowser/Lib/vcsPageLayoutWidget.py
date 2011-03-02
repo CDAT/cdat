3c4c39e596c6eb185b7053afa87738971dac3e1a
Charles Doutriaux <doutriaux1@llnl.gov>
2011-03-02 13:09:08 -0800

    Updated plot layout section

--- Packages/qtbrowser/Lib/vcsPageLayoutWidget.py
+++ Packages/qtbrowser/Lib/vcsPageLayoutWidget.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtCore,QtGui
 import vcdatCommons
-        
+import customizeVCDAT
+     
 class QPageLayoutWidget(QtGui.QWidget):
     def __init__(self,parent=None):
         QtGui.QWidget.__init__(self,parent)
@@ -8,14 +9,33 @@ class QPageLayoutWidget(QtGui.QWidget):
         self.root=parent.root
         self.grid = QtGui.QGridLayout()
         self.nlines=1
-        l = QtGui.QLabel("Here is the Page Layout Section")
-        self.grid.addWidget(l,0,0)
+        b = QtGui.QPushButton("Page Layout Section")
+        self.l = QtGui.QLabel("Page Layout Section")
+        self.menu = QtGui.QMenu()
+        add = self.menu.addAction("Create New Form")
+        b.setMenu(self.menu)
+        self.l.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.grid.addWidget(b,0,0)
+        self.grid.addWidget(self.l,0,1)
         self.addLine()
         self.setLayout(self.grid)
-        
 
+        self.connect(add,QtCore.SIGNAL('triggered ()'),self.addLine)
+        self.connect(self.l,QtCore.SIGNAL("customContextMenuRequested(QPoint)"),self.popupmenu)
+
+
+    def popupmenu(self,Point):
+        self.menu.popup(self.l.mapToGlobal(Point))
     def addLine(self):
-        self.grid.addWidget(vcdatCommons.QDropLineEdit(types=["templates",]),self.nlines,0)
-        self.grid.addWidget(vcdatCommons.QDropLineEdit(types=["graphicmethods",]),self.nlines,1)
+        tmp = vcdatCommons.QDropLineEdit(types=["templates",])
+        p = QtGui.QPalette()
+        p.setBrush(p.Base,customizeVCDAT.templatesColor)
+        tmp.setPalette(p)
+        self.grid.addWidget(tmp,self.nlines,0)
+        tmp = vcdatCommons.QDropLineEdit(types=["graphicmethods",])
+        p = QtGui.QPalette()
+        p.setBrush(p.Base,customizeVCDAT.gmsColor)
+        tmp.setPalette(p)
+        self.grid.addWidget(tmp,self.nlines,1)
         self.nlines+=1
         
