1a1bb4a657a52b00854f7fe79a7e89760c649bd8
David Partyka <david.partyka@kitware.com>
2011-03-02 20:31:55 -0500

    Cleanup Pmw build.

--- CMake/External_Pmw.cmake
+++ CMake/External_Pmw.cmake
@@ -1,19 +1,19 @@
 
 set(Pmw_source "${CMAKE_CURRENT_BINARY_DIR}/Pmw")
-set(Pmw_binary "${CMAKE_CURRENT_BINARY_DIR}/Pmw-build")
 set(Pmw_install "${CMAKE_CURRENT_BINARY_DIR}/Pmw-install")
 
 ExternalProject_Add(Pmw
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${Pmw_source}
-  BINARY_DIR ${Pmw_build}
   INSTALL_DIR ${Pmw_install}
   URL ${PMW_URL}/${PMW_GZ}
   URL_MD5 ${PMW_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  CONFIGURE_COMMAND ${LIBRARY_PATH}=${PYTHON_LIBRARY_DIR} ${PYTHON_EXECUTABLE} setup.py
 )
+
+message("PMW_URL ${PMW_URL} ${PMW_GZ}")
 
 set(Pmw_DIR "${Pmw_binary}" CACHE PATH "Pmw binary directory" FORCE)
 mark_as_advanced(Pmw_DIR)
