c21bfb399119dce553899f4565943600a09cbba4
David Partyka <david.partyka@kitware.com>
2011-03-07 17:45:48 -0500

    Make XML2 nameing consistent.

--- CMake/External_libXML2.cmake
+++ CMake/External_libXML2.cmake
@@ -1,22 +1,16 @@
 
-#  CONFIGURE_COMMAND <SOURCE_DIR>/configure
-#                    --prefix=${prefix}
-#                    --with-python=${CSE_PYTHON_HOME}
-
-set(libXML2_source "${CMAKE_CURRENT_BINARY_DIR}/libXML2")
-set(libXML2_binary "${CMAKE_CURRENT_BINARY_DIR}/libXML2-build")
-set(libXML2_install "${CMAKE_CURRENT_BINARY_DIR}/libXML2-install")
+set(libXML2_source "${CMAKE_CURRENT_BINARY_DIR}/build/libXML2")
+set(libXML2_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
 ExternalProject_Add(libXML2
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${libXML2_source}
-  BINARY_DIR ${libXML2_build}
   INSTALL_DIR ${libXML2_install}
   URL ${XML_URL}/${XML_GZ}
   URL_MD5 ${XML_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
 )
 
 set(libXML2_DIR "${libXML2_binary}" CACHE PATH "libXML2 binary directory" FORCE)
