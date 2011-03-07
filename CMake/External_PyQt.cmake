8b2f29c8973f477082526c625092f0fe68829979
David Partyka <david.partyka@kitware.com>
2011-03-07 14:36:55 -0500

    Fix fowled up PyQt URL and MD5.

--- CMake/External_PyQt.cmake
+++ CMake/External_PyQt.cmake
@@ -4,8 +4,8 @@
 set(proj PyQt)
 
 ExternalProject_Add(${proj}
-  URL ${PYQT_URL}/${PYQT_GZ_${CMAKE_PLATFORM}}
-  URL_MD5 ${PYQT_MD5_${CMAKE_PLATFORM}}
+  URL ${PYQT_URL}/${PYQT_GZ}
+  URL_MD5 ${PYQT_MD5}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ${LIBRARY_PATH}=${PYTHON_LIBRARY_DIR}
      ${PYTHON_EXECUTABLE} configure.py -q ${QT_QMAKE_EXECUTABLE} --confirm-license
