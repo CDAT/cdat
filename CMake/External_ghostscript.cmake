bf38f0a714347fa3d465ee79902df70ed8f26251
David Partyka <david.partyka@kitware.com>
2011-03-07 16:33:42 -0500

    Add ghostscript dependencies.

--- CMake/External_ghostscript.cmake
+++ CMake/External_ghostscript.cmake
@@ -13,6 +13,7 @@ ExternalProject_Add(ghostscript
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
   CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -DCONFIGURE_ARGS=${ghostscripts_args} -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  DEPENDS ${ghostscript_DEPENDENCIES}
 )
 
 set(ghostscript_DIR "${ghostscript_binary}" CACHE PATH "ghostscript binary directory" FORCE)
