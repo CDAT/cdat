7a35dd228b0fb6f1921eaed65cfca8bd22fea35a
David Partyka <david.partyka@kitware.com>
2011-03-07 17:31:00 -0500

    Enable xslt and xgks.

--- CMake/External_xgks.cmake
+++ CMake/External_xgks.cmake
@@ -10,7 +10,7 @@ ExternalProject_Add(xgks
   URL_MD5 ${XGKS_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
 )
 
 set(xgks_DIR "${xgks_binary}" CACHE PATH "xgks binary directory" FORCE)
