1474b3136030162c65c84e7a855bdf230882a0a3
David Partyka <david.partyka@kitware.com>
2011-03-07 15:04:31 -0500

    Add custom build and install steps to PyOpenGL.

--- CMake/External_PyOpenGL.cmake
+++ CMake/External_PyOpenGL.cmake
@@ -2,13 +2,25 @@
 set(PyOpenGL_source "${CMAKE_CURRENT_BINARY_DIR}/PyOpenGL")
 set(PyOpenGL_install "${CMAKE_CURRENT_BINARY_DIR}/PyOpenGL-install")
 
+configure_file(${cdat_CMAKE_SOURCE_DIR}/pyopengl_make_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/pyopengl_make_step.cmake
+  @ONLY)
+
+configure_file(${cdat_CMAKE_SOURCE_DIR}/pyopengl_install_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/pyopengl_install_step.cmake
+  @ONLY)
+
+set(Pmw_build_command ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/pyopengl_make_step.cmake)
+set(Pmw_install_command ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/pyopengl_install_step.cmake)
+
 ExternalProject_Add(PyOpenGL
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${PyOpenGL_source}
   URL ${PYOPENGL_URL}/${PYOPENGL_GZ}
   URL_MD5 ${PYOPENGL_MD5}
   BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ${LIBRARY_PATH}=${PYTHON_LIBRARY_DIR} ${PYTHON_EXECUTABLE} configure.py
+  BUILD_COMMAND ${Pmw_build_command}
+  INSTALL_COMMAND ${Pmw_install_command}
 )
 
 set(PyOpenGL_DIR "${PyOpenGL_binary}" CACHE PATH "PyOpenGL binary directory" FORCE)
