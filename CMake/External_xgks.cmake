56739229fe36ce7456f566a8808988db39025d2c
David Partyka <david.partyka@kitware.com>
2011-03-08 07:48:29 -0500

    Switch xgks to configure and use a custome configure command.

--- CMake/External_xgks.cmake
+++ CMake/External_xgks.cmake
@@ -2,6 +2,10 @@
 set(xgks_source "${CMAKE_CURRENT_BINARY_DIR}/build/xgks")
 set(xgks_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
+configure_file(${cdat_CMAKE_SOURCE_DIR}/xgks_configure_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/xgks_configure_step.cmake
+  @ONLY)
+
 ExternalProject_Add(xgks
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${xgks_source}
@@ -10,7 +14,7 @@ ExternalProject_Add(xgks
   URL_MD5 ${XGKS_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/xgks_configure_step.cmake
 )
 
 set(xgks_DIR "${xgks_binary}" CACHE PATH "xgks binary directory" FORCE)
