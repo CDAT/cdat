3dc87a7422f8ab4435e61c3e7fa80c0f3babcaa2
David Partyka <david.partyka@kitware.com>
2011-03-08 06:59:56 -0500

    Rename udunits to udunits2.

--- CMake/External_udunits.cmake
+++ CMake/External_udunits.cmake
@@ -1,18 +1,16 @@
 
-set(udunits_source "${CMAKE_CURRENT_BINARY_DIR}/udunits")
-set(udunits_binary "${CMAKE_CURRENT_BINARY_DIR}/udunits-build")
-set(udunits_install "${CMAKE_CURRENT_BINARY_DIR}/udunits-install")
+set(udunits_source "${CMAKE_CURRENT_BINARY_DIR}/build/udunits2")
+set(udunits_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
-ExternalProject_Add(udunits
+ExternalProject_Add(udunits2
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${udunits_source}
-  BINARY_DIR ${udunits_build}
   INSTALL_DIR ${udunits_install}
   URL ${UDUNITS2_URL}/${UDUNITS2_GZ}
   URL_MD5 ${UDUNITS2_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
 )
 
 set(udunits_DIR "${udunits_binary}" CACHE PATH "udunits binary directory" FORCE)
