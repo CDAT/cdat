bb6980615e24f38172d2edb7aa46ea64ef921563
Charles Doutriaux <doutriaux1@llnl.gov>
2011-03-10 16:25:07 -0800

    calculator coming along

--- Packages/qtbrowser/setup.py
+++ Packages/qtbrowser/setup.py
@@ -1,6 +1,23 @@
 from distutils.core import setup
 import os
 
+
+icons = """
+icons/ESG_download.gif       icons/equal.gif              icons/off.gif
+icons/Help.gif               icons/exp.gif                icons/on.gif
+icons/Open_folder.gif        icons/fabs.gif               icons/power.gif
+icons/Print.gif              icons/getmask.gif            icons/recycle.gif
+icons/Save.gif               icons/greater.gif            icons/regrid.gif
+icons/Script.gif             icons/grower.gif             icons/remove.gif
+icons/UV-CDAT_logo_sites.gif icons/info.gif               icons/sin.gif
+icons/add.gif                icons/inverse.gif            icons/sinh.gif
+icons/base10.gif             icons/less.gif               icons/std.gif
+icons/cos.gif                icons/log.gif                icons/subtract.gif
+icons/cosh.gif               icons/mask.gif               icons/tan.gif
+icons/divide.gif             icons/mlog.gif               icons/tanh.gif
+icons/edit.gif               icons/mlog10.gif             icons/trashcan_empty.gif
+icons/editdelete.gif         icons/multiply.gif
+""".split()
 setup (name = "qtbrowser",
        version='1.0',
        description = "Qt-based data browser",
@@ -8,5 +25,5 @@ setup (name = "qtbrowser",
        packages = ['qtbrowser'],
        package_dir = {'qtbrowser': 'Lib'},
        scripts=['Scripts/vcdat',],
-       data_files = [('share/icons',['icons/ESG_download.gif', 'icons/Open_folder.gif', 'icons/Save.gif', 'icons/Help.gif', 'icons/Print.gif', 'icons/Script.gif','icons/UV-CDAT_logo_sites.gif','icons/remove.gif','icons/on.gif','icons/off.gif']),]
+       data_files = [('share/icons',icons),]
       )
