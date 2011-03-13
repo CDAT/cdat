0153c19bc643bfd579a96a6d56430145cc797be3
David Partyka <david.partyka@kitware.com>
2011-03-13 19:37:09 -0400

    Revamp pkgconfig external project.

--- CMake/External_pkgconfig.cmake
+++ CMake/External_pkgconfig.cmake
@@ -1,14 +1,14 @@
 
-set(proj pkgconfig)
+set(pkgconfig_source "${CMAKE_CURRENT_BINARY_DIR}/build/pkgconfig")
+set(pkgconfig_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
-ExternalProject_Add(${proj}
+ExternalProject_Add(pkgconfig
   URL ${PKG_URL}/${PKG_GZ}
   URL_MD5 ${PKG_MD5}
-  SOURCE_DIR ${proj}
-  BINARY_DIR ${proj}-build
-  INSTALL_DIR ${proj}-install
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
-  BUILD_COMMAND make
-  INSTALL_COMMAND make install
+  BUILD_IN_SOURCE 1
+  PATCH_COMMAND ""
+  SOURCE_DIR ${pkgconfig_source}
+  INSTALL_DIR ${pkgconfig_install}
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
 )
 
