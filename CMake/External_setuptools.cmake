f44638d7c845efea0d4ec86231f9d411fe23b757
David Partyka <david.partyka@kitware.com>
2011-03-15 09:27:42 -0400

    Revamp setuptools external_project.

--- CMake/External_setuptools.cmake
+++ CMake/External_setuptools.cmake
@@ -1,18 +1,17 @@
 
-set(setuptools_source "${CMAKE_CURRENT_BINARY_DIR}/setuptools")
-set(setuptools_binary "${CMAKE_CURRENT_BINARY_DIR}/setuptools-build")
-set(setuptools_install "${CMAKE_CURRENT_BINARY_DIR}/setuptools-install")
+set(setuptools_source "${CMAKE_CURRENT_BINARY_DIR}/build/setuptools")
+set(setuptools_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
 ExternalProject_Add(setuptools
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${setuptools_source}
-  BINARY_DIR ${setuptools_build}
   INSTALL_DIR ${setuptools_install}
   URL ${SETUPTOOLS_URL}/${SETUPTOOLS_GZ}
   URL_MD5 ${SETUPTOOLS_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  DEPENDS ${setuptools_DEPENDENCIES}
 )
 
 set(setuptools_DIR "${setuptools_binary}" CACHE PATH "setuptools binary directory" FORCE)
