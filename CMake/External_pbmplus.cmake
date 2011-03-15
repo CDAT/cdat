fc8c3a5225ca520a5b6953dcb4cab21f737a9065
David Partyka <david.partyka@kitware.com>
2011-03-15 09:28:16 -0400

    Add patch steps to pbmplus external project step.

--- CMake/External_pbmplus.cmake
+++ CMake/External_pbmplus.cmake
@@ -2,6 +2,13 @@
 set(pbmplus_source "${CMAKE_CURRENT_BINARY_DIR}/build/pbmplus")
 set(pbmplus_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
+#cp ../../exsrc/src/pbmplus/pbmplus.h . ; cp ../../exsrc/src/pbmplus/libpbm1.c pbm  ;cp ../../exsrc/src/pbmplus/Makefile .
+
+set(EXTERNALS ${pbmplus_install})
+configure_file(${cdat_external_patch_dir}/src/pbmplus/Makefile.in
+  ${pbmplus_source}/Makefile
+  @ONLY)
+
 ExternalProject_Add(pbmplus
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${pbmplus_source}
@@ -9,9 +16,18 @@ ExternalProject_Add(pbmplus
   URL ${PBMPLUS_URL}/${PBMPLUS_GZ}
   URL_MD5 ${PBMPLUS_MD5}
   BUILD_IN_SOURCE 1
-  PATCH_COMMAND ""
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${cdat_external_patch_dir}/src/pbmplus/libpbm1.c ${pbmplus_source}/pbm/
+  CONFIGURE_COMMAND ""
 )
+
+ExternalProject_Add_Step(pbmplus CopyPbmplusHeader
+  COMMAND ${CMAKE_COMMAND} -E copy_if_different ${cdat_external_patch_dir}/src/pbmplus/pbmplus.h ${pbmplus_source}/
+  DEPENDEES patch
+  DEPENDERS configure
+  )
+
+#pbmplus install fails if this directory doesnt already exist.
+file(MAKE_DIRECTORY ${pbmplus_install}/man/mann)
 
 set(pbmplus_DIR "${pbmplus_binary}" CACHE PATH "pbmplus binary directory" FORCE)
 mark_as_advanced(pbmplus_DIR)
