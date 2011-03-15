6fea027341bb84de2d9f5102d4a758954c0f1f9a
David Partyka <david.partyka@kitware.com>
2011-03-15 10:06:10 -0400

    Add custom install and build steps for setuptools.

--- CMake/External_setuptools.cmake
+++ CMake/External_setuptools.cmake
@@ -2,6 +2,17 @@
 set(setuptools_source "${CMAKE_CURRENT_BINARY_DIR}/build/setuptools")
 set(setuptools_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
+configure_file(${cdat_CMAKE_SOURCE_DIR}/setuptools_make_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/setuptools_make_step.cmake
+  @ONLY)
+
+configure_file(${cdat_CMAKE_SOURCE_DIR}/setuptools_install_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/setuptools_install_step.cmake
+  @ONLY)
+
+set(setuptools_build_command ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/setuptools_make_step.cmake)
+set(setuptools_install_command ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/setuptools_install_step.cmake)
+
 ExternalProject_Add(setuptools
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${setuptools_source}
@@ -10,7 +21,8 @@ ExternalProject_Add(setuptools
   URL_MD5 ${SETUPTOOLS_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  BUILD_COMMAND ${setuptools_build_command}
+  INSTALL_COMMAND ${setuptools_install_command}
   DEPENDS ${setuptools_DEPENDENCIES}
 )
 
