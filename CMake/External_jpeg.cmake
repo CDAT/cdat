42f01b4ff37b806bdaa7763750692717f67c4661
David Partyka <david.partyka@kitware.com>
2011-03-15 09:29:12 -0400

    Add patch step to jpeg external project.

--- CMake/External_jpeg.cmake
+++ CMake/External_jpeg.cmake
@@ -2,6 +2,12 @@
 set(jpeg_source "${CMAKE_CURRENT_BINARY_DIR}/build/jpeg")
 set(jpeg_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
+configure_file(${cdat_CMAKE_SOURCE_DIR}/jpeg_install_step.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/jpeg_install_step.cmake
+    @ONLY)
+
+set(jpeg_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/jpeg_install_step.cmake)
+
 ExternalProject_Add(jpeg
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${jpeg_source}
@@ -11,7 +17,10 @@ ExternalProject_Add(jpeg
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
   CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  INSTALL_COMMAND ${jpeg_INSTALL_COMMAND}
 )
+
+
 
 set(jpeg_DIR "${jpeg_binary}" CACHE PATH "jpeg binary directory" FORCE)
 mark_as_advanced(jpeg_DIR)
