e792b9a37462f9dd0ba9f104ad38ce16dda10e68
David Partyka <david.partyka@kitware.com>
2011-03-21 09:14:28 -0400

    Base the the makefile build system, only configure CMOR?

--- CMake/External_CMOR.cmake
+++ CMake/External_CMOR.cmake
@@ -4,6 +4,7 @@ set(cmor_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
 set(cmor_configure_args --with-netcdf=${netcdf_install} --with-udunits2=${udunits_install} --with-uuid=${uuid_install} --with-zlib=${zlib_install} --enable-fortran=no)
 
+# it appears currently we only configure cmor but not build it.
 ExternalProject_Add(cmor
   GIT_REPOSITORY http://uv-cdat.llnl.gov/git/cmor.git
   SOURCE_DIR ${cmor_source}
@@ -11,6 +12,8 @@ ExternalProject_Add(cmor
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
   CONFIGURE_COMMAND sh configure --prefix=<INSTALL_DIR> ${cmor_configure_args}
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
   DEPENDS ${cmor_DEPENDENCIES}
 )
 
