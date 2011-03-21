becf4425d821d3618db012fd1dcad8c6dfbdd73f
David Partyka <david.partyka@kitware.com>
2011-03-21 09:14:59 -0400

    Adjust spellings of CLAPACK.

--- CMake/External_CLAPACK.cmake
+++ CMake/External_CLAPACK.cmake
@@ -4,13 +4,22 @@ set(clapack_source "${CMAKE_CURRENT_BINARY_DIR}/build/LAPACK")
 set(clapack_binary "${CMAKE_CURRENT_BINARY_DIR}/External")
 set(NUMPY_LAPACK_binary ${clapack_binary})
 
+#
+# To fix compilation problem: relocation R_X86_64_32 against `a local symbol' can not be
+# used when making a shared object; recompile with -fPIC
+# See http://www.cmake.org/pipermail/cmake/2007-May/014350.html
+#
+if(UNIX AND CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+  set(cdat_tpl_c_flags_LAPACK "-fPIC ${pv_tpl_c_flags}")
+endif()
+
 ExternalProject_Add(CLAPACK
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  SOURCE_DIR ${clapack_source}
-  BINARY_DIR ${clapack_binary}
+  SOURCE_DIR ${CLAPACK_source}
+  BINARY_DIR ${CLAPACK_binary}
   URL ${CLAPACK_URL}/${CLAPACK_GZ}
   URL_MD5 ${CLAPACK_MD5}
-  CMAKE_ARGS
+  CMAKE_CACHE_ARGS
     -DCMAKE_CXX_FLAGS:STRING=${cdat_tpl_cxx_flags}
     -DCMAKE_C_FLAGS:STRING=${cdat_tpl_c_flags}
     -DBUILD_SHARED_LIBS:BOOL=ON
