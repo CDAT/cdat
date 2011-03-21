575b3fd7ec8a21423c25ec8e200ebaa3fb314d06
David Partyka <david.partyka@kitware.com>
2011-03-21 06:27:05 -0400

    Cleanup building LAPACK.

--- CMake/External_LAPACK.cmake
+++ CMake/External_LAPACK.cmake
@@ -1,7 +1,7 @@
 # The LAPACK external project for Titan
 
-set(lapack_source "${CMAKE_CURRENT_BINARY_DIR}/LAPACK")
-set(lapack_binary "${CMAKE_CURRENT_BINARY_DIR}/LAPACK-build")
+set(lapack_source "${CMAKE_CURRENT_BINARY_DIR}/build/LAPACK")
+set(lapack_binary "${CMAKE_CURRENT_BINARY_DIR}/External")
 set(NUMPY_LAPACK_binary ${lapack_binary})
 
 ExternalProject_Add(LAPACK
@@ -11,8 +11,8 @@ ExternalProject_Add(LAPACK
   URL ${LAPACK_URL}/${LAPACK_GZ}
   URL_MD5 ${LAPACK_MD5}
   CMAKE_ARGS
-    -DCMAKE_CXX_FLAGS:STRING=${pv_tpl_cxx_flags}
-    -DCMAKE_C_FLAGS:STRING=${pv_tpl_c_flags}
+    -DCMAKE_CXX_FLAGS:STRING=${cdat_tpl_cxx_flags}
+    -DCMAKE_C_FLAGS:STRING=${cdat_tpl_c_flags}
     -DBUILD_SHARED_LIBS:BOOL=ON
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     ${LAPACK_EXTRA_ARGS}
