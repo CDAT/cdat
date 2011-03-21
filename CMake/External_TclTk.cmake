c50e71ea6b5955d6e0bee7668e61b1c40a8137b3
David Partyka <david.partyka@kitware.com>
2011-03-21 10:54:15 -0400

    Adjust build directories for tcltk.

--- CMake/External_TclTk.cmake
+++ CMake/External_TclTk.cmake
@@ -1,12 +1,19 @@
 
+set(tcl_source "${CMAKE_CURRENT_BINARY_DIR}/build/tcl")
+set(tk_source "${CMAKE_CURRENT_BINARY_DIR}/build/tk")
+set(tcltk_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
+
 # tcl
 #
 set(proj tcl-${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC})
 
 ExternalProject_Add(${proj}
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${tk_source}
+  INSTALL_DIR ${tcltk_install}
   URL ${TCLTK_URL}/${TCL_GZ}
   URL_MD5 ${TCL_MD5}
-  INSTALL_DIR tcltk-install
+  BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND <SOURCE_DIR>/unix/configure --prefix=<INSTALL_DIR>
   BUILD_COMMAND make
 )
@@ -16,9 +23,12 @@ ExternalProject_Add(${proj}
 set(proj tk-${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC})
 
 ExternalProject_Add(${proj}
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${tk_source}
+  INSTALL_DIR ${tcltk_install}
   URL ${TCLTK_URL}/${TK_GZ}
   URL_MD5 ${TK_MD5}
-  INSTALL_DIR tcltk-install
+  BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND <SOURCE_DIR>/unix/configure --prefix=<INSTALL_DIR>  --with-tcl=<INSTALL_DIR>/lib
   BUILD_COMMAND make
   DEPENDS tcl-${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC}
@@ -26,7 +36,7 @@ ExternalProject_Add(${proj}
 
 ExternalProject_Add_Step(${proj} symlink
   COMMAND ${CMAKE_COMMAND} -E create_symlink "wish${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC}" wish
-  WORKING_DIRECTORY tcltk-install/bin
+  WORKING_DIRECTORY ${tcltk_install}/bin
   COMMENT "Linking wish${TCLTK_MAJOR_SRC}.${TCLTK_MINOR_SRC} to wish"
   DEPENDEES install
 )
