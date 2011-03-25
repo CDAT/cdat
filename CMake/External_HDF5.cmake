35cf2085ec108deca7c85e9cf7d5ec6c21cbb08a
David Partyka <david.partyka@kitware.com>
2011-03-25 11:56:50 -0400

    Disable HDF5 build warnnings as they drown out the build output.

--- CMake/External_HDF5.cmake
+++ CMake/External_HDF5.cmake
@@ -1,7 +1,8 @@
 
-set(HDF5_source "${CMAKE_CURRENT_BINARY_DIR}/HDF5")
+set(HDF5_source "${CMAKE_CURRENT_BINARY_DIR}/build/HDF5")
 set(HDF5_install "${CMAKE_CURRENT_BINARY_DIR}/Externals/HDF5")
 
+# we disable HDF5 warnings because it has way too many of them.
 ExternalProject_Add(HDF5
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${HDF5_source}
@@ -10,7 +11,7 @@ ExternalProject_Add(HDF5
   URL_MD5 ${HDF5_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DADDITIONAL_CFLAGS=-w -DADDITIONAL_CPPFPAGS=-w -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
 )
 
 set(HDF5_DIR "${HDF5_binary}" CACHE PATH "HDF5 binary directory" FORCE)
