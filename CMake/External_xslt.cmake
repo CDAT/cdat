d839e30d97864d65e9ecc432ed1350f1b32ee8c0
David Partyka <david.partyka@kitware.com>
2011-03-25 11:53:13 -0400

    Pass libxml configure argument when configuring xslt.

--- CMake/External_xslt.cmake
+++ CMake/External_xslt.cmake
@@ -2,6 +2,8 @@
 set(xslt_source "${CMAKE_CURRENT_BINARY_DIR}/build/xslt")
 set(xslt_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
 
+set(xslt_configure_args --with-libxml-prefix=${xslt_install})
+
 ExternalProject_Add(xslt
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${xslt_source}
@@ -10,8 +12,7 @@ ExternalProject_Add(xslt
   URL_MD5 ${XSLT_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCONFIGURE_ARGS=${xslt_configure_args} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  DEPENDS ${xslt_DEPENDENCIES}
 )
 
-set(xslt_DIR "${xslt_binary}" CACHE PATH "xslt binary directory" FORCE)
-mark_as_advanced(xslt_DIR)
