237ed232b1050d1cb3b886dd8617d8086a687f80
David Partyka <david.partyka@kitware.com>
2011-03-25 11:58:10 -0400

    Set Runtime Library path.

--- CMake/External_CDAT.cmake
+++ CMake/External_CDAT.cmake
@@ -1,6 +1,7 @@
 
 set(CDAT_source "${cdat_SOURCE_DIR}")
 
+set(RUNTIME_FLAGS "${cdat_BINARY_DIR}/Externals/lib:$ENV{${LIBRARY_PATH}}")
 set(LDFLAGS "-L${cdat_BINARY_DIR}/Externals/NetCDF/lib -L${cdat_BINARY_DIR}/Externals/lib -L${cdat_BINARY_DIR}/lib")
 
 get_filename_component(QT_BINARY_DIR ${QT_QMAKE_EXECUTABLE} PATH)
@@ -8,12 +9,12 @@ get_filename_component(QT_ROOT ${QT_BINARY_DIR} PATH)
 
 ExternalProject_Add(CDAT
   DOWNLOAD_DIR ""
-  SOURCE_DIR ${CDAT_source}
+  SOURCE_DIR ${cdat_SOURCE_DIR}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND env LDFLAGS=${LDFLAGS} ${PYTHON_EXECUTABLE} install.py --enable-qt-framework --with-qt=${QT_ROOT}
+  INSTALL_COMMAND env LDFLAGS=${LDFLAGS} ${LIBRARY_PATH}=${RUNTIME_FLAGS} ${PYTHON_EXECUTABLE} install.py --enable-qt-framework --with-qt=${QT_ROOT}
   DEPENDS ${CDAT_DEPENDENCIES}
 )
 
