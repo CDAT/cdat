e598791a4fc1c304bfbf707a8c574cba35469f84
David Partyka <david.partyka@kitware.com>
2011-03-31 13:10:13 -0400

    Quote LDFLAGS when building CDAT.
    Also remove a non existent link search directory.

--- CMake/External_CDAT.cmake
+++ CMake/External_CDAT.cmake
@@ -2,10 +2,12 @@
 set(CDAT_source "${cdat_SOURCE_DIR}")
 
 set(RUNTIME_FLAGS "${cdat_BINARY_DIR}/Externals/lib:$ENV{${LIBRARY_PATH}}")
-set(LDFLAGS "-L${cdat_BINARY_DIR}/Externals/NetCDF/lib -L${cdat_BINARY_DIR}/Externals/lib -L${cdat_BINARY_DIR}/lib")
+set(LDFLAGS "-L${cdat_BINARY_DIR}/Externals/NetCDF/lib -L${cdat_BINARY_DIR}/Externals/lib")
 
 get_filename_component(QT_BINARY_DIR ${QT_QMAKE_EXECUTABLE} PATH)
 get_filename_component(QT_ROOT ${QT_BINARY_DIR} PATH)
+
+#env EXTERNALS=/Users/partyd/Kitware/uv-cdat/make-file-install//Externals  LDFLAGS="${LDFLAGS/"/} -undefined dynamic_lookup"  /Users/partyd/Kitware/uv-cdat/make-file-install//bin/python install.py  --enable-qt-framework  --with-qt=/Users/partyd/Kitware/uv-cdat/make-file-install//Externals
 
 ExternalProject_Add(CDAT
   DOWNLOAD_DIR ""
@@ -14,7 +16,7 @@ ExternalProject_Add(CDAT
   PATCH_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND env LDFLAGS=${LDFLAGS} ${LIBRARY_PATH}=${RUNTIME_FLAGS} ${PYTHON_EXECUTABLE} install.py --enable-qt-framework --with-qt=${QT_ROOT}
+  INSTALL_COMMAND env EXTERNALS=${cdat_BINARY_DIR}/Externals LDFLAGS="${LDFLAGS}" ${LIBRARY_PATH}="${RUNTIME_FLAGS}" ${PYTHON_EXECUTABLE} install.py --enable-qt-framework --with-qt=${QT_ROOT}
   DEPENDS ${CDAT_DEPENDENCIES}
 )
 
