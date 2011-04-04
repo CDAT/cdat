9afe71344eb563d1a26efcc1576b01ae1c1bf481
David Partyka <david.partyka@kitware.com>
2011-04-04 09:45:52 -0400

    Build tiff, dependency of pbmplus.

---
+++ CMake/External_tiff.cmake
@@ -0,0 +1,15 @@
+
+set(tiff_source "${CMAKE_CURRENT_BINARY_DIR}/build/tiff")
+set(tiff_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
+
+ExternalProject_Add(tiff
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${tiff_source}
+  INSTALL_DIR ${tiff_install}
+  URL ${TIFF_URL}/${TIFF_GZ}
+  URL_MD5 ${TIFF_MD5}
+  BUILD_IN_SOURCE 1
+  PATCH_COMMAND ""
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  DEPENDS ${tiff_DEPENDENCIES}
+)
