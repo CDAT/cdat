eeba43c7bea35595728370fbbd4a94797812aeea
David Partyka <david.partyka@kitware.com>
2011-04-04 15:15:05 -0400

    Use External_libXSLT instead of External_xlst.
    It matches the cmake find module.

--- CMake/External_libXSLT.cmake
+++ CMake/External_libXSLT.cmake
@@ -1,19 +1,19 @@
 
-set(libXSLT_source "${CMAKE_CURRENT_BINARY_DIR}/libXSLT")
-set(libXSLT_binary "${CMAKE_CURRENT_BINARY_DIR}/libXSLT-build")
-set(libXSLT_install "${CMAKE_CURRENT_BINARY_DIR}/libXSLT-install")
+set(libXSLT_source "${CMAKE_CURRENT_BINARY_DIR}/build/libXSLT")
+set(libXSLT_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
+
+if(NOT LIBXML2_FOUND)
+  set(libXSLT_configure_args --with-libxml-prefix=${xslt_install})
+endif()
 
 ExternalProject_Add(libXSLT
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
   SOURCE_DIR ${libXSLT_source}
-  BINARY_DIR ${libXSLT_build}
   INSTALL_DIR ${libXSLT_install}
   URL ${XSLT_URL}/${XSLT_GZ}
   URL_MD5 ${XSLT_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCONFIGURE_ARGS=${libXSLT_configure_args} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  DEPENDS ${libXSLT_DEPENDENCIES}
 )
-
-set(libXSLT_DIR "${libXSLT_binary}" CACHE PATH "libXSLT binary directory" FORCE)
-mark_as_advanced(libXSLT_DIR)
