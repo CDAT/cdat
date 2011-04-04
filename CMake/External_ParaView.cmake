cb8d376d9d6ca5a696a2201a57d7366988b9daa6
David Partyka <david.partyka@kitware.com>
2011-04-04 15:45:53 -0400

    Add ParaView build step.

---
+++ CMake/External_ParaView.cmake
@@ -0,0 +1,27 @@
+
+# If Windows we use CMake otherwise ./configure
+
+set(vtk_source "${CMAKE_CURRENT_BINARY_DIR}/ParaView")
+set(vtk_binary "${CMAKE_CURRENT_BINARY_DIR}/ParaView-build")
+set(vtk_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
+
+ExternalProject_Add(ParaView
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${vtk_source}
+  BINARY_DIR ${vtk_build}
+  INSTALL_DIR ${vtk_install}
+  URL ${VTK_URL}/${VTK_GZ}
+  URL_MD5 ${VTK_MD5}
+  PATCH_COMMAND ""
+  CMAKE_CACHE_ARGS
+    -DBUILD_SHARED_LIBS:BOOL=ON
+    -DBUILD_TESTING:BOOL=OFF
+    -DCMAKE_CXX_FLAGS:STRING=${cdat_tpl_cxx_flags}
+    -DCMAKE_C_FLAGS:STRING=${cdat_tpl_c_flags}
+    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_CFG_INTDIR}
+    ${cdat_compiler_args}
+    -DWRAP_VTK_PYTHON:BOOL=ON
+  CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+  DEPENDS ${
+)
