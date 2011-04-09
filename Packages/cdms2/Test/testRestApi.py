9ca119b1873fc0fd0ed30297461d5ef21fc0c613
Charles Doutriaux <doutriaux1@llnl.gov>
2011-04-08 18:12:43 -0700

    First draft at accessing via cdms donepb ; p Test/testRestApi.py

--- Packages/cdms2/Test/testRestApi.py
+++ Packages/cdms2/Test/testRestApi.py
@@ -5,12 +5,24 @@ import sys,os
 readline.parse_and_bind("tab: complete")
 
 myNode = cdms2.esgNodeConnection("esg-datanode.jpl.nasa.gov")
+#datasets =  myNode.search(stringType=True,variable="ta")
 datasets =  myNode.search(stringType=False,variable="ta")
-d0=datasets[0]
-#print d0.files[:]
-print d0.search(stringType=True)
-print d0.url
-print d0.title
-print d0.timestamp
-print d0.id
-print d0.files
+print datasets
+for d in datasets[::-1]:
+    print "---------- Dataset  ----------" 
+    print d
+print d.files
+for f in d.files:
+    print f
+    
+F = cdms2.open(f["OPENDAP"])
+
+print F.variables.keys()
+
+ta=F['ta']
+print ta.shape
+ta=ta[0,0]
+import vcs
+x=vcs.init()
+x.plot(ta)
+raw_input("Press enter")
