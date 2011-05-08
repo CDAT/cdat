912c1de117ed354f62e3b598efb227ed1fe6acc4
David Partyka <david.partyka@kitware.com>
2011-05-08 18:22:36 -0400

    Add CURL module.

---
+++ CMake/External_CURL.cmake
@@ -0,0 +1,15 @@
+
+set(curl_source "${CMAKE_CURRENT_BINARY_DIR}/build/CURL")
+set(curl_install "${CMAKE_CURRENT_BINARY_DIR}/Externals")
+
+ExternalProject_Add(CURL
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${curl_source}
+  INSTALL_DIR ${curl_install}
+  URL ${CURL_URL}/${CURL_GZ}
+  URL_MD5 ${CURL_MD5}
+  BUILD_IN_SOURCE 1
+  PATCH_COMMAND ""
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
+  DEPENDS ${curl_DEPENDENCIES}
+)
