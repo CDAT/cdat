55abe028dad52ea33de223682c3bb499562083f5
David Partyka <david.partyka@kitware.com>
2011-05-30 11:23:26 -0400

    Make custom configure line for building on Apple.
    Don't set DYLD_LIBRARY_PATH when building on the Mac.

--- CMake/External_PyQt.cmake
+++ CMake/External_PyQt.cmake
@@ -2,12 +2,22 @@
 # PyQt
 #
 set(proj PyQt)
+set(PyQt_source "${CMAKE_CURRENT_BINARY_DIR}/build/PyQt")
+
+if(APPLE)
+  set(PyQt_configure_command "${PYTHON_EXECUTABLE} configure.py -q ${QT_QMAKE_EXECUTABLE} --confirm-license")
+else()
+  set(PyQt_configure_command "${LIBRARY_PATH}=${PYTHON_LIBRARY_DIR} ${PYTHON_EXECUTABLE} configure.py -q ${QT_QMAKE_EXECUTABLE} --confirm-license")
+endif()
 
 ExternalProject_Add(${proj}
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${PyQt_source}
   URL ${PYQT_URL}/${PYQT_GZ_${CMAKE_PLATFORM}}
   URL_MD5 ${PYQT_MD5_${CMAKE_PLATFORM}}
   BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ${LIBRARY_PATH}=${PYTHON_LIBRARY_DIR}
-     ${PYTHON_EXECUTABLE} configure.py -q ${QT_QMAKE_EXECUTABLE} --confirm-license
+  CONFIGURE_COMMAND ${PyQt_configure_command}
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
   DEPENDS ${PyQt_DEPENDENCIES}
   )
