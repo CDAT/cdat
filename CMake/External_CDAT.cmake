7cf083f511a1c4fca77d631bc3d99ef216a9e3aa
David Partyka <david.partyka@kitware.com>
2011-06-09 08:32:14 -0400

    Add Qt include flags to CFLAGS, CPPFLAGS and CXXFLAGS

--- CMake/External_CDAT.cmake
+++ CMake/External_CDAT.cmake
@@ -20,6 +20,8 @@ endif()
 if(CDAT_USE_SYSTEM_QT AND QT_QTCORE_INCLUDE_DIR)
   get_filename_component(QT_INCLUDE_ROOT ${QT_QTCORE_INCLUDE_DIR} PATH)
   set(ADDITIONAL_CPPFLAGS "-I${QT_INCLUDE_ROOT}")
+  set(ADDITIONAL_CXXFLAGS "-I${QT_INCLUDE_ROOT}")
+  set(ADDITIONAL_CFLAGS "-I${QT_INCLUDE_ROOT}")
 endif()
 
 configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_python_install_step.cmake.in
