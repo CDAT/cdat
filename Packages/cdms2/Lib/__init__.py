daf1343b6b3096985076861b2f7cc08364fd798b
Charles Doutriaux <doutriaux1@llnl.gov>
2011-06-15 21:20:13 -0700

    updated pinger, only change code in one place now

--- Packages/cdms2/Lib/__init__.py
+++ Packages/cdms2/Lib/__init__.py
@@ -1,15 +1,8 @@
 """
 CDMS module-level API
 """
-try:
-    import cdat_info,os,sys
-    if cdat_info.ping is False:
-        import urllib2,os
-        urllib2.urlopen("http://uv-cdat.llnl.gov/UVCDATLogger/%s/%s/cdat/start" % (os.getlogin(),sys.platform))
-        cdat_info.ping = True
-except:
-    cdat_info.ping = False
-    pass
+import cdat_info
+cdat_info.pingPCMDIdb("cdat","start")
 __all__ = ["cdmsobj", "axis", "coord", "grid", "hgrid", "avariable", "sliceut", "error", "variable", "fvariable", "tvariable", "dataset", "database", "cache", "selectors", "MV2", "convention", "bindex", "auxcoord", "gengrid", "gsHost", "gsStatVar"]
 # Errors
 from error import CDMSError
