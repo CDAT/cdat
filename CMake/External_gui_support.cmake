28ca16f49184a49c999b57edd5d5ddd615e58111
David Partyka <david.partyka@kitware.com>
2011-06-16 17:02:25 -0400

    More runtime path fixes to deal with python relocation.

--- CMake/External_gui_support.cmake
+++ CMake/External_gui_support.cmake
@@ -1,5 +1,6 @@
 
 set(gui_support_source ${cdat_SOURCE_DIR}/Packages/gui_support)
+set(runtime_library_path ${CMAKE_INSTALL_PREFIX}/lib:${cdat_EXTERNALS}/lib)
 
 ExternalProject_Add(gui_support
   DOWNLOAD_DIR ""
@@ -8,7 +9,7 @@ ExternalProject_Add(gui_support
   PATCH_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND env EXTERNALS=${cdat_EXTERNALS} ${LIBRARY_PATH}=${RUNTIME_FLAGS} ${PYTHON_EXECUTABLE} setup.py build --force install
+  INSTALL_COMMAND env EXTERNALS=${cdat_EXTERNALS} ${LIBRARY_PATH}=${runtime_library_path} ${PYTHON_EXECUTABLE} setup.py build --force install
   DEPENDS ${gui_support_DEPENDENCIES}
 )
 
