e8fd52d1f941cca5f0a710157616a2c4dd4082ca
David Partyka <david.partyka@kitware.com>
2011-07-12 17:35:55 -0400

    Add PyTables dependency Numexpr.

---
+++ CMake/External_Numexpr.cmake
@@ -0,0 +1,17 @@
+
+# Numexpr
+#
+set(Numexpr_source "${CMAKE_CURRENT_BINARY_DIR}/build/Numexpr")
+
+ExternalProject_Add(Numexpr
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${Numexpr_source}
+  URL ${NUMEXPR_URL}/${NUMEXPR_GZ}
+  URL_MD5 ${NUMEXPR_MD5}
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
+  INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install
+  DEPENDS ${Numexpr_DEPENDENCIES}
+#  ${EP_LOG_OPTIONS}
+  )
