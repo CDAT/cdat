56554d82f1a80968218bdfcc11d60c111fc668ea
Charles Doutriaux <doutriaux1@llnl.gov>
2012-02-14 10:50:51 -0800

    Fixed bug that caused vcs to crash when plotting in background mode

--- Packages/vcs/Src/library/vcs_update.c
+++ Packages/vcs/Src/library/vcs_update.c
@@ -1408,6 +1408,7 @@ void draw_logo(cairo_t *cr) {
   int xtmp;
   cairo_surface_t *surface;
   cairo_surface_type_t stype;
+  if (cr == NULL) return;
   surface = cairo_get_target(cr);
   stype = cairo_surface_get_type(surface);
   if (stype==CAIRO_SURFACE_TYPE_IMAGE) {
