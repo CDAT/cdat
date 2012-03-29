0e781abf16b2919fa0f0aa04a28169ed29b8f88a
Harinarayan Krishnan <hkrishnan@lbl.gov>
2012-03-29 13:17:51 -0700

    updating VisIt build to post install dependent libraries, Also updated CMakeLists.txt to have SIP and PyQt as dependencies

--- CMake/External_VisIt.cmake
+++ CMake/External_VisIt.cmake
@@ -66,21 +66,22 @@ MACRO(DETERMINE_VISIT_ARCHITECTURE ARCH)
     ENDIF(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
 ENDMACRO(DETERMINE_VISIT_ARCHITECTURE ARCH)
 
+DETERMINE_VISIT_ARCHITECTURE(VISIT_INSTALL_PLATFORM)
 #SET(VISIT_HOSTNAME $ENV{HOSTNAME})
 SITE_NAME(VISIT_HOSTNAME)
 SET(TMP_STR1 "FILE(APPEND ${VisIt_source}/${VISIT_HOSTNAME}.cmake \"VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${cdat_EXTERNALS})\\n\")\n")
 SET(TMP_STR2 "FILE(APPEND ${VisIt_source}/${VISIT_HOSTNAME}.cmake \"VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${cdat_EXTERNALS})\\n\")\n")
 #SET(TMP_STR3 "FILE(APPEND ${VisIt_source}/${VISIT_HOSTNAME}.cmake \"VISIT_OPTION_DEFAULT(VISIT_C_COMPILER ${CMAKE_C_COMPILER} TYPE FILEPATH)\\n\")\n")
 #SET(TMP_STR4 "FILE(APPEND ${VisIt_source}/${VISIT_HOSTNAME}.cmake \"VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER ${CMAKE_CXX_COMPILER} TYPE FILEPATH)\\n\")\n")
-SET(TMP_STR5 "FILE(APPEND ${VisIt_source}/${VISIT_HOSTNAME}.cmake \"VISIT_OPTION_DEFAULT(HAVE_PYQT ON TYPE BOOL)\\n\")\n")
-SET(TMP_STR6 "FILE(APPEND ${VisIt_source}/${VISIT_HOSTNAME}.cmake \"VISIT_OPTION_DEFAULT(VISIT_SIP_DIR ${CMAKE_INSTALL_PREFIX})\\n\")\n")
+SET(TMP_STR3 "FILE(APPEND ${VisIt_source}/${VISIT_HOSTNAME}.cmake \"VISIT_OPTION_DEFAULT(HAVE_PYQT ON TYPE BOOL)\\n\")\n")
+SET(TMP_STR4 "FILE(APPEND ${VisIt_source}/${VISIT_HOSTNAME}.cmake \"VISIT_OPTION_DEFAULT(VISIT_SIP_DIR ${CMAKE_INSTALL_PREFIX})\\n\")\n")
 
 FILE(WRITE   ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake ${TMP_STR1})
 FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake ${TMP_STR2})
 FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake ${TMP_STR3})
 FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake ${TMP_STR4})
-FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake ${TMP_STR5})
-FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake ${TMP_STR6})
+#FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake ${TMP_STR5})
+#FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake ${TMP_STR6})
 FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake "FILE(WRITE ${VisIt_source}/CMakeLists.txt \"cmake_minimum_required(VERSION 2.8)\\n\")\n")
 FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake "FILE(APPEND ${VisIt_source}/CMakeLists.txt \"add_subdirectory(mangled_src)\\n\")\n")
 
@@ -89,13 +90,6 @@ FILE(WRITE ${CMAKE_CURRENT_BINARY_DIR}/visit_patch "cd ${VisIt_source} | echo ye
 FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_patch "${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/visit.cmake\n")
 FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit_patch "${CMAKE_COMMAND} -E  copy ${VisIt_source}/${VISIT_HOSTNAME}.cmake ${VisIt_source}/mangled_src/config-site/${VISIT_HOSTNAME}.cmake\n")
 FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_patch "cd ${VisIt_source} | echo yes | ${VisIt_source}/src/svn_bin/build_visit --console --cc ${VISIT_C_COMPILER} --cxx ${VISIT_CXX_COMPILER} --gpl --R --stdout --thirdparty-path ${VisIt_install}/thirdparty --vtk --mesa --cmake-bin-dir ${CMAKE_PATH_VAR} --alt-python-dir ${CMAKE_INSTALL_PREFIX} --alt-qt-dir ${QT_ROOT} --mangle-libraries --makeflags \"-j4\" --no-hostconf\n")
-
-IF(NOT APPLE AND NOT WIN32) #UNIX only, make symlinks..
-#determine visit architecture..
-#DETERMINE_VISIT_ARCHITECTURE(VISIT_INSTALL_PLATFORM)
-#FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit_patch "${CMAKE_COMMAND} -E create_symlink ${VisIt_install}/${VISIT_VERSION}/${VISIT_INSTALL_PLATFORM}/lib ${VisIt_install}/lib/VisIt-${VISIT_VERSION}\n")
-#FILE(APPEND  ${CMAKE_CURRENT_BINARY_DIR}/visit_patch "${CMAKE_COMMAND} -E create_symlink ${VisIt_install}/${VISIT_VERSION}/${VISIT_INSTALL_PLATFORM}/plugins ${VisIt_install}/lib/VisIt-${VISIT_VERSION}-plugins\n")
-ENDIF(NOT APPLE AND NOT WIN32)
 
 #build
 ExternalProject_Add(VisIt
@@ -112,3 +106,51 @@ ExternalProject_Add(VisIt
   DEPENDS ${VisIt_DEPENDENCIES}
   ${EP_LOG_OPTIONS}
 )
+
+if(NOT EXISTS ${CMAKE_INSTALL_PREFIX}/lib)
+  file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib)
+endif()
+
+#Make symlinks of VisIt's lib, plugins, 
+#copy hdf5, netcdf
+#move pyqt_pyqtviewer.so into python site-packages
+ExternalProject_Add_Step(VisIt InstallVisItLibSymLink
+  COMMAND ${CMAKE_COMMAND} -E create_symlink ${VisIt_install}/${VISIT_VERSION}/${VISIT_INSTALL_PLATFORM}/lib ${CMAKE_INSTALL_PREFIX}/lib/VisIt-${VISIT_VERSION}
+  DEPENDEES install
+  WORKING_DIRECTORY ${cdat_CMAKE_BINARY_DIR})
+
+ExternalProject_Add_Step(VisIt InstallVisItPluginSymLink
+  COMMAND ${CMAKE_COMMAND} -E create_symlink ${VisIt_install}/${VISIT_VERSION}/${VISIT_INSTALL_PLATFORM}/plugins ${CMAKE_INSTALL_PREFIX}/lib/VisIt-${VISIT_VERSION}-plugins
+  DEPENDEES install
+  WORKING_DIRECTORY ${cdat_CMAKE_BINARY_DIR})
+
+FILE(WRITE ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "MESSAGE(STATUS \"Executing VisIt post installation steps\")\n")
+FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "file(GLOB hdf5_files ${HDF5_install}/lib/libhdf5*${_LINK_LIBRARY_SUFFIX})\n")
+FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "file(COPY \${hdf5_files} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/VisIt-${VISIT_VERSION}/)\n")
+
+FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "file(GLOB netcdf_files ${netcdf_install}/lib/libnetcdf*${_LINK_LIBRARY_SUFFIX})\n")
+FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "file(COPY \${netcdf_files} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/VisIt-${VISIT_VERSION}/)\n")
+
+FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "file(GLOB z_files ${zlib_install}/lib/libz*${_LINK_LIBRARY_SUFFIX})\n")
+FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "file(COPY \${z_files} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/VisIt-${VISIT_VERSION}/)\n")
+
+FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "file(GLOB curl_files ${curl_install}/lib/libcurl*${_LINK_LIBRARY_SUFFIX})\n")
+FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch "file(COPY \${curl_files} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/VisIt-${VISIT_VERSION}/)\n")
+
+
+ExternalProject_Add_Step(VisIt InstallVisItExternalLibraries
+  COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/visit_install_patch
+  DEPENDEES InstallVisItLibSymLink
+  WORKING_DIRECTORY ${cdat_CMAKE_BINARY_DIR}
+  )
+
+ExternalProject_Add_Step(VisIt InstallVisItPyQtViewer
+  COMMAND ${CMAKE_COMMAND} -E copy ${VisIt_install}/${VISIT_VERSION}/${VISIT_INSTALL_PLATFORM}/lib/pyqt_pyqtviewer.so ${PYTHON_SITE_PACKAGES}
+  DEPENDEES install
+  WORKING_DIRECTORY ${cdat_CMAKE_BINARY_DIR})
+
+ExternalProject_Add_Step(VisIt InstallVisItModule
+  COMMAND ${CMAKE_COMMAND} -E copy ${VisIt_install}/${VISIT_VERSION}/${VISIT_INSTALL_PLATFORM}/lib/visit.so ${PYTHON_SITE_PACKAGES}
+  DEPENDEES install
+  WORKING_DIRECTORY ${cdat_CMAKE_BINARY_DIR})
+
