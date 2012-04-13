21c773c064c726df3f40a0314eaae1ef537d1093
Dave Kindig <kindig@txcorp.com>
2012-04-12 18:50:34 -0600

    Extended gsRegrid tests to include setting the mask for both setMask and setValidMask

--- Packages/cdms2/Lib/gsRegrid.py
+++ Packages/cdms2/Lib/gsRegrid.py
@@ -108,7 +108,7 @@ def makeCurvilinear(coords):
         elif rank == 3 and nd == 2 and i > 0:
             # assume leading coordinate is an axis
             o1 = numpy.ones( (len(coords[0]),), coords[i].dtype )
-            coords[i] = numpy.outer(o1, coords[i]).reshape(dims)
+            coords[i] = numpy.ma.outer(o1, coords[i]).reshape(dims)
         else:
             raise CDMSError, \
                 "ERROR in %s: funky mixture of axes and curvilinear coords %s" \
@@ -530,7 +530,6 @@ class Regrid:
         @note This must be invoked before computing the weights, the 
         mask is a property of the grid (not the data).
         """
-        print 'gsRegrid.setValidMask, self.weightsComputed', self.weightsComputed
         if self.weightsComputed:
             raise CDMSError, 'Must set mask before computing weights'
     
@@ -554,7 +553,6 @@ class Regrid:
         @note see setValidMask for the opposite definition 
         @note should be called before computing the weights
         """
-        print 'gsRegrid.setMask, self.weightsComputed', self.weightsComputed
         mask = None
         if hasattr(inDataOrMask, 'getmask'):
             # cdms2 variable
@@ -634,6 +632,9 @@ class Regrid:
                                         byref(src_dataid))
         catchError(status, sys._getframe().f_lineno)
         def setFillValue(data):
+            
+            if not hasattr(data, 'fill_value'):
+                data = numpy.ma.masked_array(data)
             if data.dtype == numpy.float64:
                 if data.fill_value is not None or \
                    data.missing_value is not None:
@@ -925,7 +926,6 @@ def testOuterProduct():
 
 
 def test():
-
     def func1(coords):
         return coords[0]*coords[1] + coords[2]
     def func2(coords):
@@ -986,9 +986,74 @@ def test():
     #print func(dst_coords)
     print 'error = ', error
 
+def testMasking():
+    import numpy.ma as ma
+
+    def func1(coords):
+        return coords[0]*coords[1]
+    def func2(coords):
+        return coords[0] * coords[1]
+
+    # source grid, tensor product of axes
+    src_x = ma.masked_array([1, 2, 3, 4, 5, 6], mask = [0,0,1,0,0,0])
+    src_y = ma.masked_array([10, 20, 30, 40, 50],mask = [0,0,0,1,0])
+
+    # destination grid, product of axes
+    dst_x = numpy.array([0.5,1,1.5, 2.0, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5,8.5,9.5])
+    dst_y = numpy.array([15., 20., 25., 30., 35.,40.,45.,50.,55])
+
+    # regridding constructor
+    rg = Regrid([src_x, src_y],
+                [dst_x, dst_y])
+
+    initialIndexGuess = numpy.array([0.0, 0.0, 0.0])
+    indices = rg._findIndices(numpy.array([1.5, 18.0, 140.0]),
+                              20, 1.e-2, initialIndexGuess)
+
+    # Mask needs to be set before weights are computed
+    mask = rg.getSrcGrid()[0] == 3
+    mask[:,3] = True
+    rg.setValidMask(mask)
+    rg.setMask(mask)
+    maxNumIters = 20
+    posTol = 1.e-2
+    rg.computeWeights(maxNumIters, posTol)
+
+    # number of valid points (some destination points may fall 
+    # outside the domain)
+    nvalid = rg.getNumValid()
+
+    # number of destination points
+    ndstpts = rg.getNumDstPoints()
+    print 'nvalid = ', nvalid, ' ndstpts = ', ndstpts
+
+    # get the indices and weights for a single target location
+    dst_indices = [4, 2, 1]
+    inds, weights = rg.getIndicesAndWeights(dst_indices)
+    print 'indices and weights are: ', inds, weights
+
+    # data
+    src_coords = rg.getSrcGrid()
+    dst_coords = rg.getDstGrid()
+    #print 'src_coords = ', src_coords
+    #print 'dst_coords = ', dst_coords
+    src_data = numpy.array( func1(src_coords), numpy.float32 )
+    dst_data = -numpy.ones( dst_coords[0].shape, numpy.float32 )
+
+    # regrid
+    rg(src_data, dst_data)
+    print 'after interp: dst_data =\n', dst_data
+
+    # check
+    error = numpy.sum(abs(dst_data - func1(dst_coords)))
+    #print dst_data
+    #print func(dst_coords)
+    print 'error = ', error
+
 if __name__ == '__main__':
     #testOuterProduct()
     test()
+    testMasking()
     #testMakeCyclic()
     #testHandleCut()
 
