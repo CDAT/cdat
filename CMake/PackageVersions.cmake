1375b3e288e6cc72c93f73fc3c4aeb7a3342bbe2
Alex Pletzer <pletzer@idefix.txcorp.com>
2012-04-30 17:36:58 -0600

    putting things into place, not yet tested

--- CMake/PackageVersions.cmake
+++ CMake/PackageVersions.cmake
@@ -418,7 +418,7 @@ set(MPI4PY_MAJOR 1)
 set(MPI4PY_MINOR 3)
 set(MPI4PY_VERSION ${MPI4PY_MAJOR}.${MPI4PY_MINOR})
 set(MPI4PY_URL http://uv-cdat.llnl.gov/cdat/resources)
-set(MPI4PY_GZ mpi4py-${LIBCF_VERSION}.tar.gz)
+set(MPI4PY_GZ mpi4py-${MPI4PY_VERSION}.tar.gz)
 set(MPI4PY_MD5 978472a1a71f3142c866c9463dec7103)
 
 set(ESMF_MAJOR 5)
