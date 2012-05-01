1898071b14ea5631a91c3951b67f45fb5c054f2a
Alex Pletzer <pletzer@idefix.txcorp.com>
2012-04-30 20:38:45 -0600

    + license file

--- Packages/distarray/Lib/mvDistArray.py
+++ Packages/distarray/Lib/mvDistArray.py
@@ -1,7 +1,7 @@
 #/usr/bin/env python
 
 """
-Copyright (c) 2008, Tech-X Corporation
+Copyright (c) 2008-2012, Tech-X Corporation
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
