eaeea541728f11c1e3cefc7473466eb115c4d285
Alex Pletzer <pletzer@idefix.txcorp.com>
2012-05-10 15:19:39 -0600

    now able to build ESMF with MPI enabled (need to set CDAT_BUILD_ESMF_PARALLEL=ON in cmake).
    I'm getting 3 errors
    
    	  8 - testESMP_3x4_6x8_Conserve (Failed)
    	 10 - testESMP_3x4_6x8_Conserve_Masked (Failed)
    	 11 - testESMP_3x4_6x8_Bilinear_Masked (Failed)

--- CMake/External_esmf.cmake
+++ CMake/External_esmf.cmake
@@ -1,8 +1,37 @@
 
 set(esmf_source "${CMAKE_CURRENT_BINARY_DIR}/build/esmf" CACHE INTERNAL "")
 set(esmf_install "${cdat_EXTERNALS}" CACHE INTERNAL "")
-set(esmf_comm "mpiuni")
 set(esmf_pthreads "OFF")
+
+# check if ESMF should be built in parallel
+set(emsf_enable_mpi FALSE)
+if(CDAT_BUILD_ESMF_PARALLEL)
+  set(emsf_enable_mpi TRUE)
+endif()
+if(NOT MPI_FOUND)
+  message("MPI was not found")
+  set(emsf_enable_mpi FALSE)
+endif()
+if("${emsf_enable_mpi}" AND "${MPI_CXX_COMPILER}" STREQUAL "MPI_CXX_COMPILER_NOTFOUND")
+  message("MPI C++ compiler was not found")
+  set(emsf_enable_mpi FALSE)
+endif()
+if("${emsf_enable_mpi}" AND "${MPI_Fortran_COMPILER}" STREQUAL "MPI_Fortran_COMPILER_NOTFOUND")
+  message("MPI Fortran compiler was not found")
+  set(emsf_enable_mpi FALSE)
+endif()
+if("${emsf_enable_mpi}" AND "${MPI_C_COMPILER}" STREQUAL "MPI_C_COMPILER_NOTFOUND")
+  message("MPI C compiler was not found")
+  set(emsf_enable_mpi FALSE)
+endif()
+if("${emsf_enable_mpi}")
+  # other possibilites are "mpich" "mpich2" "intelmpi" "lam" ...
+  message("Will build ESMF assuming openmpi")
+  set(esmf_comm "openmpi")
+else()
+  message("Will build ESMF serial")
+  set(esmf_comm "mpiuni")
+endif()
 
 configure_file(${cdat_CMAKE_SOURCE_DIR}/esmf_make_step.cmake.in
   ${cdat_CMAKE_BINARY_DIR}/esmf_make_step.cmake
