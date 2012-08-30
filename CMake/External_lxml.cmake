652c0d8d1c2fe9bfdb8e8cb7079de367a60811a5
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-08-30 01:47:51 -0400

    Fixed broken lxml build

--- CMake/External_lxml.cmake
+++ CMake/External_lxml.cmake
@@ -1,20 +1,26 @@
 # create an external project to install lxml,
 # and configure and build it
+set(LXML_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/lxml)
+set(LXML_BINARY_DIR ${LXML_SOURCE_DIR})
 
-include(@cdat_CMAKE_BINARY_DIR@/cdat_common_environment.cmake)
+configure_file(${cdat_CMAKE_SOURCE_DIR}/lxml_build_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/lxml_build_step.cmake @ONLY)
+configure_file(${cdat_CMAKE_SOURCE_DIR}/lxml_install_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/lxml_install_step.cmake @ONLY)
 
-set(lxml_source "${CMAKE_CURRENT_BINARY_DIR}/build/lxml")
+set(LXML_BUILD_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/lxml_build_step.cmake)
+set(LXML_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/lxml_install_step.cmake)
 
 ExternalProject_Add(lxml
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  SOURCE_DIR ${lxml_source}
+  SOURCE_DIR ${LXML_SOURCE_DIR}
   URL ${LXML_URL}/${LXML_GZ}
   URL_MD5 ${LXML_MD5}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-  INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
+  BUILD_COMMAND ${LXML_BUILD_COMMAND}
+  INSTALL_COMMAND ${LXML_INSTALL_COMMAND}
+ # INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
   DEPENDS ${lxml_DEPENDENCIES}
   ${EP_LOG_OPTIONS}
-  )
-
+ )
