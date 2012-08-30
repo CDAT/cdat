8c153a132c91bc8246b5c2bcb590c68a254e0050
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-08-30 01:12:23 -0400

    Fixed lxml build

--- CMake/External_lxml.cmake
+++ CMake/External_lxml.cmake
@@ -1,9 +1,12 @@
 # create an external project to install lxml,
 # and configure and build it
+configure_file(${cdat_CMAKE_SOURCE_DIR}/lxml_build_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/lxml_build_step.cmake)
+configure_file(${cdat_CMAKE_SOURCE_DIR}/lxml_install_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/lxml_install_step.cmake)
 
-include(@cdat_CMAKE_BINARY_DIR@/cdat_common_environment.cmake)
-
-set(lxml_source "${CMAKE_CURRENT_BINARY_DIR}/build/lxml")
+set(LXML_BUILD_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/lxml_build_step.cmake)
+set(LXML_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/lxml_install_step.cmake)
 
 ExternalProject_Add(lxml
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
@@ -12,9 +15,9 @@ ExternalProject_Add(lxml
   URL_MD5 ${LXML_MD5}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-  INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
+  BUILD_COMMAND ${LXML_BUILD_COMMAND}
+  INSTALL_COMMAND ${LXML_INSTALL_COMMAND}
+ # INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
   DEPENDS ${lxml_DEPENDENCIES}
   ${EP_LOG_OPTIONS}
   )
-
