f0e7826370ae9800192935ae76a04dcff25cddff
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-01 11:52:13 -0400

    Added missing version info for YASM

--- CMake/yasm_pkg.cmake
+++ CMake/yasm_pkg.cmake
@@ -1,2 +1,9 @@
+set(YASM_MAJOR_SRC 1)
+set(YASM_MINOR_SRC 2)
+set(YASM_PATCH_SRC 0)
+set(YASM_URL ${LLNL_URL})
+set(YASM_GZ yasm-${YASM_MAJOR_SRC}.${YASM_MINOR_SRC}.${YASM_PATCH_SRC}.tar.gz)
+set(YASM_MD5 4cfc0686cf5350dd1305c4d905eb55a6)
+
 add_cdat_package(YASM "" "" "" "")
 
