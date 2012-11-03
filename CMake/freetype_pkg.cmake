9aa60c3404658f44ce70c2e05d5739c6020c9413
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-03 01:39:27 -0400

    Fixed freetype

--- CMake/freetype_pkg.cmake
+++ CMake/freetype_pkg.cmake
@@ -1,1 +1,11 @@
+set(FT_MAJOR 9)
+set(FT_MINOR 7)
+set(FT_PATCH 3)
+set(FT_MAJOR_SRC 2)
+set(FT_MINOR_SRC 4)
+set(FT_PATCH_SRC 10)
+set(FT_URL ${LLNL_URL})
+set(FT_GZ freetype-${FT_MAJOR_SRC}.${FT_MINOR_SRC}.${FT_PATCH_SRC}.tar.gz)
+set(FT_MD5 4b1887901730ff2e12562ef30fa521d5)
+
 add_cdat_package(freetype "" "" "")
