aa2b3b5b846f3b67ce0f63d75d16a48ead0d5585
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-03 01:26:38 -0400

    Fixed PyTables build error

--- CMake/pytables_deps.cmake
+++ CMake/pytables_deps.cmake
@@ -1,1 +1,1 @@
-set(PyTable_deps ${pkgconfig_pkg} ${python_pkg} ${cython_pkg} ${numexpr_pkg} ${hdf5_pkg} ${libxml2_pkg} ${libxslt_pkg} ${zlib_pkg})
+set(PyTables_deps ${pkgconfig_pkg} ${python_pkg} ${cython_pkg} ${numexpr_pkg} ${hdf5_pkg} ${libxml2_pkg} ${libxslt_pkg} ${zlib_pkg})
