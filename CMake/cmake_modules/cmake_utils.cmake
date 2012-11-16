62d5735093ae74bffcc4d9cc88f109ea17f84013
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-16 15:06:45 -0500

    Force enable dependant package

--- CMake/cmake_modules/cmake_utils.cmake
+++ CMake/cmake_modules/cmake_utils.cmake
@@ -6,8 +6,8 @@ macro (add_cdat_package package_name version_string msg default)
   string(TOUPPER ${package_name} uc_package)
   string(TOLOWER ${package_name} lc_package)
   set(version)
-  set(message)
-  set(option_default)
+  set(message "Build ${package_name}")
+  set(option_default ON)
 
   # ARGV1 will be the version string
   if(NOT "" STREQUAL "${version_string}")
@@ -23,42 +23,33 @@ macro (add_cdat_package package_name version_string msg default)
   # ARGV3 (ON / OFF)
   if(NOT "" STREQUAL "${default}")
     set(option_default ${default})
-    message("[INFO] ${uc_package} is optional")
-  endif()
-
-  # Find system package first and if it exits provide an option to use
-  # system package
-  if(DEFINED version)
-    find_package(${package_name} ${version} QUIET)
-  else()
-    find_package(${package_name} QUIET)
   endif()
 
   # Check if package is optional, and if yes populate the GUI appropriately
-  if(DEFINED option_default)
-    option(CDAT_BUILD_${uc_package} ${message} ${option_default})
+  option(CDAT_BUILD_${uc_package} ${message} ${option_default})
+  mark_as_advanced(CDAT_BUILD_${uc_package})
+  if(NOT "" STREQUAL "${default}")
+    # Find system package first and if it exits provide an option to use
+    # system package
+    if(DEFINED version)
+      find_package(${package_name} ${version} QUIET)
+    else()
+      find_package(${package_name} QUIET)
+    endif()
+
+    mark_as_advanced(CLEAR CDAT_BUILD_${uc_package})
+    option(CDAT_USE_SYSTEM_${uc_package} "Use system installed ${lc_package}" OFF)
   endif()
 
-  option(CDAT_USE_SYSTEM_${uc_package} "Use system installed ${lc_package}" OFF)
   if(NOT ${package_name}_FOUND)
     mark_as_advanced(${package_name}_DIR)
-    mark_as_advanced(CDAT_USE_SYSTEM_${uc_package})
   endif()
 
   # Check if package is found, if not found or found but user prefers to use cdat package
   # then use cdat package or else use system package
   if(NOT CDAT_USE_SYSTEM_${uc_package})
-    if(DEFINED CDAT_BUILD_${uc_package} AND CDAT_BUILD_${uc_package})
-      message("[INFO] ${uc_package} will be build by the UVCDAT-superbuild")
-      list(APPEND external_packages "${package_name}")
-      message("external_packages are: ${external_packages}")
-      set(${lc_package}_pkg "${package_name}")
-    endif()
-    if(NOT DEFINED CDAT_BUILD_${uc_package})
-      message("[INFO] ${uc_package} will be build by the UVCDAT-superbuild")
       list(APPEND external_packages "${package_name}")
       set(${lc_package}_pkg "${package_name}")
-    endif()
   else()
     if(CDAT_USE_SYSTEM_${uc_package} AND ${uc_package}_FOUND)
       message("[INFO] Removing external package ${package_name}")
@@ -77,10 +68,23 @@ macro (add_cdat_package package_name version_string msg default)
         list(APPEND found_system_libraries ${lib_path})
         message("[INFO]  Linking: ${uc_package}_LIBRARY: ${lib_path}")
       endif()
-
     endif() # use system package
-
   endif()
+endmacro()
+
+macro(enable_cdat_package_deps package_name)
+  string(TOUPPER ${package_name} uc_package)
+  string(TOLOWER ${package_name} lc_package)
+
+  foreach(dep ${${package_name}_deps})
+    string(TOUPPER ${dep} uc_dep)
+    if(NOT CDAT_USE_SYSTEM_${uc_dep})
+      set(CDAT_BUILD_${uc_dep} ON CACHE BOOL "" FORCE)
+    endif()
+    if(NOT DEFINED CDAT_USE_SYSTEM_${uc_dep})
+      mark_as_advanced(CDAT_BUILD_${uc_dep})
+    endif()
+  endforeach()
 endmacro()
 
 # Disable a cdat package
