49d4b56339eeca0add37e2346c95d98c45fa4b39
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-19 16:56:12 -0500

    Make sure to reset the tag when switching branches

--- CMake/External_vistrails.cmake
+++ CMake/External_vistrails.cmake
@@ -11,12 +11,23 @@ endif()
 
 set(VISTRAILS_TAG_POINT ${vistrails_branch} CACHE STRING "Specify branch of vistrails to be used for UVCDAT")
 set(vistrails_url "${GIT_PROTOCOL}vistrails.org/git/vistrails.git")
+
 option(CDAT_DELETE_VISTRAILS_HISTORY "Delete GIT history of vistrails" OFF)
+option(CDAT_AUTO_UPDATE_VISTRAILS_TAG_POINT "Delete GIT history of vistrails" ON)
 
 # FIXME: Workaround. For some reason just using GIT_* to clone breaks the superbuild.
 set(vistrails_install_command ${GIT_EXECUTABLE} clone --depth 1 -b ${VISTRAILS_TAG_POINT}  ${vistrails_url})
 if(EXISTS ${CMAKE_INSTALL_PREFIX}/vistrails/.git)
-  set(vistrails_install_command ${GIT_EXECUTABLE} pull ${VISTRAILS_TAG_POINT})
+  if(CDAT_AUTO_UPDATE_VISTRAILS_TAG_POINT)
+    set(VISTRAILS_TAG_POINT ${vistrails_branch} CACHE STRING "" FORCE)
+  endif()
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} fetch ${vistrails_url}
+    COMMAND ${GIT_EXECUTABLE} checkout ${VISTRAILS_TAG_POINT}
+    WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/vistrails
+  )
+
+  set(vistrails_install_command ${GIT_EXECUTABLE} --git-dir=${CMAKE_INSTALL_PREFIX}/vistrails/.git pull)
 endif()
 
 ExternalProject_Add(vistrails
