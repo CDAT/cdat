1e611da4db9b99d2abf302b959d83aec30d4c352
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-23 16:06:36 -0500

    Execute python test in a way to avoid source tree pollution

--- CMake/install.py
+++ CMake/install.py
@@ -1,11 +1,10 @@
-from test_python_ok import *
-
 import os
 
 # The main installation script is installation/install.py
 # However, we need to first check for problems using 1.5.2 syntax only.
-
 current_dir = os.path.dirname(__file__)
-install_script_path = os.path.join(current_dir, '..', 'installation', 'install.py')
 
+execfile(os.path.join(current_dir, 'test_python_ok.py'))
+
+install_script_path = os.path.join(current_dir, '..', 'installation', 'install.py')
 execfile(install_script_path)
