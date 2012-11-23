ebd6983d3e8ac1a5d08156ac7da3424e7103061c
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-23 13:51:24 -0500

    Build gui_support out of source as well

--- CMake/cdat_modules/gui_support_external.cmake
+++ CMake/cdat_modules/gui_support_external.cmake
@@ -1,17 +1,17 @@
-
-set(gui_support_source ${cdat_SOURCE_DIR}/Packages/gui_support)
+set(gui_support_source_dir "${cdat_SOURCE_DIR}/Packages/gui_support")
+set(gui_support_binary_dir "${CMAKE_CURRENT_BINARY_DIR}/build/gui_support-build")
 set(runtime_library_path ${CMAKE_INSTALL_PREFIX}/lib:${cdat_EXTERNALS}/lib)
-
-
 
 #  BUILD_COMMAND env EXTERNALS=${cdat_EXTERNALS} ${LIBRARY_PATH}=${runtime_library_path} ${PYTHON_EXECUTABLE} setup.py build
 #  INSTALL_COMMAND env EXTERNALS=${cdat_EXTERNALS} ${LIBRARY_PATH}=${runtime_library_path} ${PYTHON_EXECUTABLE} setup.py install --prefix=${CMAKE_INSTALL_PREFIX}
 ExternalProject_Add(gui_support
   DOWNLOAD_DIR ""
-  SOURCE_DIR ${gui_support_source}
-  BUILD_IN_SOURCE 1
-  BUILD_COMMAND env PYTHONPATH=$ENV{PYTHONPATH} LD_LIBRARY_PATH=$ENV{LD_LIBRARY_PATH} EXTERNALS=${cdat_EXTERNALS}  ${PYTHON_EXECUTABLE} setup.py build
-  INSTALL_COMMAND env PYTHONPATH=$ENV{PYTHONPATH} LD_LIBRARY_PATH=$ENV{LD_LIBRARY_PATH} EXTERNALS=${cdat_EXTERNALS} ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
+  SOURCE_DIR ${gui_support_source_dir}
+  BINARY_DIR ${gui_support_binary_dir}
+  BUILD_IN_SOURCE 0
+  BUILD_COMMAND ""
+#  BUILD_COMMAND env PYTHONPATH=$ENV{PYTHONPATH} LD_LIBRARY_PATH=$ENV{LD_LIBRARY_PATH} EXTERNALS=${cdat_EXTERNALS}  ${PYTHON_EXECUTABLE} ${gui_support_source_dir}/setup.py build -b ${gui_support_binary_dir}
+  INSTALL_COMMAND env PYTHONPATH=$ENV{PYTHONPATH} LD_LIBRARY_PATH=$ENV{LD_LIBRARY_PATH} EXTERNALS=${cdat_EXTERNALS} ${PYTHON_EXECUTABLE} ${gui_support_source_dir}/setup.py build -b ${gui_support_binary_dir} install ${PYTHON_EXTRA_PREFIX}
   PATCH_COMMAND ""
   CONFIGURE_COMMAND ""
   DEPENDS ${gui_support_deps}
