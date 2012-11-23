a328d3e563eec14c6aabcfee979668ffdd9f7e40
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-23 13:11:35 -0500

    Build CMOR package in the build directory

--- CMake/cdat_modules/cmor_external.cmake
+++ CMake/cdat_modules/cmor_external.cmake
@@ -1,15 +1,17 @@
-set(cmor_source "${cdat_SOURCE_DIR}/Packages/cmor")
-set(cmor_install "${cdat_EXTERNALS}")
+set(cmor_source_dir "${cdat_SOURCE_DIR}/Packages/cmor")
+set(cmor_binary_dir  "${CMAKE_CURRENT_BINARY_DIR}/build/CMOR-build")
+set(cmor_install_dir "${cdat_EXTERNALS}")
 
 set(cmor_configure_args --with-netcdf=${netcdf_install} --with-udunits2=${udunits_install} --with-uuid=${uuid_install} --with-zlib=${zlib_install} --enable-fortran=no)
 
 # it appears currently we only configure cmor but not build it.
 ExternalProject_Add(CMOR
-  SOURCE_DIR ${cmor_source}
-  INSTALL_DIR ${cmor_install}
-  BUILD_IN_SOURCE 1
+  SOURCE_DIR ${cmor_source_dir}
+  BINARY_DIR ${cmor_binary_dir}
+  INSTALL_DIR ${cmor_install_dir}
+  BUILD_IN_SOURCE 0
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND sh configure --prefix=<INSTALL_DIR> ${cmor_configure_args}
+  CONFIGURE_COMMAND sh ${cmor_source_dir}/configure --prefix=<INSTALL_DIR> ${cmor_configure_args}
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
   DEPENDS ${CMOR_deps}
