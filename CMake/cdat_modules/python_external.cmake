c08b23fe56076e9153aeeea8279c43b00337aac6
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-27 01:29:07 -0500

    Fixed python run-time error on ubuntu 12.04

--- CMake/cdat_modules/python_external.cmake
+++ CMake/cdat_modules/python_external.cmake
@@ -1,53 +1,53 @@
 #-----------------------------------------------------------------------------
 set(proj Python)
 
-  set(python_SOURCE_DIR ${cdat_BINARY_DIR}/build/Python)
-  set(python_BUILD_IN_SOURCE 1)
+set(python_SOURCE_DIR ${cdat_BINARY_DIR}/build/Python)
+set(python_BUILD_IN_SOURCE 1)
 
-  set(python_aqua_cdat no)
+set(python_aqua_cdat no)
 
-  configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/python_patch_step.cmake.in
-    ${cdat_CMAKE_BINARY_DIR}/python_patch_step.cmake
-    @ONLY)
+configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/python_patch_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/python_patch_step.cmake
+  @ONLY)
 
-  configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/python_configure_step.cmake.in
-    ${cdat_CMAKE_BINARY_DIR}/python_configure_step.cmake
-    @ONLY)
+configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/python_configure_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/python_configure_step.cmake
+  @ONLY)
 
-  configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/python_make_step.cmake.in
-    ${cdat_CMAKE_BINARY_DIR}/python_make_step.cmake
-    @ONLY)
+configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/python_make_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/python_make_step.cmake
+  @ONLY)
 
-  configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/python_install_step.cmake.in
-    ${cdat_CMAKE_BINARY_DIR}/python_install_step.cmake
-    @ONLY)
+configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/python_install_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/python_install_step.cmake
+  @ONLY)
 
-  set(python_PATCH_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_patch_step.cmake)
+set(python_PATCH_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_patch_step.cmake)
 
- if(APPLE)
-    set(python_CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_configure_step.cmake)
-    set(python_BUILD_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_make_step.cmake)
-    set(python_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_install_step.cmake)
-  else()
-    set(python_CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_configure_step.cmake)
-    set(python_BUILD_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_make_step.cmake)
-    set(python_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_install_step.cmake)
-  endif()
+if(APPLE)
+  set(python_CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_configure_step.cmake)
+  set(python_BUILD_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_make_step.cmake)
+  set(python_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_install_step.cmake)
+else()
+  set(python_CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_configure_step.cmake)
+  set(python_BUILD_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_make_step.cmake)
+  set(python_INSTALL_COMMAND ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/python_install_step.cmake)
+endif()
 
-  ExternalProject_Add(${proj}
-    URL ${PYTHON_URL}/${PYTHON_GZ}
-    URL_MD5 ${PYTHON_MD5}
-    DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-    SOURCE_DIR ${python_SOURCE_DIR}
-    BUILD_IN_SOURCE ${python_BUILD_IN_SOURCE}
-    UPDATE_COMMAND pwd
-    PATCH_COMMAND ${python_PATCH_COMMAND}
-    CONFIGURE_COMMAND ${python_CONFIGURE_COMMAND}
-    BUILD_COMMAND ${python_BUILD_COMMAND}
-    INSTALL_COMMAND ${python_INSTALL_COMMAND}
-    DEPENDS ${Python_deps}
-    ${ep_log_options}
-    )
+ExternalProject_Add(${proj}
+  URL ${PYTHON_URL}/${PYTHON_GZ}
+  URL_MD5 ${PYTHON_MD5}
+  DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
+  SOURCE_DIR ${python_SOURCE_DIR}
+  BUILD_IN_SOURCE ${python_BUILD_IN_SOURCE}
+  UPDATE_COMMAND pwd
+  PATCH_COMMAND ${python_PATCH_COMMAND}
+  CONFIGURE_COMMAND ${python_CONFIGURE_COMMAND}
+  BUILD_COMMAND ${python_BUILD_COMMAND}
+  INSTALL_COMMAND ${python_INSTALL_COMMAND}
+  DEPENDS ${Python_deps}
+  ${ep_log_options}
+)
 
 #-----------------------------------------------------------------------------
 # Set PYTHON_INCLUDE and PYTHON_LIBRARY variables
