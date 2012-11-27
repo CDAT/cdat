f5f1cbd543aef55ea283c6d6ae1a6f59c6a50a80
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-27 17:00:23 -0500

    Fixing matplotlib PNG runtime error

--- CMake/cdat_modules/matplotlib_deps.cmake
+++ CMake/cdat_modules/matplotlib_deps.cmake
@@ -1,1 +1,1 @@
-set(Matplotlib_deps ${pyqt_pkg} ${cairo_pkg} ${numpy_pkg})
+set(Matplotlib_deps ${pyqt_pkg} ${cairo_pkg} ${numpy_pkg} ${png_pkg})
--- CMake/cdat_modules/matplotlib_external.cmake
+++ CMake/cdat_modules/matplotlib_external.cmake
@@ -1,17 +1,29 @@
 
 # Matplotlib
 #
-set(Matplotlib_source "${CMAKE_CURRENT_BINARY_DIR}/build/Matplotlib")
+set(matplotlib_source_dir "${CMAKE_CURRENT_BINARY_DIR}/build/Matplotlib")
+
+configure_file(
+  "${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/matplotlib_patch_step.cmake.in"
+  "${cdat_CMAKE_BINARY_DIR}/matplotlib_patch_step.cmake"
+  @ONLY
+)
+
+set(matplotlib_patch_command ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/matplotlib_patch_step.cmake)
+
+# Configuration specific
+set(INSTALL_DIR "${CMAKE_INSTALL_PREFIX}")
 
 ExternalProject_Add(Matplotlib
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  SOURCE_DIR ${Matplotlib_source}
+  SOURCE_DIR ${Matplotlib_SOURCE_DIR}
   URL ${MATPLOTLIB_URL}/${MATPLOTLIB_GZ}
   URL_MD5 ${MATPLOTLIB_MD5}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
+  PATCH_COMMAND ${matplotlib_patch_command}
   BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
   INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
   DEPENDS ${Matplotlib_deps}
   ${ep_log_options}
-  )
+)
