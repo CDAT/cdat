7c080c264ab586e2bbe063d8cce3599c8ffb442a
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-11-27 20:26:56 -0500

    Attempt to fix matplotlib png runtime error

--- CMake/cdat_modules/matplotlib_external.cmake
+++ CMake/cdat_modules/matplotlib_external.cmake
@@ -1,4 +1,3 @@
-
 # Matplotlib
 #
 set(matplotlib_source_dir "${CMAKE_CURRENT_BINARY_DIR}/build/Matplotlib")
@@ -9,21 +8,25 @@ configure_file(
   @ONLY
 )
 
-set(matplotlib_patch_command ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/matplotlib_patch_step.cmake)
+configure_file(
+  "${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/matplotlib_build_step.cmake.in"
+  "${cdat_CMAKE_BINARY_DIR}/matplotlib_build_step.cmake"
+  @ONLY
+)
 
-# Configuration specific
-set(INSTALL_DIR "${CMAKE_INSTALL_PREFIX}")
+set(matplotlib_patch_command ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/matplotlib_patch_step.cmake)
+set(matplotlib_build_command ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/matplotlib_build_step.cmake)
 
 ExternalProject_Add(Matplotlib
   DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-  SOURCE_DIR ${Matplotlib_SOURCE_DIR}
+  SOURCE_DIR ${matplotlib_source_dir}
   URL ${MATPLOTLIB_URL}/${MATPLOTLIB_GZ}
   URL_MD5 ${MATPLOTLIB_MD5}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
   PATCH_COMMAND ${matplotlib_patch_command}
-  BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-  INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
+  BUILD_COMMAND ${matplotlib_build_command}
+  INSTALL_COMMAND ""
   DEPENDS ${Matplotlib_deps}
   ${ep_log_options}
 )
