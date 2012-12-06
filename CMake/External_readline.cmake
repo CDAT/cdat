ad1f6a237ef380c166376ef01364138657eb57f7
Dave DeMarle <dave.demarle@kitware.com>
2012-12-06 14:45:36 -0500

    fix readline compilation on mac
    without this patch we get compilation error on lion

--- CMake/External_readline.cmake
+++ CMake/External_readline.cmake
@@ -11,7 +11,7 @@ ExternalProject_Add(readline
   URL ${READLINE_URL}/${READLINE_GZ}
   URL_MD5 ${READLINE_MD5}
   BUILD_IN_SOURCE 1
-  PATCH_COMMAND ""
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${cdat_external_patch_dir}/src/readline/shobj-conf ${readline_source}/support/shobj-conf
   CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCONFIGURE_ARGS=${readline_conf_args} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
   DEPENDS ${readline_DEPENDENCIES}
   ${EP_LOG_OPTIONS}
