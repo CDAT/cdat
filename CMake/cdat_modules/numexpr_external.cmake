8ff0480f2ea4971291014ba13e03c6942cf33bf3
Charles Doutriaux <doutriaux1@llnl.gov>
2013-01-10 16:01:27 -0800

    Got it to build out of box on hopper LD_LIBRARY_PATH now correctly setup on easy_install (at least for hopper)

--- CMake/cdat_modules/numexpr_external.cmake
+++ CMake/cdat_modules/numexpr_external.cmake
@@ -1,4 +1,4 @@
-include(@cdat_CMAKE_BINARY_DIR@/cdat_common_environment.cmake)
+include(${cdat_CMAKE_BINARY_DIR}/cdat_common_environment.cmake)
 
 ExternalProject_Add(Numexpr
   DOWNLOAD_COMMAND ""
@@ -6,7 +6,7 @@ ExternalProject_Add(Numexpr
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND ${EASY_INSTALL_BINARY} Numexpr==${NUMEXPR_VERSION}
+  INSTALL_COMMAND env "LD_LIBRARY_PATH=$ENV{LD_LIBRARY_PATH}" ${EASY_INSTALL_BINARY} Numexpr==${NUMEXPR_VERSION}
   DEPENDS ${Numexpr_deps}
   ${ep_log_options}
 )
