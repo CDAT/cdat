23f63ab2d9a9525226b6b2d65b644dfba7d5aae0
Charles Doutriaux <doutriaux1@llnl.gov>
2013-06-18 18:26:50 -0700

    Ok got the fetching going

--- CMake/cdat_modules/gui_support_pkg.cmake
+++ CMake/cdat_modules/gui_support_pkg.cmake
@@ -1,4 +1,5 @@
-SET(GUI_SUPPORT_SOURCE N/A)
-SET(GUI_SUPPORT_VERSION N/A)
+set(GUI_SUPPORT_SOURCE N/A)
+set(GUI_SUPPORT_VERSION N/A)
+set(GUI_SUPPORT_MD5 N/A)
 
 add_cdat_package_dependent(gui_support "" "" ON "CDAT_BUILD_GUI" OFF)
--- CMake/cdat_modules/lepl_pkg.cmake
+++ CMake/cdat_modules/lepl_pkg.cmake
@@ -5,7 +5,7 @@ set(lepl_PATCH_SRC 3)
 set (nm lepl)
 string(TOUPPER ${nm} uc_nm)
 set(${uc_nm}_VERSION ${${nm}_MAJOR_SRC}.${${nm}_MINOR_SRC}.${${nm}_PATCH_SRC})
-set(LEPL_SOURCE ${LLNL_URL}/LEPL_${LEPL_VERSION}.tar.gz)
+set(LEPL_SOURCE ${LLNL_URL}/LEPL-${LEPL_VERSION}.tar.gz)
 set(LEPL_MD5 5f653984c57ad8efad828c5153660743 )
 
 add_cdat_package_dependent(lepl "" "" ON "CDAT_BUILD_GUI" OFF)
