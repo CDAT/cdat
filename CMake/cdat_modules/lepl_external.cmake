b6cd31505a2ee9449e5533d40cd041d13b0f8dba
Charles Doutriaux <doutriaux1@llnl.gov>
2013-06-20 12:16:23 -0700

    Ok I think we are ready for pass 1...

--- CMake/cdat_modules/lepl_external.cmake
+++ CMake/cdat_modules/lepl_external.cmake
@@ -2,14 +2,18 @@
 # and configure and build it
 
 include(@cdat_CMAKE_BINARY_DIR@/cdat_common_environment.cmake)
+if (INTERNET_ACCESS STREQUAL "ON") 
+    set(EGG_GZ )
+else ()
+    set(EGG_GZ ${CDAT_PACKAGE_CACHE_DIR}/${LEPL_GZ})
+endif()
 
 ExternalProject_Add(lepl
-  DOWNLOAD_COMMAND ""
-  WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}
+    DOWNLOAD_COMMAND ""
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND ${EGG_CMD} lepl==${LEPL_VERSION}
+  INSTALL_COMMAND ${EGG_CMD} lepl==${LEPL_VERSION} ${EGG_GZ}
   DEPENDS ${lepl_deps}
   ${ep_log_options}
   )
--- CMake/cdat_modules/sphinx_external.cmake
+++ CMake/cdat_modules/sphinx_external.cmake
@@ -2,13 +2,18 @@
 # and configure and build it
 
 include(@cdat_CMAKE_BINARY_DIR@/cdat_common_environment.cmake)
+if (INTERNET_ACCESS STREQUAL "ON") 
+    set(EGG_GZ )
+else ()
+    set(EGG_GZ ${CDAT_PACKAGE_CACHE_DIR}/${SPHINX_GZ})
+endif()
 ExternalProject_Add(Sphinx
   DOWNLOAD_COMMAND ""
   WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND ${EGG_CMD} Sphinx==${SPHINX_VERSION}
+  INSTALL_COMMAND ${EGG_CMD} Sphinx==${SPHINX_VERSION} ${EGG_GZ}
   DEPENDS ${Sphinx_deps}
   ${ep_log_options}
   )
