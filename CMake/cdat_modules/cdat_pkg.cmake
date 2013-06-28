d4f1be430aa39bccd9c781687137d602028bbf89
Charles Doutriaux <doutriaux1@llnl.gov>
2013-06-28 11:01:23 -0700

    first pass at better version name

--- CMake/cdat_modules/cdat_pkg.cmake
+++ CMake/cdat_modules/cdat_pkg.cmake
@@ -2,11 +2,24 @@ set(cdat_VERSION_MAJOR 1)
 set(cdat_VERSION_MINOR 3)
 set(cdat_VERSION_PATCH 1)
 set(cdat_VERSION ${cdat_VERSION_MAJOR}.${cdat_VERSION_MINOR}.${cdat_VERSION_PATCH})
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --tags
+    WORKING_DIRECTORY ${cdat_SOURCE_DIR}
+    RESULT_VARIABLE res
+    OUTPUT_VARIABLE ver
+    )
+
+if (NOT ${res} EQUAL 0) 
+    set(cdat_VERSION ${cdat_VERSION_MAJOR}.${cdat_VERSION_MINOR}.${cdat_VERSION_PATCH})
+    message("Couldn't get version from git setting from values in cdat_pkg.cmake ${cdat_VERSION}")
+else ()
+    set(cdat_VERSION ${ver})
+endif()
 
 # configure version file
 set (nm cdat_VERSION)
 string(TOUPPER ${nm} uc_nm)
-set(${uc_nm}_VERSION ${${nm}_MAJOR}.${${nm}_MINOR}.${${nm}_PATCH})
+set(${uc_nm}_VERSION ${cdat_VERSION})
 set(CDAT_VERSION ${CDAT_VERSION_VERSION})
 
 configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/version.in
