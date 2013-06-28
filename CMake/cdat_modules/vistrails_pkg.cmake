7618285e56588ff63db59d425629fae4a4df8302
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-06-28 12:10:39 -0400

    Fixed vistrails url

--- CMake/cdat_modules/vistrails_pkg.cmake
+++ CMake/cdat_modules/vistrails_pkg.cmake
@@ -1,5 +1,6 @@
 set(VISTRAILS_VERSION ${VISTRAILS_TAG_POINT})
-set(VISTRAILS_SOURCE git://vistrails.org/vistrails.git)
+set(VISTRAILS_SOURCE "${GIT_PROTOCOL}vistrails.org/git/vistrails.git")
+
 ## Now tarball so faking md5 to tell checkout point
 set(VISTRAILS_MD5 uvcdat-master)
 add_cdat_package_dependent(vistrails "" "" ON "CDAT_BUILD_GUI" OFF)
