6283ed20fb4ecd1482e14cd831f3e74d8ffa5a22
Remi Rampin <remirampin@gmail.com>
2013-07-09 15:06:57 -0400

    Handles cdms2.createVariable() with non-tuple axes
    If a single value is passed as the axes parameter (for instance, because
    we forgot the trailing comma), a one-element tuple is assumed.

--- Packages/cdms2/Lib/tvariable.py
+++ Packages/cdms2/Lib/tvariable.py
@@ -270,6 +270,8 @@ class TransientVariable(AbstractVariable,numpy.ma.MaskedArray):
         self.__domain = [None]*self.rank()
         if axes is not None:
             flataxes = []
+            if not isinstance(axes, (list, tuple)):
+                axes = (axes,)
             for item in axes:
                 if isinstance(item, AbstractAxis) or item is None:
                     flataxes.append(item)
