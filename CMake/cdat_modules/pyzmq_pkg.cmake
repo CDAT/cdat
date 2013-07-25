1d7978073037e954e3da15d0375216581ac86efa
Charles Doutriaux <doutriaux1@llnl.gov>
2013-07-25 14:25:17 -0700

    Fixed error message to say pyzmq instead of mpi4py, source from llnl

--- CMake/cdat_modules/pyzmq_pkg.cmake
+++ CMake/cdat_modules/pyzmq_pkg.cmake
@@ -1,3 +1,6 @@
-set(PYZMQ_VERSION 13.0.2)
+set(PYZMQ_VERSION 13.1.0)
+set(PYZMQ_URL ${LLNL_URL})
+set(PYZMQ_GZ pyzmq-${PYZMQ_VERSION}.tar.gz)
+set(PYZMQ_SOURCE ${PYZMQ_URL}/${PYZMQ_GZ})
 
 add_cdat_package(pyzmq "" "" "")
