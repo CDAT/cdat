393386386339aa7be5b531795b823aa5435b331f
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-08-02 23:16:22 -0400

    Fixed various issues with merge

--- CMake/cdat_modules/curl_pkg.cmake
+++ CMake/cdat_modules/curl_pkg.cmake
@@ -7,10 +7,7 @@ set(CURL_PATCH_SRC 0)
 set(CURL_URL ${LLNL_URL})
 set(CURL_GZ curl-${CURL_MAJOR_SRC}.${CURL_MINOR_SRC}.${CURL_PATCH_SRC}.tar.gz)
 set(CURL_MD5 e8c36126d21c893f085aca233906b5cc)
-
-set (nm CURL)
-string(TOUPPER ${nm} uc_nm)
-set(${uc_nm}_VERSION ${${nm}_MAJOR_SRC}.${${nm}_MINOR_SRC}.${${nm}_PATCH_SRC})
-set(${uc_nm}_SOURCE ${${nm}_URL}/${${nm}_GZ})
+set(CURL_VERSION ${CURL_MAJOR_SRC}.${CURL_MINOR_SRC}.${CURL_PATCH_SRC})
+set(CURL_SOURCE ${CURL_URL}/${CURL_GZ})
 
 add_cdat_package(CURL "" "" "")
