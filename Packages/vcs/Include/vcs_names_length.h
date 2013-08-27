f8be82b1d77c3c10ce002802fce82765b03773fd
Charles Doutriaux <doutriaux1@llnl.gov>
2013-08-27 15:45:46 -0700

    Seems to work

--- Packages/vcs/Include/vcs_names_length.h
+++ Packages/vcs/Include/vcs_names_length.h
@@ -1,4 +1,6 @@
 #define VCS_MX_NM_LEN 256
-const char DOT_DIRECTORY[VCS_MX_NM_LEN] = ".uvcdat";
-const char DOT_DIRECTORY_ENV[VCS_MX_NM_LEN] = "UVCDAT_DIR";
+#ifndef DOT_DIRECTORY
+#define DOT_DIRECTORY ".uvcdat"
+#define DOT_DIRECTORY_ENV "UVCDAT_DIR"
+#endif
 
