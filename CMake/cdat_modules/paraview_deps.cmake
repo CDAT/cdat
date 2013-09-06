e4eeac376bac231b1e55aecbd0b58f7d0ac630a0
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-09-06 14:17:59 -0400

    Qt is only needed when ParaView is built with GUI support

--- CMake/cdat_modules/paraview_deps.cmake
+++ CMake/cdat_modules/paraview_deps.cmake
@@ -1,4 +1,8 @@
-set(ParaView_deps ${pkgconfig_pkg} ${python_pkg} ${hdf5_pkg} ${png_pkg} ${jpeg_pkg} ${libxml2_pkg} ${qt_pkg} ${freetype_pkg} ${netcdfplus_pkg} ${zlib_pkg} ${r_pkg})
+set(ParaView_deps ${pkgconfig_pkg} ${python_pkg} ${hdf5_pkg} ${png_pkg} ${jpeg_pkg} ${libxml2_pkg} ${freetype_pkg} ${netcdfplus_pkg} ${zlib_pkg} ${r_pkg})
+
+if (NOT CDAT_BUILD_GUI)
+  list(APPEND ParaView_deps ${qt_pkg})
+endif()
 
 if(CDAT_BUILD_PARALLEL)
   list(APPEND ParaView_deps "${mpi_pkg}")
