336d3322972ccf9669763f680d229ad84c2eeb39
Charles Doutriaux <doutriaux1@llnl.gov>
2013-09-20 17:26:27 -0700

    Fixed download data, was mistakenly turned off is NOT building ONLINE, switch to the other way around, if building offline, then no sample data Also now get WGET executable at conf time, used to get it from checked_get but that is written before we know if WGET is here so it was wrong on system w/o it

--- CMake/cdat_modules/data_pkg.cmake
+++ CMake/cdat_modules/data_pkg.cmake
@@ -1,10 +1,12 @@
 # Do we download the data ?
 option(CDAT_DOWNLOAD_SAMPLE_DATA "Download sample data" ON)
 if (NOT CDAT_BUILD_WO_ESGF)
+    message([INFO] ESGF MAKES US TURN DATA OFF)
     set(CDAT_DOWNLOAD_SAMPLE_DATA OFF)
 endif()
 
-if (NOT OFFLINE_BUILD)
+if (OFFLINE_BUILD)
+    message([INFO] OFFLINE MAKES US TURN DATA OFF)
     set(CDAT_DOWNLOAD_SAMPLE_DATA OFF)
 endif()
 
