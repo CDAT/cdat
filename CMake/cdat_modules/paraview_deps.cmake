07b40eace6eedf5d4ec9e9b376b38bbcd51df5ff
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-09-21 00:35:02 -0400

    Added missing r dependency

--- CMake/cdat_modules/paraview_deps.cmake
+++ CMake/cdat_modules/paraview_deps.cmake
@@ -1,4 +1,4 @@
-set(ParaView_deps ${pkgconfig_pkg} ${python_pkg} ${hdf5_pkg} ${png_pkg} ${jpeg_pkg} ${libxml2_pkg} ${freetype_pkg} ${netcdfplus_pkg} ${zlib_pkg})
+set(ParaView_deps ${pkgconfig_pkg} ${python_pkg} ${hdf5_pkg} ${png_pkg} ${jpeg_pkg} ${libxml2_pkg} ${freetype_pkg} ${netcdfplus_pkg} ${zlib_pkg} ${r_pkg})
 
 if (NOT CDAT_BUILD_GUI)
   list(APPEND ParaView_deps ${qt_pkg})
