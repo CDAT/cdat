349a473d7a9b7f11691bb9f3ce87c36002ece3ed
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-09-23 20:03:04 -0400

    Fixed cmake error

--- CMake/cdat_modules/data_pkg.cmake
+++ CMake/cdat_modules/data_pkg.cmake
@@ -1,12 +1,12 @@
 # Do we download the data ?
 option(CDAT_DOWNLOAD_SAMPLE_DATA "Download sample data" ON)
 if (NOT CDAT_BUILD_WO_ESGF)
-    message([INFO] ESGF MAKES US TURN DATA OFF)
+    message("[INFO] Disabling download data for ESGF")
     set(CDAT_DOWNLOAD_SAMPLE_DATA OFF)
 endif()
 
 if (OFFLINE_BUILD)
-    message([INFO] OFFLINE MAKES US TURN DATA OFF)
+    message("[INFO] Disabling download data for offline build")
     set(CDAT_DOWNLOAD_SAMPLE_DATA OFF)
 endif()
 
