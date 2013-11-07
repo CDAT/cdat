0631f855d4e59acc5666284c560e457a6c02fe17
Charles Doutriaux <doutriaux1@llnl.gov>
2013-11-06 17:54:40 -0800

    hdf5 patch for ubuntu 1310, more fixes wil go in

--- CMake/cdat_modules/hdf5_external.cmake
+++ CMake/cdat_modules/hdf5_external.cmake
@@ -2,6 +2,10 @@
 set(HDF5_source "${CMAKE_CURRENT_BINARY_DIR}/build/HDF5")
 set(HDF5_install "${cdat_EXTERNALS}")
 
+configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/hdf5_patch_step.cmake.in
+  ${cdat_CMAKE_BINARY_DIR}/hdf5_patch_step.cmake
+  @ONLY)
+
 # we disable HDF5 warnings because it has way too many of them.
 ExternalProject_Add(HDF5
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
@@ -10,7 +14,7 @@ ExternalProject_Add(HDF5
   URL ${HDF5_URL}/${HDF5_GZ}
   URL_MD5 ${HDF5_MD5}
   BUILD_IN_SOURCE 1
-  PATCH_COMMAND ""
+  PATCH_COMMAND ${CMAKE_COMMAND} -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/hdf5_patch_step.cmake 
   CONFIGURE_COMMAND ${CMAKE_COMMAND} -DADDITIONAL_CFLAGS=-w -DADDITIONAL_CPPFPAGS=-w -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
   LOG_BUILD 1
   DEPENDS ${HDF5_deps}
