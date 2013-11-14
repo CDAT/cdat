73036cffbf37e63381d59c0d7c730197ad873c5d
Elo Leung <leung25@llnl.gov>
2013-11-14 15:43:54 -0800

    Revert "Enable libxml2 python"
    This reverts commit 4132dbd278c7e4eee4caf957ba003b9d602c655e.

--- CMake/cdat_modules/libxml2_external.cmake
+++ CMake/cdat_modules/libxml2_external.cmake
@@ -2,11 +2,6 @@
 set(libXML2_source "${CMAKE_CURRENT_BINARY_DIR}/build/libXML2")
 set(libXML2_install "${cdat_EXTERNALS}")
 
-configure_file(${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/libxml2_configure_step.cmake.in
-    ${cdat_CMAKE_BINARY_DIR}/libxml2_configure_step.cmake
-    @ONLY
-)
-
 ExternalProject_Add(libXML2
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
   SOURCE_DIR ${libXML2_source}
@@ -15,7 +10,7 @@ ExternalProject_Add(libXML2
   URL_MD5 ${XML_MD5}
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/libxml2_configure_step.cmake
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
   DEPENDS ${libXML2_deps}
   ${ep_log_options}
 )
