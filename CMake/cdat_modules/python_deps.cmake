3b5c955a624d0696cfa24c3e5823ac52d4135958
Charles Doutriaux <doutriaux1@llnl.gov>
2013-11-19 15:17:59 -0800

    since we removed the build of tcltk which depended on readline, python does not depend on readline and gets built w/o readline support. Fixed

--- CMake/cdat_modules/python_deps.cmake
+++ CMake/cdat_modules/python_deps.cmake
@@ -1,1 +1,2 @@
-set(Python_deps ${pkgconfig_pkg} ${tcltk_pkg} ${hdf5_pkg} ${libxml2_pkg} ${libxslt_pkg} ${zlib_pkg})
+set(Python_deps ${pkgconfig_pkg} ${tcltk_pkg} ${hdf5_pkg} ${libxml2_pkg} ${libxslt_pkg} ${zlib_pkg} ${readline_pkg})
+
