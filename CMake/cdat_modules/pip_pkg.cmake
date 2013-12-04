98dc8c44323a8898de5d1796ec11d8fe2ebd71ae
Charles Doutriaux <doutriaux1@llnl.gov>
2013-12-04 13:36:13 -0800

    PYTHONUSERBASE is now CMAKE_INSTALL_PREFIX installing in Externals led to subsequent packages not finding pip installed modules, because PYTHONPATH not set properly. And it didn't make sense to spread the python packages in so many places

--- CMake/cdat_modules/pip_pkg.cmake
+++ CMake/cdat_modules/pip_pkg.cmake
@@ -1,5 +1,5 @@
 set(PIP_MAJOR_SRC 1)
-set(PIP_MINOR_SRC 3)
+set(PIP_MINOR_SRC 4)
 set(PIP_PATCH_SRC 1)
 
 set (nm PIP)
@@ -8,6 +8,6 @@ set(${uc_nm}_VERSION ${${nm}_MAJOR_SRC}.${${nm}_MINOR_SRC}.${${nm}_PATCH_SRC})
 set(PIP_URL ${LLNL_URL})
 set(PIP_GZ pip-${PIP_VERSION}.tar.gz)
 set(PIP_SOURCE ${PIP_URL}/${PIP_GZ})
-set(PIP_MD5 )
+set(PIP_MD5 6afbb46aeb48abac658d4df742bff714)
 
 add_cdat_package(pip "" "" "")
