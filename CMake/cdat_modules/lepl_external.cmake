0da85cca72ce7b2acc943ee6b04984b06bfeea1c
Charles Doutriaux <doutriaux1@llnl.gov>
2013-12-09 16:42:46 -0800

    Got lepl to use pipinstaller macro

--- CMake/cdat_modules/lepl_external.cmake
+++ CMake/cdat_modules/lepl_external.cmake
@@ -1,19 +1,5 @@
-# create an external project to install lepl,
+# create an external project to install MyProxyClient,
 # and configure and build it
+set(nm lepl)
 
-include(@cdat_CMAKE_BINARY_DIR@/cdat_common_environment.cmake)
-if (NOT OFFLINE_BUILD)
-    set(EGG_GZ lepl==${LEPL_VERSION})
-else ()
-    set(EGG_GZ ${CDAT_PACKAGE_CACHE_DIR}/${LEPL_GZ})
-endif()
-
-ExternalProject_Add(lepl
-    DOWNLOAD_COMMAND ""
-  BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ${EGG_CMD} ${EGG_GZ}
-  DEPENDS ${lepl_deps}
-  ${ep_log_options}
-  )
+include(pipinstaller)
