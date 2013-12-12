e021fbca14a16d0a4c004ff7c81005d159132d94
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-12-12 14:04:03 -0500

    Fixed cmake syntax and style issues

--- CMake/cmake_modules/pipinstaller.cmake
+++ CMake/cmake_modules/pipinstaller.cmake
@@ -4,14 +4,13 @@
 include(@cdat_CMAKE_BINARY_DIR@/cdat_common_environment.cmake)
 string(TOUPPER ${nm} uc_nm)
 
-message([INFO] PACKAGE: ${uc_nm} -${GIT_CMD_STR}- )
-if ( NOPREFIXOPT )
+if(NOPREFIXOPT )
     set(PRFX )
 else()
     set(PRFX --prefix=${PYTHONUSERBASE})
 endif()
 
-if ( GIT_CMD_STR )
+if(DEFINED GIT_CMD_STR )
     message([INFO] getting ${nm} from git repo: ${GIT_CMD_STR})
     include(GetGitRevisionDescription)
     set(URL_STR )
@@ -26,17 +25,18 @@ endif()
 message([INFO] URL STRING IS: ${URL_STR})
 
 ExternalProject_Add(${nm}
-    DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
-    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/build/${nm}
-    ${URL_STR} 
-    ${URL_MD5_STR}
-    ${GIT_CMD_STR}
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND env PYTHONPATH=${PYTHONPATH} ${USR_ENVS} ${PYTHON_EXECUTABLE} setup.py install ${PRFX}
-    DEPENDS ${${nm}_deps}
-    ${ep_log_options}
-    )
+  DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
+  SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/build/${nm}
+  ${URL_STR}
+  ${URL_MD5_STR}
+  ${GIT_CMD_STR}
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND env PYTHONPATH=${PYTHONPATH} ${USR_ENVS} ${PYTHON_EXECUTABLE} setup.py install ${PRFX}
+  DEPENDS ${${nm}_deps}
+  ${ep_log_options}
+)
 
+# Reset it
 set(GIT_CMD_STR )
