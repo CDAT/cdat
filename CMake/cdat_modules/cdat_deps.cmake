a437486df5c072b5e9a807c74fa08d13d7e5ffc1
Charles Doutriaux <doutriaux1@llnl.gov>
2013-12-16 13:45:53 -0800

    only add esmf deps if actually built

--- CMake/cdat_modules/cdat_deps.cmake
+++ CMake/cdat_modules/cdat_deps.cmake
@@ -1,4 +1,8 @@
 set(CDAT_deps ${wget_pkg} ${cmor_pkg} ${python_pkg} ${pyqt_pkg} ${numpy_pkg}
               ${pmw_pkg} ${jasper_pkg} ${g2clib_pkg} ${ffmpeg_pkg} ${qt_pkg}
-              ${pixman_pkg} ${tiff_pkg} ${libcf_pkg} ${esmf_pkg} ${netcdf_pkg}
+              ${pixman_pkg} ${tiff_pkg} ${libcf_pkg} ${netcdf_pkg}
               ${myproxyclient_pkg})
+if (CDAT_BUILD_ESMF_ESMP)
+    list(APPEND CDAT_deps ${esmf_pkg})
+endif()
+
