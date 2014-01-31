ff7d97fe1f8af9df2f14357a227a78e22a474135
Charles Doutriaux <doutriaux1@llnl.gov>
2014-01-30 17:42:28 -0800

    Turned off pycliamte and scietificpython for esgf

--- CMake/cdat_modules/pyclimate_pkg.cmake
+++ CMake/cdat_modules/pyclimate_pkg.cmake
@@ -4,4 +4,4 @@ set(PYCLIMATE_GZ PyClimate-${PYCLIMATE_VERSION}.tar.gz)
 set(PYCLIMATE_SOURCE ${PYCLIMATE_URL}/${PYCLIMATE_GZ})
 set(PYCLIMATE_MD5 094ffd0adedc3ede24736e0c0ff1699f)
 
-add_cdat_package(pyclimate "" "" "")
+add_cdat_package_dependent(pyclimate "" "" ON "CDAT_BUILD_WO_ESGF" OFF)
