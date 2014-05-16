552cebfbaebf128c2c120d5fb7a0fabe8838ebe3
Charles Doutriaux <doutriaux1@llnl.gov>
2014-05-16 10:54:30 -0700

    added capability to change bg color

--- Packages/vcs/Test/test_basic_animation.py
+++ Packages/vcs/Test/test_basic_animation.py
@@ -12,11 +12,9 @@ if interact:
   i.Initialize()
 f=cdms2.open(os.path.join(sys.prefix,'sample_data','clt.nc'))
 s=f("clt")
-s2 = f("u")
 #f=cdms2.open(os.path.join(sys.prefix,"sample_data","sampleCurveGrid4.nc"))
 #s=f("sample")
 x=vcs.init()
-x.backgroundcolor = 65*2.55,65*2.55,65*2.55
 tmpl = x.createtemplate()
 #tmpl.data.x1=.001
 #tmpl.data.x2=.5
@@ -25,25 +23,16 @@ tmpl = x.createtemplate()
 if interact:
   x.backend.renWin = renWin
 x.setcolormap("rainbow")
-isol = x.createisoline()
-isof = x.createisofill()
-wc = [-180,180,-90,90]
-isof.datawc_x1=wc[0]
-isof.datawc_x2=wc[1]
-isof.datawc_y1=wc[2]
-isof.datawc_y2=wc[3]
-isol.datawc_x1=wc[0]
-isol.datawc_x2=wc[1]
-isol.datawc_y1=wc[2]
-isol.datawc_y2=wc[3]
+#gm=x.createisoline()
+#gm=x.createboxfill()
+gm=x.createisofill()
+gm.datawc_x1=0
+gm.datawc_x2=40
+gm.datawc_y1=-40
+gm.datawc_y2=40
 bg = False
 #bg = True
-isof.levels = range(-20,135,10)
-isol.levels = range(-20,135,10)
-x.plot(s,tmpl,isof,bg=bg)
-x.plot(s2,tmpl,isol,bg=bg)
-x.png("test")#,width=2000,height=1000)
-if interact:
-  i.Start()
-else:
-  raw_input("Press enter")
+gm.levels = range(-20,135,10)
+x.plot(s,tmpl,gm,bg=bg)
+x.animate.create()
+raw_input("Press enter")
