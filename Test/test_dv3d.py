2d7f3f8e8a8ef16b76c0eb916020b7fffc12cebd
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-06-20 17:08:20 -0400

    Interaction fixes

--- Test/test_dv3d.py
+++ Test/test_dv3d.py
@@ -8,8 +8,20 @@ import cdms2
 import sys
 import os
 
-f = cdms2.open( os.path.join( sys.prefix, "sample_data", "geos5-sample.nc") )
-u = f["uwnd"] 
+class DataType:
+   STRUCTURED = 0
+   UNSTRUCTURED = 1                           
+
+data_type = DataType.STRUCTURED
+
+if data_type == DataType.UNSTRUCTURED:
+    testDataDir = '/Users/tpmaxwel/Data'
+    testDataFile = os.path.join( testDataDir, 'WRF', 'wrfout_d03_2013-07-02_02-00-00.nc' )
+    f = cdms2.open( testDataFile )
+    u = f["U"] 
+else:
+    f = cdms2.open( os.path.join( sys.prefix, "sample_data", "geos5-sample.nc") )
+    u = f["uwnd"] 
 
 x = vcs.init()
 dv3d = x.createdv3d()
