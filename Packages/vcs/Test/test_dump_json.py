40c20a1953f562e70775174f6a39392f3cfd51f1
Charles Doutriaux <doutriaux1@llnl.gov>
2014-06-23 16:46:36 -0700

    test file for json dump

---
+++ Packages/vcs/Test/test_dump_json.py
@@ -0,0 +1,16 @@
+import vcs
+
+x=vcs.init()
+
+#b = x.createboxfill("Charles.Doutriaux")
+#b = x.createisofill("Charles.Doutriaux")
+#b = x.createisoline("Charles.Doutriaux")
+#b = x.createmeshfill("Charles.Doutriaux")
+#b = x.createoneD("Charles.Doutriaux")
+#b = x.createfillarea("Charles.Doutriaux")
+#b = x.createtext("Charles.Doutriaux")
+#b = x.createline("Charles.Doutriaux")
+b = x.createmarker("Charles.Doutriaux")
+
+b.script("box","a")
+
