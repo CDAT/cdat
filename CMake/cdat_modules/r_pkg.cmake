87592b4b6060ec2ecdf9036267ebc5c3edb4a285
Charles Doutriaux <doutriaux1@llnl.gov>
2014-06-26 11:00:10 -0700

    updated a lot of packages

--- CMake/cdat_modules/r_pkg.cmake
+++ CMake/cdat_modules/r_pkg.cmake
@@ -1,9 +1,9 @@
-set(R_MAJOR_SRC 2)
-set(R_MINOR_SRC 15)
-set(R_PATCH_SRC 1)
+set(R_MAJOR_SRC 3)
+set(R_MINOR_SRC 1)
+set(R_PATCH_SRC 0)
 set(R_URL ${LLNL_URL})
 set(R_GZ R-${R_MAJOR_SRC}.${R_MINOR_SRC}.${R_PATCH_SRC}.tar.gz)
-set(R_MD5 fcdf247e707fdade82b78bcf911a54f1)
+set(R_MD5 a1ee52446bee81820409661e6d114ab1)
 set(R_SOURCE ${R_URL}/${R_GZ})
 
 set (nm R)
