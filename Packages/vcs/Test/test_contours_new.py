81be58475ffb182310eebbcb7a7caf9aba038b60
Charles Doutriaux <doutriaux1@llnl.gov>
2014-07-09 12:22:02 -0700

    Got missing value color to work

--- Packages/vcs/Test/test_contours_new.py
+++ Packages/vcs/Test/test_contours_new.py
@@ -6,10 +6,12 @@ import MV2
 
 f=cdms2.open(os.path.join(sys.prefix,"sample_data","clt.nc"))
 s=f("clt",time=slice(0,1),squeeze=1)
-s=MV2.masked_greater(s,67.)
+s=MV2.masked_less(s,65.)
 x=vcs.init()
 gm=x.createisofill()
 gm=x.createboxfill()
+gm.missing = 252
+#gm.levels = [65,70,75,80,85,90,95,100]
 x.plot(s,gm)
 x.png(gm.g_name)
 x.interact()
