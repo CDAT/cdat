5cef9c3867439793f30d31d22009c82d8c27c644
Charles Doutriaux <doutriaux1@llnl.gov>
2014-07-09 17:25:33 -0700

    got most vector properties going, need to do reference attribute now

--- Test/test_basic_vectors.py
+++ Test/test_basic_vectors.py
@@ -3,16 +3,20 @@ import cdms2
 import os
 import sys
 
+x=vcs.init()
 f=cdms2.open(os.path.join(sys.prefix,'sample_data','clt.nc'))
 u=f("u")
 v=f("v")
 
-V=vcs.createvector()
+V=x.createvector()
+V.linecolor=242
+V.scale = 5.
+V.type = "arrows"
+V.reference = 6.
 V.list()
 
-x=vcs.init()
-x.plot(u,v,V)
+x.plot(u[::2],v[::2],V)
 
-x.png("vectors_bad")
+x.png("vectors")
 x.interact()
 
