9c7b3db2abe565d7c66d603535e60a4465bd3de8
Chris Harris <chris.harris@kitware.com>
2014-07-15 12:35:09 -0400

    Turn Wget to SYSTEM by default

--- CMake/cdat_modules/wget_pkg.cmake
+++ CMake/cdat_modules/wget_pkg.cmake
@@ -7,7 +7,7 @@ set(WGET_GZ wget-${WGET_MAJOR}.${WGET_MINOR}.tar.gz)
 set(WGET_MD5 141461b9c04e454dc8933c9d1f2abf83)
 set(WGET_SOURCE ${WGET_URL}/${WGET_GZ})
 
-add_cdat_package(Wget "" "Build Wget" OFF)
+add_cdat_package(Wget "" "Build Wget" SYSTEM)
 
 set (nm WGET)
 string(TOUPPER ${nm} uc_nm)
