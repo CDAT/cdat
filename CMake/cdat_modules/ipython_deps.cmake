db1d8103c71b93cf174c4260fd8702b54ea61656
Charles Doutriaux <doutriaux1@llnl.gov>
2014-07-17 18:14:55 -0700

    add the option in wrong order

--- CMake/cdat_modules/ipython_deps.cmake
+++ CMake/cdat_modules/ipython_deps.cmake
@@ -1,5 +1,5 @@
 if (CDAT_BUILD_FULL)
-  set(IPYTHON_deps ${pip_pkg} ${numpy_pkg})
-else ()
   set(IPYTHON_deps ${pip_pkg} ${tornado_pkg} ${numpy_pkg} ${numexpr_pkg})
+else ()
+  set(IPYTHON_deps ${pip_pkg} ${numpy_pkg})
 endif()
