0af7eb6ec2677270efccc07f884cf7f7bf1457cd
Charles Doutriaux <doutriaux1@llnl.gov>
2014-07-30 12:56:07 -0700

    work with aashish

--- Test/test_basic_polar_plots.py
+++ Test/test_basic_polar_plots.py
@@ -14,7 +14,7 @@ f=cdms2.open(os.path.join(sys.prefix,"sample_data","clt.nc"))
 #s=f("clt",slice(0,1),longitude=(0,360),squeeze=1)
 s=f("clt",slice(0,1),squeeze=1)
 
-x.plot(s,b,ratio="1t")
+x.plot(s,b)
 raw_input()
 
 x.png("vcs_test_polar")
