d709d42f893869c0499524642cf59729199f8fed
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-08-28 16:04:28 -0400

    Patch rebase discrepanies

--- Packages/vcs/Lib/VTKPlots.py
+++ Packages/vcs/Lib/VTKPlots.py
@@ -206,6 +206,7 @@ class VTKVCSBackend(object):
       self.renderer = vtk.vtkRenderer()
       r,g,b = self.canvas.backgroundcolor
       self.renderer.SetBackground(r/255.,g/255.,b/255.)
+      self.createDefaultInteractor(self.renderer) 
       self.renWin.AddRenderer(self.renderer)
       return True
     else:
