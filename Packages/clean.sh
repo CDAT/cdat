85df4e5785ad5372f010ff342acf92e81216266f
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-08-28 17:52:03 -0400

    Update testing

---
+++ Packages/clean.sh
@@ -0,0 +1,4 @@
+rm -rf ./DV3D/build
+cd /Users/tpmaxwell/Development/uvcdat/master_build/build/install/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/
+rm -r DV3D
+rm -r vcs
