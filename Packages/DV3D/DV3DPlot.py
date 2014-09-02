47be18e062f2b74b6207be22b2cdc84ae301d17b
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-09-02 15:22:38 -0400

    Improve window resize handling

--- Packages/DV3D/DV3DPlot.py
+++ Packages/DV3D/DV3DPlot.py
@@ -521,7 +521,7 @@ class DV3DPlot():
 #            self.addObserver( self.renderWindowInteractor, 'ComputeVisiblePropBoundsEvent', self.onAnyEvent )
 #            self.addObserver( self.renderWindowInteractor, 'AnyEvent', self.onAnyEvent )
             RenderWindow = self.renderWindowInteractor.GetRenderWindow()   
-#            RenderWindow.AddObserver( 'ModifiedEvent', self.onWindowModified )
+#            RenderWindow.AddObserver( 'AnyEvent', self.onAnyWindowEvent )
             RenderWindow.AddObserver( 'RenderEvent', self.onWindowRenderEvent )
             self.updateInteractor() 
             self.activated = True 
@@ -642,7 +642,10 @@ class DV3DPlot():
             self.onRenderWindowResize()
             self.renderWindowSize = window_size
              
-                                        
+    def onAnyWindowEvent( self, caller=None, event=None ):
+         print "Window Event: ", event
+
+                                       
 #     def onWindowModified( self, caller=None, event=None ):
 #         print "Window Modified Event "
 #         renwin = self.renderWindow if (caller == None) else caller 
@@ -663,9 +666,12 @@ class DV3DPlot():
             
     def onRenderWindowResize( self ):
         if not self.resizingWindow:
+#            print " onRenderWindowResize, size = ", str( self.renderWindowSize )
             self.resizingWindow = True
             self.updateTextDisplay()
             self.buttonBarHandler.repositionButtons()
+            self.renderWindow.Modified()
+            self.renderWindow.MakeCurrent()
             self.render()
             self.resizingWindow = False
 
