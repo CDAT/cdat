bdbefd787306eb4675c004447f8d5b2e58f119a1
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-09-05 21:36:12 -0400

    Enabled ctest for cdat-travis

--- CMake/travis_build.cmake
+++ CMake/travis_build.cmake
@@ -1,27 +1,15 @@
 set(CTEST_SOURCE_DIRECTORY "$ENV{TRAVIS_BUILD_DIR}")
-set(CTEST_BINARY_DIRECTORY "$ENV{TRAVIS_BUILD_DIR}/../_build")
+set(CTEST_BINARY_DIRECTORY "$ENV{TRAVIS_BUILD_DIR}/_build")
 
 include(${CTEST_SOURCE_DIRECTORY}/CTestConfig.cmake)
 set(CTEST_SITE "Travis")
 set(CTEST_BUILD_NAME "Linux-$ENV{TRAVIS_BRANCH}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(PATH "${CTEST_BINARY_DIRECTORY}/install/bin:${CTEST_BINARY_DIRECTORY}/install/Externals/bin")
-set(LD_LIBRARY_PATH "${CTEST_BINARY_DIRECTORY}/install/lib:${CTEST_BINARY_DIRECTORY}/install/Externals/lib")
-set(DYLD_LIBRARY_PATH "${CTEST_BINARY_DIRECTORY}/install/lib:${CTEST_BINARY_DIRECTORY}/install/Externals/lib")
-set(PYTHONPATH "${CTEST_BINARY_DIRECTORY}/install/lib/python2.7/site-packages:${CTEST_BINARY_DIRECTORY}/install/Externals/lib/python2.7/site-packages")
-
-set(CTEST_ENVIRONMENT
-   "PATH=${PATH}"
-   "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
-   "DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}"
-   "PYTHONPATH=${PYTHONPATH}")
-set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4")
-message("CTEST_ENVIRONMENT ${CTEST_ENVIRONMENT}")
 
 ctest_start("Continuous")
 ctest_configure()
 ctest_build()
-ctest_test(INCLUDE vcs PARALLEL_LEVEL 1 RETURN_VALUE res)
+ctest_test(PARALLEL_LEVEL 1 RETURN_VALUE res)
 #ctest_coverage()
 #file(REMOVE ${CTEST_BINARY_DIRECTORY}/coverage.xml)
 
