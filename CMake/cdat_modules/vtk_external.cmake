5f65410a8d9bdafdbe52a6f4b21c04f31414875b
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-09-07 16:46:21 -0400

    Use cdat packages

--- CMake/cdat_modules/vtk_external.cmake
+++ CMake/cdat_modules/vtk_external.cmake
@@ -6,6 +6,58 @@ set(GIT_CMD_STR GIT_REPOSITORY "${VTK_SOURCE}")
 
 set(_vtk_modules "vtkRenderingImage;vtkRenderingVolume;vtkRenderingLabel;vtkRenderingFreeType;vtkRenderingFreeTypeOpenGL;vtkRenderingVolumeOpenGL;vtkRenderingCore;vtkRenderingOpenGL;vtkGeovisCore;vtkViewsCore;vtkViewsGeovis;vtkInteractionImage;vtkInteractionStyle;vtkInteractionWidgets;vtkCommonTransforms;vtkCommonCore;vtkCommonComputationalGeometry;vtkCommonExecutionModel;vtkCommonSystem;vtkCommonMisc;vtkFiltersFlowPaths;vtkFiltersStatistics;vtkFiltersAMR;vtkFiltersGeneric;vtkFiltersSources;vtkFiltersModeling;vtkFiltersExtraction;vtkFiltersSelection;vtkFiltersSMP;vtkFiltersCore;vtkFiltersHybrid;vtkFiltersTexture;vtkFiltersGeneral;vtkFiltersImaging;vtkFiltersGeometry;vtkIOImage;vtkIOCore;vtkIOExport;vtkIOImport;vtkIOGeometry;vtkImagingColor;vtkImagingSources;vtkImagingCore;vtkImagingGeneral;vtkImagingMath")
 
+# Either we use cdat zlib and libxml or system zlib and libxml
+list(APPEND vtk_build_args
+  -DVTK_USE_SYSTEM_ZLIB:BOOL=ON
+  -DVTK_USE_SYSTEM_LIBXML2:BOOL=ON
+  -DVTK_USE_SYSTEM_HDF5:BOOL=ON
+  -DVTK_USE_SYSTEM_NETCDF:BOOL=ON
+  -DVTK_USE_SYSTEM_FREETYPE:BOOL=ON
+)
+
+# Turn off testing and other non essential featues
+list(APPEND vtk_build_args
+  -DBUILD_TESTING:BOOL=OFF
+  -DCMAKE_PREFIX_PATH:PATH=${cdat_EXTERNALS}
+)
+
+# Use cdat zlib
+if(NOT CDAT_USE_SYSTEM_ZLIB)
+  list(APPEND vtk_build_args
+    -DZLIB_INCLUDE_DIR:PATH=${cdat_EXTERNALS}/include
+    -DZLIB_LIBRARY:FILEPATH=${cdat_EXTERNALS}/lib/libz${_LINK_LIBRARY_SUFFIX}
+  )
+endif()
+
+# Use cdat libxml
+if(NOT CDAT_USE_SYSTEM_LIBXML2)
+  list(APPEND vtk_build_args
+    -DLIBXML2_INCLUDE_DIR:PATH=${cdat_EXTERNALS}/include/libxml2
+    -DLIBXML2_LIBRARIES:FILEPATH=${cdat_EXTERNALS}/lib/libxml2${_LINK_LIBRARY_SUFFIX}
+    -DLIBXML2_XMLLINT_EXECUTABLE:FILEPATH=${cdat_EXTERNALS}/bin/xmllint
+  )
+endif()
+
+# Use cdat hdf5
+if(NOT CDAT_USE_SYSTEM_HDF5)
+  list(APPEND vtk_build_args
+    -DHDF5_DIR:PATH=${cdat_EXTERNALS}/
+    -DHDF5_C_INCLUDE_DIR:PATH=${cdat_EXTERNALS}/include
+    -DHDF5_INCLUDE_DIR:PATH=${cdat_EXTERNALS}/include
+    -DHDF5_LIBRARY:FILEPATH=${cdat_EXTERNALS}/lib/libhdf5${_LINK_LIBRARY_SUFFIX}
+    -DHDF5_hdf5_LIBRARY:FILEPATH=${cdat_EXTERNALS}/lib/libhdf5${_LINK_LIBRARY_SUFFIX}
+    -DHDF5_hdf5_LIBRARY_RELEASE:FILEPATH=${cdat_EXTERNALS}/lib/libhdf5${_LINK_LIBRARY_SUFFIX}
+  )
+
+  if(NOT CDAT_USE_SYSTEM_ZLIB)
+    list(APPEND vtk_build_args
+      -DHDF5_z_LIBRARY:FILEPATH=${cdat_EXTERNALS}/lib/libz${_LINK_LIBRARY_SUFFIX}
+      -DHDF5_z_LIBRARY_RELEASE:FILEPATH=${cdat_EXTERNALS}/lib/libz${_LINK_LIBRARY_SUFFIX}
+    )
+  endif()
+endif()
+
+
 set(_vtk_module_options)
 foreach(_module ${_vtk_modules})
   list(APPEND _vtk_module_options "-DModule_${_module}:BOOL=ON")
@@ -27,6 +79,7 @@ ExternalProject_Add(VTK
     -DCMAKE_C_FLAGS:STRING=${cdat_tpl_c_flags}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_CFG_INTDIR}
     ${cdat_compiler_args}
+    ${vtk_build_args}
     -DVTK_WRAP_PYTHON:BOOL=ON
     -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON_EXECUTABLE}
     -DPYTHON_INCLUDE_DIR:PATH=${PYTHON_INCLUDE}
