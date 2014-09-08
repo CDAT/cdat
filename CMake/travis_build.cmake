c603348c832a961737d82e8ae14f96356ce5f97e
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-09-08 02:24:25 -0400

    Fixed path to externals

--- CMake/travis_build.cmake
+++ CMake/travis_build.cmake
@@ -6,11 +6,12 @@ set(CTEST_SITE "Travis")
 set(CTEST_BUILD_NAME "Linux-$ENV{TRAVIS_BRANCH}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_ENVIRONMENT
-   "PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/bin:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/bin"
-   "LD_LIBRARY_PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/lib"
-   "DYLD_LIBRARY_PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/lib"
+   "PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/bin:$ENV{TRAVIS_BUILD_DIR}/../_build/install/Externals/bin"
+   "LD_LIBRARY_PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib:$ENV{TRAVIS_BUILD_DIR}/../_build/install/Externals/lib"
+   "DYLD_LIBRARY_PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib:$ENV{TRAVIS_BUILD_DIR}/../_build/install/Externals/lib"
    "PYTHONPATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib/python2.7/site-packages:$ENV{TRAVIS_BUILD_DIR}/../_build/install/Externals/lib/python2.7/site-packages")
 set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4")
+message("CTEST_ENVIRONMENT ${CTEST_ENVIRONMENT}")
 
 ctest_start("Continuous")
 ctest_configure()
