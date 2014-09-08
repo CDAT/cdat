980883b7cc3464ff95af22fa71d70f211f782022
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-09-07 23:28:44 -0400

    Fixed travis build script

--- CMake/travis_build.cmake
+++ CMake/travis_build.cmake
@@ -5,6 +5,12 @@ include(${CTEST_SOURCE_DIRECTORY}/CTestConfig.cmake)
 set(CTEST_SITE "Travis")
 set(CTEST_BUILD_NAME "Linux-$ENV{TRAVIS_BRANCH}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set (CTEST_ENVIRONMENT
+   "PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/bin:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/bin"
+   "LD_LIBRARY_PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/lib"
+   "DYLD_LIBRARY_PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/lib"
+)
+set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4")
 
 ctest_start("Continuous")
 ctest_configure()
