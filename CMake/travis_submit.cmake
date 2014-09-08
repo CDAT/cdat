aac82e967444db2cffb0fbf4af9fbd0182feb874
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-09-08 00:49:04 -0400

    Fixed styling

--- CMake/travis_submit.cmake
+++ CMake/travis_submit.cmake
@@ -5,12 +5,11 @@ include(${CTEST_SOURCE_DIRECTORY}/CTestConfig.cmake)
 set(CTEST_SITE "Travis")
 set(CTEST_BUILD_NAME "Linux-$ENV{TRAVIS_BRANCH}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set (CTEST_ENVIRONMENT
+set(CTEST_ENVIRONMENT
    "PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/bin:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/bin"
    "LD_LIBRARY_PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/lib"
    "DYLD_LIBRARY_PATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib:$ENV{TRAVIS_BUILD_DIR}/../_build/install/External/lib"
-   "PYTHONPATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib/python2.7/site-packages:$ENV{TRAVIS_BUILD_DIR}/../_build/install/Externals/lib/python2.7/site-packages"
-)
+   "PYTHONPATH=$ENV{TRAVIS_BUILD_DIR}/../_build/install/lib/python2.7/site-packages:$ENV{TRAVIS_BUILD_DIR}/../_build/install/Externals/lib/python2.7/site-packages")
 set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME} -j4")
 
 ctest_start("Continuous")
