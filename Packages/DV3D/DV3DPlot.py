77f2a40f4d803e2068e499d671a1e25acb531050
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-09-26 12:32:34 -0400

    Stop animation in gui on closing

--- Packages/DV3D/DV3DPlot.py
+++ Packages/DV3D/DV3DPlot.py
@@ -405,6 +405,7 @@ class DV3DPlot():
                      
     def stopAnimation(self):
         self.animating = False
+        self.animationStepper.stopAnimation()
         if self.animationTimerId <> -1: 
             self.animationTimerId = -1
             self.renderWindowInteractor.DestroyTimer( self.animationTimerId  ) 
