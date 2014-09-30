63c593ce3c30d48767f88e55f7c0bc075aa69e65
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-09-30 12:27:41 -0400

    Fix widget error for 2D data

--- Packages/DV3D/RectilinearGridPlot.py
+++ Packages/DV3D/RectilinearGridPlot.py
@@ -1399,8 +1399,9 @@ class RectGridPlot(StructuredGridPlot):
     def updatingColormap( self, cmap_index, colormapManager ):
         if cmap_index == 0:
             for widget in ( self.planeWidgetX, self.planeWidgetY, self.planeWidgetZ ):
-                widget.SetLookupTable( colormapManager.lut )
-                widget.SetTextureInterpolate( colormapManager.smoothColormap )
+                if widget <> None:
+                    widget.SetLookupTable( colormapManager.lut )
+                    widget.SetTextureInterpolate( colormapManager.smoothColormap )
             self.updateModule()
             
     def getPlaneWidget( self, plane ):       
