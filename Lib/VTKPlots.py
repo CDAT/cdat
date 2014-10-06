631fa509cf031340bb791a96879a6c596dbe2a11
Charles <doutriaux1@llnl.gov>
2014-10-06 07:33:00 -0700

    Revert "Fixed GUI color map update (HACK)"

--- Lib/VTKPlots.py
+++ Lib/VTKPlots.py
@@ -234,7 +234,6 @@ class VTKVCSBackend(object):
       return ren
 
   def update(self, *args, **kargs):
-      self._lastSize = -1
       if self.renWin:
           self.configureEvent(None,None)
 
