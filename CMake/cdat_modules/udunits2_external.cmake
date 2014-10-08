e32def3f383451c2efc645a1cd29269f53237a91
David C. Lonie <david.lonie@kitware.com>
2014-10-08 10:15:10 -0400

    Make fix mac specific.
    Forgot to uncomment these after testing....

--- CMake/cdat_modules/udunits2_external.cmake
+++ CMake/cdat_modules/udunits2_external.cmake
@@ -2,13 +2,13 @@ set(udunits_source "${CMAKE_CURRENT_BINARY_DIR}/build/udunits2")
 set(udunits_install "${cdat_EXTERNALS}")
 
 set(udunits_patch_command "")
-#if(APPLE)
+if(APPLE)
   # Modified configure file to workaround random flex failures
   set(udunits_patch_command
     ${CMAKE_COMMAND} -E copy_if_different
       "${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/udunits2_apple_configure.in"
       "${udunits_source}/configure")
-#endif()
+endif()
 
 ExternalProject_Add(udunits2
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
