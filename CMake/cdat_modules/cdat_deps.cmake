fc24152b68e9d600c30d5742521b90b77aaf3102
Charles Doutriaux <doutriaux1@llnl.gov>
2014-10-09 12:38:27 -0700

    Ok cmor is now optional and builds fine when turned on

--- CMake/cdat_modules/cdat_deps.cmake
+++ CMake/cdat_modules/cdat_deps.cmake
@@ -1,8 +1,8 @@
-set(CDAT_deps ${wget_pkg} ${cmor_pkg} ${python_pkg} ${numpy_pkg}
-              ${jasper_pkg} ${g2clib_pkg} ${ffmpeg_pkg} ${tiff_pkg}
+set(CDAT_deps ${wget_pkg} ${python_pkg} ${numpy_pkg}
+              ${jasper_pkg} ${g2clib_pkg} ${tiff_pkg}
               ${libcf_pkg} ${netcdf_pkg} ${myproxyclient_pkg} ${udunits2_pkg})
 if (CDAT_BUILD_GRAPHICS)
-  list(APPEND CDAT_deps ${paraview_pkg})
+  list(APPEND CDAT_deps ${ffmpeg_pkg} ${paraview_pkg})
 endif()
 
 if (CDAT_BUILD_ESMF)
