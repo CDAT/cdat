848cac8acaad9f1c143ccf3450a9fdd940e0b416
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-10-09 19:53:31 -0400

    Fixed cdat dependencies

--- CMake/cdat_modules/cdat_deps.cmake
+++ CMake/cdat_modules/cdat_deps.cmake
@@ -2,7 +2,12 @@ set(CDAT_deps ${wget_pkg} ${python_pkg} ${numpy_pkg}
               ${jasper_pkg} ${g2clib_pkg} ${tiff_pkg}
               ${libcf_pkg} ${netcdf_pkg} ${myproxyclient_pkg} ${udunits2_pkg})
 if (CDAT_BUILD_GRAPHICS)
-  list(APPEND CDAT_deps ${ffmpeg_pkg} ${paraview_pkg})
+  if (CDAT_BUILD_PARALLEL)
+    list(APPEND CDAT_deps ${paraview_pkg})
+  else
+    list(APPEND CDAT_deps ${vtk_pkg})
+  endif()
+  list(APPEND CDAT_deps ${ffmpeg_pkg})
 endif()
 
 if (CDAT_BUILD_ESMF)
