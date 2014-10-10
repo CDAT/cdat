663c6a3dd74412fb47bfe8a7ee69c9a0ab71cc99
Charles Doutriaux <doutriaux1@llnl.gov>
2014-10-10 10:51:22 -0700

    ok maybe got a fix for mac

--- CMake/cdat_modules/cmor_pkg.cmake
+++ CMake/cdat_modules/cmor_pkg.cmake
@@ -1,7 +1,7 @@
-set(CMOR_VERSION 2.9.1)
+set(CMOR_VERSION 2.9.2)
 set(CMOR_URL https://github.com/PCMDI/cmor/archive)
 set(CMOR_GZ CMOR-${CMOR_VERSION}.tar.gz)
-set(CMOR_MD5 3568dd5e42083494ff4b5b2b2b60ba39)
+set(CMOR_MD5 f7e78104878d93ffbd3cae2ccd95db2c)
 set (nm CMOR)
 string(TOUPPER ${nm} uc_nm)
 
