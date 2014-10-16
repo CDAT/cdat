196acdbb22569a691035181ee6109ff3f54b1e55
Charles Doutriaux <doutriaux1@llnl.gov>
2014-10-16 16:11:51 -0700

    Ok got it to dump associatedobj, updated test suite

--- testing/vcs/test_vcs_template_saves_associated_to_tt.py
+++ testing/vcs/test_vcs_template_saves_associated_to_tt.py
@@ -1,8 +1,17 @@
-import vcs
+import vcs,sys,filecmp,os
+
+good = sys.argv[1]
+
 x=vcs.init()
 tt=x.createtexttable("this_is_my_test_tt")
 t=x.createtemplate("this_is_our_test_template")
 
 t.xname.texttable=tt
 
-t.script("template_test_associated_dump.json")
+fnm = "template_test_associated_dump.json"
+if os.path.exists(fnm):
+  os.remove(fnm)
+
+t.script(fnm)
+
+assert filecmp.cmp(fnm,good)
