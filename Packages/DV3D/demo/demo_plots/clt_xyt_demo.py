89cc3f78e8293f7339bfafea7b0f314ee8d25772
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-10-16 14:02:46 -0400

    Fix constituent buttons

--- Packages/DV3D/demo/demo_plots/clt_xyt_demo.py
+++ Packages/DV3D/demo/demo_plots/clt_xyt_demo.py
@@ -3,6 +3,10 @@ x = vcs.init()
 f = cdms2.open(sys.prefix+"/sample_data/clt.nc")   
 v = f["clt"] 
 dv3d = vcs.get3d_scalar('xyt')
+dv3d.ToggleSurfacePlot = vcs.on
+dv3d.ToggleVolumePlot = vcs.on
+dv3d.IsosurfaceValue = [11.4]
+dv3d.ScaleTransferFunction = [80, 100, 1]
 x.plot( v, dv3d )
 x.interact()
 
