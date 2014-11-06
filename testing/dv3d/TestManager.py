0ef2644e8c5dc82eb585b6c0c91c0399f5cd8be3
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-11-06 15:10:51 -0500

    Add constituents test

--- testing/dv3d/TestManager.py
+++ testing/dv3d/TestManager.py
@@ -117,9 +117,13 @@ class vcsTest:
         test_image = '.'.join( [ self.name, 'test', 'png' ] )
         ref_image  = '.'.join( [ self.name, 'png' ] )
         self.canvas.png( test_image )
-        print "Copying ref image %s to %s in %s " % ( self.image_name, ref_image, os.path.abspath('.') )
-        shutil.copy( self.image_name, ref_image )
-        ret = checkimage.check_result_image( ref_image, test_image, checkimage.defaultThreshold )
+        try:
+            shutil.copy( self.image_name, ref_image )
+            print "Copying ref image %s to %s in %s " % ( self.image_name, ref_image, os.path.abspath('.') )
+            ret = checkimage.check_result_image( ref_image, test_image, checkimage.defaultThreshold )
+        except IOError:
+            print "No ref image found."
+            ret = 0
         if  interactive: 
             print "Type <Enter> to continue and update ref image ( type 'n' to skip update )." 
             sys.stdout.flush()
@@ -150,4 +154,4 @@ if __name__ == '__main__':
     from TestDefinitions import testManager    
 #    testManager.runTests()
 #    testManager.runTest( 'dv3d_slider_test', True )
-#    testManager.showTest( 'dv3d_slider_test' )
+    testManager.showTest( 'dv3d_constituents_test' )
