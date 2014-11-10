98438bd48d9d2616931539fa68cb976f6fe9968f
Remi Rampin <remirampin@gmail.com>
2014-11-10 12:47:42 -0500

    Changes VisTrails URL to Github

--- CMake/cdat_modules/vistrails_pkg.cmake
+++ CMake/cdat_modules/vistrails_pkg.cmake
@@ -1,5 +1,5 @@
 set(VISTRAILS_VERSION ${VISTRAILS_TAG_POINT})
-set(VISTRAILS_SOURCE "${GIT_PROTOCOL}vistrails.org/git/vistrails.git")
+set(VISTRAILS_SOURCE "${GIT_PROTOCOL}github.com/UV-CDAT/VisTrails.git")
 set(VISTRAILS_VERSION uvcdat-master)
 ## Now tarball so faking md5 to tell checkout point
 set(VISTRAILS_MD5 uvcdat-master)
