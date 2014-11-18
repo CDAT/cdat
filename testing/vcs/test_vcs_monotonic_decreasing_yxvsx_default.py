e2ed28a79f4151c34dd15643610ff8d2697ca4d3
Charles Doutriaux <doutriaux1@llnl.gov>
2014-11-18 11:07:31 -0800

    Added test to test suite

--- testing/vcs/test_vcs_monotonic_decreasing_yxvsx_default.py
+++ testing/vcs/test_vcs_monotonic_decreasing_yxvsx_default.py
@@ -1,4 +1,11 @@
-import vcs,cdms2,numpy,MV2
+import vcs,numpy,cdms2,MV2,os,sys
+src=sys.argv[1]
+pth = os.path.join(os.path.dirname(__file__),"..")
+sys.path.append(pth)
+import checkimage
+x=vcs.init()
+
+x.setbgoutputdimensions(1200,1091,units="pixels")
 
 t=cdms2.createAxis(numpy.arange(120))
 t.designateTime()
@@ -8,5 +15,11 @@ data = MV2.arange(120,0,-1)
 data.id="data"
 data.setAxis(0,t)
 x=vcs.init()
-x.plot(data)
-raw_input("Press enter")
+x.plot(data,bg=1)
+fnm = 'test_vcs_monotonic_decreasing_yxvsx_default.png'
+
+x.png(fnm)
+print "fnm:",fnm
+print "src:",src
+ret = checkimage.check_result_image(fnm,src,checkimage.defaultThreshold)
+sys.exit(ret)
