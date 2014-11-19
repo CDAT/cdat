f2f5990cdc0647fa984104561e0023ee7a4c76eb
Charles Doutriaux <doutriaux1@llnl.gov>
2014-11-19 15:13:27 -0800

    beg of update for fat anim

---
+++ testing/vcs/test_update_array.py
@@ -0,0 +1,13 @@
+import vcs,numpy
+
+a=numpy.arange(100)
+a.shape=(10,10)
+b=numpy.ma.cos(a)
+a=numpy.ma.sin(a)
+
+x=vcs.init()
+d = x.plot(a)
+raw_input("Press enter")
+print d
+x.backend.update_input(d.backend,b)
+raw_input("ok done")
