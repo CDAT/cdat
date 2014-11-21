3bbf8559581ec86ff3672459b5d4d8b26f90d6fd
Charles Doutriaux <doutriaux1@llnl.gov>
2014-11-20 17:14:02 -0800

    removed print statements
    and made test input data smaller

--- testing/vcs/test_update_array.py
+++ testing/vcs/test_update_array.py
@@ -1,14 +1,14 @@
 import vcs,numpy,cdms2,MV2
 
-a=numpy.arange(6000)
-a.shape=(12,5,10,10)
+a=numpy.arange(400)
+a.shape=(2,2,10,10)
 b=MV2.cos(a)/2.
 a=MV2.sin(a)
-t=cdms2.createAxis(range(12))
+t=cdms2.createAxis(range(2))
 t.designateTime()
 t.units="months since 2014"
 t.id="time"
-l=cdms2.createAxis(numpy.arange(1,6)*1000.)
+l=cdms2.createAxis(numpy.arange(1,3)*1000.)
 l.designateLevel()
 l.units="hPa"
 l.id="plev"
@@ -18,6 +18,5 @@ b.setAxisList(a.getAxisList())
 x=vcs.init()
 d = x.plot(a)
 raw_input("Press enter")
-print d
 x.backend.update_input(d.backend,b(slice(1,2),slice(1,2)))
 raw_input("ok done")
