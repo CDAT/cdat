73e86c43ff82f5b9d76e9f852c2bd5154f7a3caa
Sam Fries <fries2@llnl.gov>
2014-12-19 13:35:24 -0800

    Moved NoInteractionStyle out of color_picker

--- Packages/vcs/Lib/vtk_ui/__init__.py
+++ Packages/vcs/Lib/vtk_ui/__init__.py
@@ -6,3 +6,7 @@ from resize_box import ResizeBox
 from handle import Handle
 import image_utils as image
 from text import Label
+import vtk
+class NoInteractionStyle(vtk.vtkInteractorStyle):
+    def __init__(self, parent=None):
+        pass
