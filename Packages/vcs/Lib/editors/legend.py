e0eadf00e045bdabb6b7a1e2e6818588830e93ce
Sam Fries <fries2@llnl.gov>
2015-01-07 09:47:01 -0800

    Added legend editor

---
+++ Packages/vcs/Lib/editors/legend.py
@@ -0,0 +1,27 @@
+from box import BoxEditor
+from vcs.vtk_ui import Toolbar
+import vcs
+
+class LegendEditor(BoxEditor):
+    def __init__(self, interactor, template, configurator):
+        super(LegendEditor, self).__init__(interactor, template.legend, configurator)
+        # Legend doesn't need anything too exciting, just need to add a toolbar button
+        self.toolbar = Toolbar(interactor, "Legend Options")
+        self.toolbar.show()
+
+        maps = vcs.elements["colormap"]
+        self.maps = maps.keys()
+        colormaps = []
+
+        for ind, cm in enumerate(self.maps):
+            colormaps.append("Colormap: %s" % cm)
+
+        self.toolbar.add_button(colormaps, action=self.change_map)
+
+    def change_map(self, state):
+        self.configurator.canvas.setcolormap(self.maps[state])
+        self.save()
+
+    def detach(self):
+        self.toolbar.detach()
+        super(LegendEditor, self).detach()
