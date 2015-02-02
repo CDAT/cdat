0ba74a56299419c8745faa55bbd4b7b6e655c59c
Sam Fries <fries2@llnl.gov>
2015-02-02 12:20:47 -0800

    Fixed issues with double click line

--- Packages/vcs/Lib/vtk_ui/behaviors/draggable.py
+++ Packages/vcs/Lib/vtk_ui/behaviors/draggable.py
@@ -1,11 +1,12 @@
 from behavior import Behavior
-
+import datetime
 class DraggableMixin(Behavior):
 
     def __init__(self):
         super(DraggableMixin, self).__init__()
         self.drag_origin = None
         self.drag_position = None
+        self.drag_started = None
         self.drag_buffer = 10 # in pixels
         self.add_event_handler("LeftButtonPressEvent", self.drag_clicked)
         self.add_event_handler("MouseMoveEvent", self.drag_moved)
@@ -30,13 +31,13 @@ class DraggableMixin(Behavior):
         if self.in_bounds(x, y):
             self.drag_origin = self.event_position()
             self.drag_position = None
-
+            self.drag_started = datetime.datetime.now()
             self.drag_start()
 
     def drag_moved(self, obj, event):
         x, y = self.event_position()
 
-        if self.drag_origin is None:
+        if self.drag_origin is None or datetime.datetime.now() - self.drag_started < datetime.timedelta(0, .25):
             return
 
         if self.drag_position is None:
@@ -59,7 +60,7 @@ class DraggableMixin(Behavior):
         return
 
     def drag_released(self, obj, event):
-        if self.drag_origin is not None or self.drag_position is not None:
+        if self.drag_origin is not None or self.drag_position is not None and datetime.datetime.now() - self.drag_started > datetime.timedelta(0, .25):
             self.drag_stop()
             self.drag_origin = None
             self.drag_position = None
