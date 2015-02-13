7727368b26b093574c6d792043e419514f4d0071
Thomas Maxwell <thomas.maxwell@nasa.gov>
2015-02-13 17:56:58 -0500

    Add timestamp to GPM reader

--- Packages/DV3D/demo/demo_plots/gpm_demo.py
+++ Packages/DV3D/demo/demo_plots/gpm_demo.py
@@ -1,16 +1,34 @@
-import collections, vcs, cdms2, h5py
+import collections, vcs, cdms2, h5py, cdtime
 from cdms2.hgrid import TransientCurveGrid
 from cdms2.coord import TransientAxis2D, TransientVirtualAxis
 import numpy as np
 import numpy.ma as ma
 eps = 0.0001
+time_units = "days since 1996-1-1"
 
 def print_attributes( grp ):
     print "\n ----> %s attributes:" % grp.name
     for attr in grp.attrs.keys():
         print "    > Attr %s = %s: " % ( attr, str(grp.attrs.get(attr)) )
 
-data_file="/Users/tpmaxwel/Data/GPM/2A.GPM.Ku.V520140829.20150201-S011128-E014127.V03B.RT-H5"
+def getFileMetadata( attrs ):
+    md = {}
+    for mdkey in attrs.keys():
+        mdtext = attrs[mdkey]
+        for mdentry in mdtext.split(';'):
+            mdelems = mdentry.split('=')
+            if len( mdelems ) == 2:
+               md[ mdelems[0].strip() ] = mdelems[1].strip()
+    return md
+
+def getReltime( timestamp, units ):
+    tstoks = timestamp.split('T')
+    ymd = tstoks[0].split('-')
+    hms = tstoks[1].split('.')[0].split(':')
+    ctval = cdtime.comptime( int(ymd[0]), int(ymd[1]), int(ymd[2]), int(hms[0]), int(hms[1]), int(hms[2])  )
+    return ctval.torel( units )
+
+data_file="/Users/tpmaxwel/Dropbox/Data/GPM/2A.GPM.Ku.V520140829.20150201-S011128-E014127.V03B.RT-H5"
 point_coord_var_names=[ "Latitude", "Longitude" ]
 lev_axis_name='nbin'
 time_axis_name=None
@@ -24,7 +42,7 @@ point_coord_axes = collections.OrderedDict()
 print "Shape: ", str( var.shape )
 
 fillval = float(var.attrs['CodeMissingValue'])
-var_shape = var.shape[:]
+var_shape = list(var.shape)
 data_array = np.zeros(var.shape, var.dtype )
 var.read_direct(data_array)
 data_array = ma.masked_inside( data_array, fillval - eps, fillval + eps )
@@ -41,6 +59,8 @@ for group in groups:
         group_path.append( subgrp )
 
 for grp in group_path: print_attributes( grp )
+file_mdata = getFileMetadata( hfile.attrs )
+timestamp = file_mdata.get( 'StartGranuleDateTime', None )
 
 dim_names = var.attrs['DimensionNames'].split(',')
 
@@ -68,6 +88,7 @@ if slice is not None:
    data_array = data_array[:,:,slice]
 
 else:
+    time_axis = None
     for caxis_name in coord_axes_rec.keys():
         trans_axis = transient_axes.get( caxis_name, None )
         if trans_axis is not None:
@@ -80,9 +101,18 @@ else:
             elif caxis_name == time_axis_name:
                 caxis.designateTime()
                 caxis.units = "indexed"
+                time_axis = caxis
             caxes.append( caxis )
+    if time_axis == None:
+        tval = getReltime( timestamp, time_units )
+        caxis = cdms2.createAxis( [ tval ], id="time" )
+        caxis.designateTime()
+        caxis.units = time_units
+        caxes.insert( 0, caxis )
+        var_shape.insert(0, 1 )
+        data_array = data_array.reshape( var_shape )
 
-v = cdms2.createVariable( data_array, var.dtype, 0, 0, data_array.mask, fillval, tcgrid, caxes, hfile.attrs, varname, 0 )
+v = cdms2.createVariable( data_array, var.dtype, 0, 0, data_array.mask, fillval, tcgrid, caxes, file_mdata, varname, 0 )
 
 hfile.close()
 
