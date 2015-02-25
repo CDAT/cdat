55b955fcdf309c7d909e47b280da61e10dce914c
Charles Doutriaux <doutriaux1@llnl.gov>
2015-02-25 14:49:44 -0800

    fix #450

--- CMake/cdat_modules/sampledata_external.cmake
+++ CMake/cdat_modules/sampledata_external.cmake
@@ -1,7 +1,16 @@
-message("SITEPKG:" ${PYTHON_SITE_PACKAGES_PREFIX})
 if (CDAT_DOWNLOAD_SAMPLE_DATA)
-  file(READ ${cdat_CMAKE_SOURCE_DIR}/Packages/dat/files.txt SAMPLE_FILES)
-  foreach(SAMPLE_FILE ${SAMPLE_FILES})
-    message("TRYING TO DOWNLOAD"${SAMPLE_FILE})
-  endforeach()
+  message("[INFO] ------------------------------------------------------------------------------------------------------------------------------")
+  configure_file(
+    "${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/cdat_download_sample_data.cmake.in"
+    "${cdat_CMAKE_BINARY_DIR}/cdat_download_sample_data.cmake"
+    @ONLY
+    )
+  set(sampledata_cmd ${CMAKE_COMMAND} -P ${cdat_CMAKE_BINARY_DIR}/cdat_download_sample_data.cmake)
+  ExternalProject_Add(sampledata
+    SOURCE_DIR ${cdat_SOURCE_DIR}/Packages/dat
+    CONFIGURE_COMMAND ${sampledata_cmd}
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    ${ep_log_options}
+    )
 endif()
