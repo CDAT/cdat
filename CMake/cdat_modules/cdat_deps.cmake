871ee89f6bb354301f28af7eff096fe704678790
Charles Doutriaux <doutriaux1@llnl.gov>
2015-02-25 13:35:30 -0800

    fixed merge issue

--- CMake/cdat_modules/cdat_deps.cmake
+++ CMake/cdat_modules/cdat_deps.cmake
@@ -1,4 +1,5 @@
 set(CDAT_deps ${wget_pkg} ${python_pkg} ${numpy_pkg}
+              ${libcdms_pkg}
               ${jasper_pkg} ${g2clib_pkg} ${tiff_pkg}
               ${libcf_pkg} ${netcdf_pkg} ${myproxyclient_pkg} ${udunits2_pkg})
 if (CDAT_BUILD_GRAPHICS)
