62f8e8156c3b002f5fad4c25220066258b4242bb
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-02-25 15:08:35 -0500

    Updating vtk with vtkweb fix

--- CMake/cdat_modules/vtk_pkg.cmake
+++ CMake/cdat_modules/vtk_pkg.cmake
@@ -1,4 +1,4 @@
 set(VTK_SOURCE ${GIT_PROTOCOL}github.com/UV-CDAT/VTK.git )
 set(VTK_MD5)
-set(VTK_BRANCH uvcdat-master)
+set(VTK_BRANCH uvcdat-with-objectid-fix)
 add_cdat_package_dependent(VTK "" "" ON "CDAT_BUILD_GRAPHICS" OFF)
