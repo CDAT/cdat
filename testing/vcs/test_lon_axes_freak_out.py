fcc92ed43e39a52828b31f50a097a94a586b8f57
Charles Doutriaux <doutriaux1@llnl.gov>
2015-03-26 12:02:19 -0700

    cleaned up new test case

--- testing/vcs/test_lon_axes_freak_out.py
+++ testing/vcs/test_lon_axes_freak_out.py
@@ -1,17 +1,29 @@
 import os,sys,vcs,cdms2
+src=sys.argv[1]
+pth = os.path.join(os.path.dirname(__file__),"..")
+sys.path.append(pth)
+import checkimage
+
 
 f=cdms2.open(os.path.join(sys.prefix,"sample_data","clt.nc"))
 s=f("clt")
-s2=f("clt",longitude=(-180,180))
 s3=f("clt",longitude=(0,360))
 
-print s.shape,s2.shape,s3.shape
+print s.shape,s3.shape
+
 
 x=vcs.init()
-y=vcs.init()
-z=vcs.init()
+x.drawlogooff()
+x.setbgoutputdimensions(1200,1091,units="pixels")
 
-b=x.getisofill("a_isofill")
-x.plot(s,"default")
-y.plot(s2,"default")
-z.plot(s3,"default")
+x.plot(s,bg=1)
+x.clear()
+x.plot(s3,bg=1)
+
+fnm = "test_lon_axes_freak_out.png"
+
+x.png(fnm)
+print "fnm:",fnm
+print "src:",src
+ret = checkimage.check_result_image(fnm,src,checkimage.defaultThreshold)
+sys.exit(ret)
