5bf4d3c3f4354d18772122261e105ad72e65bc2b
Sam Fries <fries2@llnl.gov>
2015-04-01 13:38:05 -0700

    Implemented test_vtk_ui_button_alignment

--- testing/vcs/vtk_ui/test_vtk_ui_button_alignment.py
+++ testing/vcs/vtk_ui/test_vtk_ui_button_alignment.py
@@ -1,13 +1,19 @@
 """
 Test button alignment
 """
-import vcs.vtk_ui
-
+from vcs.vtk_ui import button
 
 from vtk_ui_test import vtk_ui_test
 
 class test_vtk_ui_button_alignment(vtk_ui_test):
     def do_test(self):
-        return
+        self.win.SetSize(300, 50)
+        left_button = button.Button(self.inter, label="Left", halign=button.LEFT_ALIGN, valign=button.TOP_ALIGN, width=125)
+        left_button.show()
+        center_button = button.Button(self.inter, label="Center", halign=button.CENTER_ALIGN, valign=button.BOTTOM_ALIGN, top=25, width=125)
+        center_button.show()
+        right_button = button.Button(self.inter, label="Right", halign=button.RIGHT_ALIGN, valign=button.CENTER_ALIGN, width=125)
+        right_button.show()
+        self.test_file = "test_vtk_ui_button_alignment.png"
 
 test_vtk_ui_button_alignment().test()
