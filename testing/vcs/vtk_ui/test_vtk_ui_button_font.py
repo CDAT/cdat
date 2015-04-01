020ab4d2e17ba13c3ebe79decf1ea67fdaa9eaa5
Sam Fries <fries2@llnl.gov>
2015-04-01 14:08:48 -0700

    Implemented font test

--- testing/vcs/vtk_ui/test_vtk_ui_button_font.py
+++ testing/vcs/vtk_ui/test_vtk_ui_button_font.py
@@ -1,13 +1,17 @@
 """
 Test button font/size
 """
-import vcs.vtk_ui
 
-
+import vcs, os
 from vtk_ui_test import vtk_ui_test
 
 class test_vtk_ui_button_font(vtk_ui_test):
     def do_test(self):
-        return
-
+        self.win.SetSize(100, 80)
+        fonts = ["Arial", "Courier", os.path.join(os.environ["HOME"], vcs.getdotdirectory()[0], "HelvMono.ttf")]
+        sizes = [8, 12, 16]
+        for ind, font in enumerate(fonts):
+            b = vcs.vtk_ui.Button(self.inter, label="Font Test", font=font, top=ind * 25, size=sizes[ind])
+            b.show()
+        self.test_file = "test_vtk_ui_button_font.png"
 test_vtk_ui_button_font().test()
