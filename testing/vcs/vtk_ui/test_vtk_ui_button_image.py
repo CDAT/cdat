df962d6b81c04939131ebbc866f348900d6249f6
Sam Fries <fries2@llnl.gov>
2015-04-01 14:21:37 -0700

    Implemented test_vtk_ui_button_image

--- testing/vcs/vtk_ui/test_vtk_ui_button_image.py
+++ testing/vcs/vtk_ui/test_vtk_ui_button_image.py
@@ -3,11 +3,16 @@ Test button image
 """
 import vcs.vtk_ui
 
-
 from vtk_ui_test import vtk_ui_test
+import os
 
 class test_vtk_ui_button_image(vtk_ui_test):
     def do_test(self):
-        return
+        self.win.SetSize(80, 80)
+        directory = os.path.dirname(__file__)
+        image = os.path.join(directory, "Pepper.png")
+        b = vcs.vtk_ui.Button(self.inter, image=image, left=5, top=5)
+        b.show()
+        self.test_file = "test_vtk_ui_button_image.png"
 
 test_vtk_ui_button_image().test()
