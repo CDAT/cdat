e795065b355d7921d3b8b4b934d2fd9d12e12b5f
Paul J. Durack <durack1@llnl.gov>
2015-04-13 10:32:09 -0700

    fix #1212 - Update spyder 2.3.2 to 2.3.4

--- CMake/cdat_modules/spyder_pkg.cmake
+++ CMake/cdat_modules/spyder_pkg.cmake
@@ -1,7 +1,7 @@
-set(SPYDER_VERSION 2.3.3)
+set(SPYDER_VERSION 2.3.4)
 set(SPYDER_URL ${LLNL_URL})
 set(SPYDER_GZ spyder-${SPYDER_VERSION}.zip)
 set(SPYDER_SOURCE ${SPYDER_URL}/${SPYDER_GZ})
-set(SPYDER_MD5 8442bbb0a74475ed3307d6e983ecd700)
+set(SPYDER_MD5 e751f19b0c872cdfad6b1e3a69250f20)
 
 add_cdat_package_dependent(spyder "" "" OFF "CDAT_BUILD_GUI" OFF)
