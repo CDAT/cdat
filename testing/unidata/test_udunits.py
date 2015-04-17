25905e4d0c2b56ed0b07e32e40396f5bc6a502b0
Charles Doutriaux <doutriaux1@llnl.gov>
2015-04-16 17:09:35 -0700

    Started to adapt test to add to unidata test suite

--- testing/unidata/test_udunits.py
+++ testing/unidata/test_udunits.py
@@ -1,4 +1,4 @@
-import unidata
+import unidata,numpy
 
 m=unidata.udunits(5,'m')
 
@@ -17,14 +17,24 @@ dC=unidata.udunits(33,'degC')
 dF=unidata.udunits(83,'degF')
 dK=unidata.udunits(300,'K')
 
-print m,'+',cm,'=',m+cm
+o = m+cm
+print m,'+',cm,'=',o
+assert(o.units==m.units)
+assert(numpy.allclose(o.value,5.07))
 
+
+o=cm+i
 print cm,'+',i,'=',cm+i
+assert(o.units==cm.units)
+assert(numpy.allclose(o.value,14.62))
+o=i+cm
 print i,'+',cm,'=',i+cm
+assert(o.units==i.units)
+assert(numpy.allclose(o.value,5.75590551181))
 
 m.units='km'
 print m
-
+assert(numpy.allclose(m.value,5E-3))
 try:
     m2=m+dK
 except:
@@ -52,17 +62,17 @@ print 'how to go from',dF.units,'to K',dF.how('K')
 ## print dF.available()
 
 kn=dF.known_units()
-for k in kn.keys():
+"""for k in kn.keys():
     print k,kn[k]
-
+"""
 kn=dF.known_units(bytype=1)
 
-for k in kn.keys():
+"""for k in kn.keys():
     print 'UNITS OF',k
     ln=kn[k]
     for l in ln:
         print '\t',l
-
+"""
 
 print m
 print m**2
