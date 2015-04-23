3d1a7384e593593eea34ebf8c86b1e8fb8c346bf
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-04-23 09:20:53 -0400

    Removed duplicated code

--- testing/vcs/test_plot_unstructured_via_boxfill.py
+++ testing/vcs/test_plot_unstructured_via_boxfill.py
@@ -13,10 +13,6 @@ x=vcs.init()
 x.drawlogooff()
 x.setbgoutputdimensions(1200,1091,units="pixels")
 
-import vcs
-import os,sys
-import cdms2
-
 pth = os.path.join(os.path.dirname(__file__),"..")
 sys.path.append(pth)
 import checkimage
@@ -29,16 +25,6 @@ x.drawlogooff()
 x.setbgoutputdimensions(1200,1091,units="pixels")
 
 x.plot(s,bg=1)
-
-fnm = "test_plot_unstructured_via_boxfill.png"
-src=sys.argv[1]
-
-x.png(fnm)
-
-print "fnm:",fnm
-print "src:",src
-ret = checkimage.check_result_image(fnm,src,checkimage.defaultThreshold)
-sys.exit(ret)
 
 fnm = "test_plot_unstructured_via_boxfill.png"
 src=sys.argv[1]
