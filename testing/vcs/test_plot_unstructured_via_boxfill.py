d7bf1c3c862ee7f74e38f845d3bf142829a91a00
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-04-23 09:33:34 -0400

    Removed some more duplicated code

--- testing/vcs/test_plot_unstructured_via_boxfill.py
+++ testing/vcs/test_plot_unstructured_via_boxfill.py
@@ -4,34 +4,17 @@ import cdms2
 
 pth = os.path.join(os.path.dirname(__file__),"..")
 sys.path.append(pth)
+
 import checkimage
 
-f=cdms2.open(os.path.join(vcs.prefix,"sample_data","sampleCurveGrid4.nc"))
-
-s=f("sample")
-x=vcs.init()
+f = cdms2.open(os.path.join(vcs.prefix,"sample_data","sampleCurveGrid4.nc"))
+s = f("sample")
+x = vcs.init()
 x.drawlogooff()
 x.setbgoutputdimensions(1200,1091,units="pixels")
-
-pth = os.path.join(os.path.dirname(__file__),"..")
-sys.path.append(pth)
-import checkimage
-
-f=cdms2.open(os.path.join(vcs.prefix,"sample_data","sampleCurveGrid4.nc"))
-
-s=f("sample")
-x=vcs.init()
-x.drawlogooff()
-x.setbgoutputdimensions(1200,1091,units="pixels")
-
 x.plot(s,bg=1)
-
 fnm = "test_plot_unstructured_via_boxfill.png"
-src=sys.argv[1]
-
+src = sys.argv[1]
 x.png(fnm)
-
-print "fnm:",fnm
-print "src:",src
 ret = checkimage.check_result_image(fnm,src,checkimage.defaultThreshold)
 sys.exit(ret)
