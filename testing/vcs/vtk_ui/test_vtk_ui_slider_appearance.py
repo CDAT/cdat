91e6a4586f6bd6f2bea4a043814db5cb1a988a73
Sam Fries <samuelbfries@gmail.com>
2015-05-06 09:39:58 -0700

    Updated docstring for slider tests

--- testing/vcs/vtk_ui/test_vtk_ui_slider_appearance.py
+++ testing/vcs/vtk_ui/test_vtk_ui_slider_appearance.py
@@ -5,10 +5,6 @@ import vcs.vtk_ui
 from vtk_ui_test import vtk_ui_test
 
 class test_vtk_ui_slider_appearance(vtk_ui_test):
-    def __init__(self):
-        self.failed = False
-        self.updated = False
-        super(test_vtk_ui_slider_appearance, self).__init__()
     def do_test(self):
         self.win.SetSize(100, 500)
 
