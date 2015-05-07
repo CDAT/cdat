531bff8c599d958d3e7c558ee02aa260b8eb7b1c
Sam Fries <samuelbfries@gmail.com>
2015-05-07 12:57:55 -0700

    Fixed a typo in the widget init test's name

--- testing/vcs/vtk_ui/test_vtk_ui_widget_init.py
+++ testing/vcs/vtk_ui/test_vtk_ui_widget_init.py
@@ -6,7 +6,7 @@ import vcs.vtk_ui
 import vtk
 from vtk_ui_test import vtk_ui_test
 
-class test_vtk_ui_button_widget_init(vtk_ui_test):
+class test_vtk_ui_widget_init(vtk_ui_test):
     def do_test(self):
         vw = vtk.vtkTextWidget()
         vr = vtk.vtkTextRepresentation()
@@ -19,4 +19,4 @@ class test_vtk_ui_button_widget_init(vtk_ui_test):
         self.passed = 0
 
 if __name__ == "__main__":
-    test_vtk_ui_button_widget_init().test()
+    test_vtk_ui_widget_init().test()
