3d159809aa86635642097d5347550ace3f9a628a
Sam Fries <samuelbfries@gmail.com>
2015-05-08 09:47:55 -0700

    Added proper testing for blank feature in textbox

--- testing/vcs/vtk_ui/test_vtk_ui_textbox_blank_text.py
+++ testing/vcs/vtk_ui/test_vtk_ui_textbox_blank_text.py
@@ -22,6 +22,19 @@ class test_vtk_ui_textbox_blank_text(vtk_ui_test):
         textbox.stop_editing()
         assert textbox.text == "", "Start/stop editing altered empty string"
 
+        textbox.text = "Hi"
+        textbox.on_editing_end = self.end
+        textbox.start_editing()
+        textbox.delete_character()
+        textbox.delete_character()
+        textbox.stop_editing()
+
+        if self.passed != 0:
+            print "Did not call editing end callback"
+            return
+
+    def end(self, textbox):
+        assert textbox.text == "", "Deleting all characters should send blank string to editing end"
         self.passed = 0
 
 if __name__ == "__main__":
