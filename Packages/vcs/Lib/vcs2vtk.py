722ca16f0b5b1e17e35738d68c85f9683f7a0512
David C. Lonie <david.lonie@kitware.com>
2015-05-27 12:03:10 -0400

    Update vcs2vtk.putMaskOnVTKGrid to reflect blanking changes in VTK.
    All blanking information is now stored by setting bits on the
    corresponding element in a field data array.

--- Packages/vcs/Lib/vcs2vtk.py
+++ Packages/vcs/Lib/vcs2vtk.py
@@ -36,8 +36,6 @@ def putMaskOnVTKGrid(data,grid,actorColor=None,cellData=True,deep=True):
   imsk =  numpy_to_vtk_wrapper(msk.astype(numpy.int).flat,deep=deep)
   mapper = None
   if msk is not numpy.ma.nomask and not numpy.allclose(msk,False):
-      msk =  numpy_to_vtk_wrapper(numpy.logical_not(msk).astype(numpy.uint8).flat,deep=deep)
-      nomsk = numpy_to_vtk_wrapper(numpy.ones(data.mask.shape,numpy.uint8).flat,deep=deep)
       if actorColor is not None:
           if grid.IsA("vtkStructuredGrid"):
             grid2 = vtk.vtkStructuredGrid()
@@ -49,8 +47,8 @@ def putMaskOnVTKGrid(data,grid,actorColor=None,cellData=True,deep=True):
           r,g,b = actorColor
           lut.SetNumberOfTableValues(2)
           if not cellData:
-              if grid2.IsA("vtkStructuredGrid"):
-                  grid2.SetPointVisibilityArray(nomsk)
+              grid2.GetPointData().RemoveArray(
+                    vtk.vtkDataSetAttributes.GhostArrayName())
               grid2.GetPointData().SetScalars(imsk)
               #grid2.SetCellVisibilityArray(imsk)
               #p2c = vtk.vtkPointDataToCellData()
@@ -60,8 +58,8 @@ def putMaskOnVTKGrid(data,grid,actorColor=None,cellData=True,deep=True):
               lut.SetTableValue(0,r/100.,g/100.,b/100.,1.)
               lut.SetTableValue(1,r/100.,g/100.,b/100.,1.)
           else:
-              if grid2.IsA("vtkStructuredGrid"):
-                  grid2.SetCellVisibilityArray(nomsk)
+              grid2.GetCellData().RemoveArray(
+                    vtk.vtkDataSetAttributes.GhostArrayName())
               grid2.GetCellData().SetScalars(imsk)
               geoFilter.SetInputData(grid2)
               lut.SetTableValue(0,r/100.,g/100.,b/100.,0.)
@@ -71,11 +69,24 @@ def putMaskOnVTKGrid(data,grid,actorColor=None,cellData=True,deep=True):
           mapper.SetInputData(geoFilter.GetOutput())
           mapper.SetLookupTable(lut)
           mapper.SetScalarRange(0,1)
-      if grid.IsA("vtkStructuredGrid"):
-        if not cellData:
-          grid.SetPointVisibilityArray(msk)
-        else:
-          grid.SetCellVisibilityArray(msk)
+
+      # The ghost array now stores information about hidden (blanked)
+      # points/cells. Setting an array entry to the bitwise value
+      # `vtkDataSetAttributes.HIDDEN(CELL|POINT)` will blank the cell/point.
+      flatMask = msk.flat
+      ghost = numpy.zeros(len(flatMask), dtype=numpy.uint8)
+      invalidMaskValue = vtk.vtkDataSetAttributes.HIDDENCELL if cellData else \
+                         vtk.vtkDataSetAttributes.HIDDENPOINT
+      for i, isInvalid in enumerate(flatMask):
+          if isInvalid:
+              ghost[i] = invalidMaskValue
+
+      ghost = numpy_to_vtk_wrapper(ghost, deep=deep)
+      ghost.SetName(vtk.vtkDataSetAttributes.GhostArrayName())
+      if cellData:
+          grid.GetCellData().AddArray(ghost)
+      else:
+          grid.GetPointData().AddArray(ghost)
   return mapper
 
 def genGridOnPoints(data1,gm,deep=True,grid=None,geo=None):
