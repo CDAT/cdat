7d9d37d990145811140f0a970ca5b86ab9a8536e
Charles Doutriaux <doutriaux1@llnl.gov>
2015-06-03 10:42:11 -0700

    removed vcs_legacy, really not used any longer

--- Packages/vcs_legacy/Test/test_kw_yrev.py
+++
@@ -1,9 +0,0 @@
-# Adapted for numpy/ma/cdms2 by convertcdms.py
-import vcs_legacy,cdms2 as cdms,sys,support,os
-bg=support.bg
-
-f=cdms.open(os.path.join(vcs.sample_data,'clt.nc'))
-s=f('clt',time=slice(0,2))
-x=vcs_legacy.init()
-x.plot(s,yrev=1,bg=bg)
-support.check_plot(x)
