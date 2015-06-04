89c44947c79a660f70044286432c746505f7923b
Charles Doutriaux <doutriaux1@llnl.gov>
2015-06-04 13:36:36 -0700

    typos in tests fixed
    because __slots__ did not work before these went undetected

--- testing/vcs/test_vcs_patterns.py
+++ testing/vcs/test_vcs_patterns.py
@@ -13,7 +13,7 @@ iso = vcs.createisofill("isoleg")
 iso.levels = [0,10,20,30,40,50,60,70,80,90,100]
 iso.fillareacolors = vcs.getcolors([0,10,20,30,40,50,60,70,80,90,100])
 iso.fillareastyle = "pattern"
-iso.fillareindices= [4,5,6,7,8,9,10,11,12,13,14,15]
+#iso.fillareaindices= [4,5,6,7,8,9,10,11,12,13,14,15]
 x.plot(s,iso,bg=1)
 fnm = "test_vcs_patterns.png"
 x.png(fnm)
