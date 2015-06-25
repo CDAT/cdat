e989528ba4468f4a435808f8f28818a9ebe2aece
Charles Doutriaux <doutriaux1@llnl.gov>
2015-06-25 14:12:03 -0700

    added missing packages that flake8 depends on

--- CMake/cdat_modules/pyflakes_pkg.cmake
+++ CMake/cdat_modules/pyflakes_pkg.cmake
@@ -1,13 +1,13 @@
-set(nm flake8)
+set(nm pyflakes)
 string(TOUPPER ${nm} uc_nm)
 
-set(${uc_nm}_MAJOR 2)
-set(${uc_nm}_MINOR 4)
+set(${uc_nm}_MAJOR 0)
+set(${uc_nm}_MINOR 8)
 set(${uc_nm}_PATCH 1)
 set(${uc_nm}_VERSION ${${uc_nm}_MAJOR}.${${uc_nm}_MINOR}.${${uc_nm}_PATCH})
-set(${uc_nm}_URL ${LLNL_URL}
+set(${uc_nm}_URL ${LLNL_URL})
 set(${uc_nm}_GZ ${nm}-${${uc_nm}_VERSION}.tar.gz)
-set(${uc_nm}_MD5 ed45d3db81a3b7c88bd63c6e37ca1d65)
+set(${uc_nm}_MD5 905fe91ad14b912807e8fdc2ac2e2c23 )
 
 set(${uc_nm}_VERSION ${${uc_nm}_MAJOR}.${${uc_nm}_MINOR}.${${uc_nm}_PATCH})
 set(${uc_nm}_SOURCE ${${uc_nm}_URL}/${${uc_nm}_GZ})
