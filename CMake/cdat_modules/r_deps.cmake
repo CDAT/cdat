5fb42b01451081e5d62d2eb4770741813ebfe99c
Charles Doutriaux <doutriaux1@llnl.gov>
2015-07-01 12:31:47 -0700

    new URL at LLNL for sources
    added aqua for mac
    added openmp for parallel builds
    disabled -j1 for install step

--- CMake/cdat_modules/r_deps.cmake
+++ CMake/cdat_modules/r_deps.cmake
@@ -1,1 +1,4 @@
 set(R_deps ${readline_pkg})
+if (CDAT_BUILD_PARALLEL)
+  list(APPEND ${mpi_pkg})
+endif()
--- CMake/cdat_modules/r_external.cmake
+++ CMake/cdat_modules/r_external.cmake
@@ -4,15 +4,24 @@ set(R_install "${cdat_EXTERNALS}")
 if (APPLE)
     message("[INFO] Building R without X support for MacOS")
     set(WITHX "no")
+    set(WITH_AQUA "yes")
 else ()
     set(WITHX "yes")
+    set(WITH_AQUA "no")
 endif()
+
+if (CDAT_BUILD_PARALLEL)
+  message([INFO] Enabling openmp for R)
+  set(R_OPENMP "--enable-openmp")
+else ()
+  message([INFO] Disabling openmp for R)
+  set(R_OPENMP "--disable-openmp")
+endif ()
 
 list(APPEND USR_ENVS
   "CPPFLAGS=-I${cdat_EXTERNALS}/include $ENV{CPPFLAGS}"
   "LDFLAGS=-L${cdat_EXTERNALS}/lib"
   )
-
 ExternalProject_Add(R
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
   SOURCE_DIR ${R_source}
@@ -22,8 +31,8 @@ ExternalProject_Add(R
   BUILD_IN_SOURCE 1
   PATCH_COMMAND ""
   DEPENDS ${R_deps}
-  CONFIGURE_COMMAND env ${USR_ENVS} <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> LIBnn=lib --without-jpeglib --disable-R-framework --enable-R-shlib --disable-openmp --without-cairo --without-ICU --without-libpng --without-system-xz --without-aqua --without-tcltk --with-x=${WITHX}
-  INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} -j1 install
+  CONFIGURE_COMMAND env ${USR_ENVS} <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> LIBnn=lib --without-jpeglib --disable-R-framework --enable-R-shlib ${R_OPENMP} --without-cairo --without-ICU --without-system-xz --with-aqua=${WITH_AQUA} --without-tcltk --with-x=${WITHX}
+  INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM}  install
   ${ep_log_options}
 )
 if(APPLE)
