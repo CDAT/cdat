debdff5bc9039dbe9eed723a8f0d019d2de4b154
Charles Doutriaux <doutriaux1@llnl.gov>
2015-08-13 16:40:12 -0700

    get cmor via git repo

--- CMake/cdat_modules/cmor_external.cmake
+++ CMake/cdat_modules/cmor_external.cmake
@@ -8,12 +8,15 @@ set(cmor_configure_args --with-netcdf=${netcdf_install} --with-udunits2=${udunit
 ExternalProject_Add(CMOR
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
   SOURCE_DIR ${cmor_source_dir}
-  URL ${CMOR_URL}/${CMOR_GZ}
-  URL_MD5 ${CMOR_MD5}
   BUILD_IN_SOURCE 1
+  ${GIT_CMD_STR_CMOR}
+  ${GIT_TAG}
   INSTALL_DIR ${cmor_install_dir}
   PATCH_COMMAND ""
   CONFIGURE_COMMAND sh ${cmor_source_dir}/configure ${cmor_configure_args}
   DEPENDS ${CMOR_deps}
   ${ep_log_options}
 )
+if (DEFINED GIT_CMD_STR_CMOR)
+  unset(GIT_CMD_STR_CMOR)
+endif()
