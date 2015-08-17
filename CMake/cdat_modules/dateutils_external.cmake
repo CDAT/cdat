d6cad0442a941699e75cd16f4dca798ef0b425da
Charles Doutriaux <doutriaux1@llnl.gov>
2015-08-17 11:19:30 -0700

    building dateutil w/o egg
    Should fix #1486
    previous commit shoud fix #1192

--- CMake/cdat_modules/dateutils_external.cmake
+++ CMake/cdat_modules/dateutils_external.cmake
@@ -2,5 +2,6 @@
 # create an external project to install MyProxyClient,
 # and configure and build it
 set(nm DATEUTILS)
-
+set(USER_INSTALL_OPTIONS --old-and-unmanageable)
 include(pipinstaller)
+unset(USER_INSTALL_OPTIONS)
