8f4888e2281c1f4a70f118a09f374b5c7bb5a4d4
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-09-18 23:40:55 -0400

    Fixing vector plots not working for mercator and robinson projection

--- Packages/vcs/Lib/vcsvtk/vectorpipeline.py
+++ Packages/vcs/Lib/vcsvtk/vectorpipeline.py
@@ -4,6 +4,7 @@ import vcs
 from vcs import vcs2vtk
 import vtk
 
+import math
 
 class VectorPipeline(Pipeline):
 
@@ -16,6 +17,7 @@ class VectorPipeline(Pipeline):
         """Overrides baseclass implementation."""
         # Preserve time and z axis for plotting these inof in rendertemplate
         geo = None  # to make flake8 happy
+        projection = vcs.elements["projection"][self._gm.projection]
         returned = {}
         taxis = data1.getTime()
         if data1.ndim > 2:
@@ -27,13 +29,43 @@ class VectorPipeline(Pipeline):
         data1 = self._context().trimData2D(data1)
         data2 = self._context().trimData2D(data2)
 
+        scale = 1.0
+
+        lat = data1.getLatitude()[:]
+        lon = data1.getLongitude()[:]
+
+        if projection is not None:
+            scale = (lat.max() - lat.min()) *  (lon.max() - lon.min())
+
         gridGenDict = vcs2vtk.genGridOnPoints(data1, self._gm, deep=False, grid=grid,
-                                              geo=transform)
+                                              geo=transform, skipReprojection=False,
+                                              data2=data2)
+
+
+        data1 = gridGenDict["data"]
+        data2 = gridGenDict["data2"]
+        geo = gridGenDict["geo"]
+
         for k in ['vtk_backend_grid', 'xm', 'xM', 'ym', 'yM', 'continents',
                   'wrap', 'geo']:
             exec("%s = gridGenDict['%s']" % (k, k))
         grid = gridGenDict['vtk_backend_grid']
         self._dataWrapModulo = gridGenDict['wrap']
+
+        if geo is not None:
+            newv = vtk.vtkDoubleArray()
+            newv.SetNumberOfComponents(3)
+            newv.InsertTupleValue(0, [lon.min(), lat.min(),  0])
+            newv.InsertTupleValue(1, [lon.max(), lat.max(),  0])
+
+            vcs2vtk.projectArray(newv, projection=projection)
+            dimMin = [0, 0, 0]
+            dimMax = [0, 0, 0]
+            newv.GetTupleValue(0, dimMin)
+            newv.GetTupleValue(1, dimMax)
+            scale = (dimMax[1] - dimMin[1]) * (dimMax[0] - dimMin[0])/scale
+        else:
+            scale = 1.0
 
         returned["vtk_backend_grid"] = grid
         returned["vtk_backend_geo"] = geo
@@ -68,6 +100,7 @@ class VectorPipeline(Pipeline):
 
         arrow = vtk.vtkGlyphSource2D()
         arrow.SetGlyphTypeToArrow()
+        arrow.SetOutputPointsPrecision(vtk.vtkAlgorithm.DOUBLE_PRECISION)
         arrow.FilledOff()
 
         glyphFilter = vtk.vtkGlyph2D()
@@ -81,7 +114,7 @@ class VectorPipeline(Pipeline):
 
         # Scale to vector magnitude:
         glyphFilter.SetScaleModeToScaleByVector()
-        glyphFilter.SetScaleFactor(2. * self._gm.scale)
+        glyphFilter.SetScaleFactor(math.sqrt(scale) * 2.0 * self._gm.scale)
 
         # These are some unfortunately named methods. It does *not* clamp the
         # scale range to [min, max], but rather remaps the range
@@ -90,7 +123,12 @@ class VectorPipeline(Pipeline):
         glyphFilter.SetRange(0.01, 1.0)
 
         mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(glyphFilter.GetOutputPort())
+        writer = vtk.vtkXMLPolyDataWriter()
+
+        glyphFilter.Update()
+        data = glyphFilter.GetOutput()
+
+        mapper.SetInputData(data)
         act = vtk.vtkActor()
         act.SetMapper(mapper)
 
@@ -100,13 +138,13 @@ class VectorPipeline(Pipeline):
 
         x1, x2, y1, y2 = vcs.utils.getworldcoordinates(self._gm, data1.getAxis(-1),
                                                        data1.getAxis(-2))
-
-        act = vcs2vtk.doWrap(act, [x1, x2, y1, y2], self._dataWrapModulo)
+        # act = vcs2vtk.doWrap(act, [x1, x2, y1, y2], self._dataWrapModulo)
         self._context().fitToViewport(act, [tmpl.data.x1, tmpl.data.x2,
                                             tmpl.data.y1, tmpl.data.y2],
-                                      [x1, x2, y1, y2],
+                                      wc=None,
                                       priority=tmpl.data.priority,
                                       create_renderer=True)
+
 
         returned.update(
             self._context().renderTemplate(tmpl, data1, self._gm, taxis, zaxis))
@@ -114,7 +152,6 @@ class VectorPipeline(Pipeline):
         if self._context().canvas._continents is None:
             continents = False
         if continents:
-            projection = vcs.elements["projection"][self._gm.projection]
             self._context().plotContinents(x1, x2, y1, y2, projection,
                                            self._dataWrapModulo, tmpl)
 
