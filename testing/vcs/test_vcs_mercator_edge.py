5b3a11718da6fdf1c56ad113e4c7dc45ba6068a8
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-09-21 13:54:18 -0400

    Updated test to use background mode and other testing params

--- testing/vcs/test_vcs_mercator_edge.py
+++ testing/vcs/test_vcs_mercator_edge.py
@@ -3,9 +3,12 @@ import os,sys
 f = cdms2.open(os.path.join(vcs.sample_data, "clt.nc"))
 s = f("clt")
 x = vcs.init()
+x.setantialiasing(0)
+x.drawlogooff()
+x.setbgoutputdimensions(1200,1091,units="pixels")
 iso = x.createisofill()
 iso.projection = "mercator"
-x.plot(s(latitude=(-90, 90)), iso)
+x.plot(s(latitude=(-90, 90)), iso, bg=1)
 
 # Load the image testing module:
 testingDir = os.path.join(os.path.dirname(__file__), "..")
