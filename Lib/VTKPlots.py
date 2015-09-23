9ab58945acf67891ee25009ad744b0e3434a7853
Charles Doutriaux <doutriaux1@llnl.gov>
2015-09-22 20:58:36 -0700

    flake8ed

--- Lib/VTKPlots.py
+++ Lib/VTKPlots.py
@@ -409,13 +409,13 @@ class VTKVCSBackend(object):
         # Gets user physical screen dimensions
         screenSize = self.renWin.GetScreenSize()
         try:
-          # following works on some machines but not all
-          # Creates the window to be 60% of user's screen's width
-          bgX = int(screenSize[0]*.6)
+            # following works on some machines but not all
+            # Creates the window to be 60% of user's screen's width
+            bgX = int(screenSize[0] * .6)
         except:
-          bgX=self.canvas.bgX
+            bgX = self.canvas.bgX
         # Respect user chosen aspect ratio
-        bgY = int(bgX/self.canvas.size)
+        bgY = int(bgX / self.canvas.size)
         # Sets renWin dimensions
         self.renWin.SetSize(bgX, bgY)
         self._lastSize = (bgX, bgY)
@@ -462,7 +462,8 @@ class VTKVCSBackend(object):
         tpl = vcs.elements["template"][template]
 
         if kargs.get("renderer", None) is None:
-            if (gtype in ["3d_scalar", "3d_dual_scalar", "3d_vector"]) and (self.renderer is not None):
+            if (gtype in ["3d_scalar", "3d_dual_scalar", "3d_vector"]) and (
+                    self.renderer is not None):
                 ren = self.renderer
         else:
             ren = kargs["renderer"]
