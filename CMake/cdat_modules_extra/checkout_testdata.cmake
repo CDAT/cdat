f1bb2223c22091c091108c72ce9c625df0eb7407
Charles Doutriaux <doutriaux1@llnl.gov>
2015-09-22 23:06:10 -0700

    I think I finally commented out enough...
    ##bot##cmake_xtra -DCDAT_CHECKOUT_BASELINE_MATCHING_BRANCH=OFF

--- CMake/cdat_modules_extra/checkout_testdata.cmake
+++ CMake/cdat_modules_extra/checkout_testdata.cmake
@@ -55,119 +55,119 @@ if(NOT EXISTS "${TESTDATA_DIR}")
   endif()
 endif()
 
-# 2) Is TESTDATA_DIR a clone of TESTDATA_URL?
-execute_process(COMMAND
-  "${GIT_EXECUTABLE}" config --get remote.origin.url
-  WORKING_DIRECTORY "${TESTDATA_DIR}"
-  RESULT_VARIABLE RESULT
-  ERROR_VARIABLE OUTPUT
-  OUTPUT_VARIABLE OUTPUT)
-
-if(NOT RESULT EQUAL 0)
-  message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
-          "Directory exists and is not a git repository. "
-          "Baseline images may be out of date.")
-  return()
-endif()
-
-string(STRIP "${OUTPUT}" OUTPUT)
-
-if(NOT "${TESTDATA_URL}" STREQUAL "${OUTPUT}")
-  message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
-          "Directory is a git clone of \"${OUTPUT}\", not \"${TESTDATA_URL}\". "
-          "Baseline images may be out of date.")
-  return()
-endif()
-
-# 3) Is the current testdata repo clean? Don't want to clobber any local mods.
-# Update the index first:
-execute_process(COMMAND
-  "${GIT_EXECUTABLE}" update-index -q --refresh
-  WORKING_DIRECTORY "${TESTDATA_DIR}"
-  RESULT_VARIABLE RESULT
-  ERROR_VARIABLE OUTPUT
-  OUTPUT_VARIABLE OUTPUT)
-
-if(NOT RESULT EQUAL 0)
-  message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
-          "Error updating current index with 'git update-index -q --refresh':\n."
-          "${OUTPUT}\n"
-          "Baseline images may be out of date.")
-  return()
-endif()
-
-# Now check if the index is dirty:
-execute_process(COMMAND
-  "${GIT_EXECUTABLE}" diff-index --name-only HEAD "--"
-  WORKING_DIRECTORY "${TESTDATA_DIR}"
-  RESULT_VARIABLE RESULT
-  ERROR_VARIABLE OUTPUT
-  OUTPUT_VARIABLE OUTPUT)
-
-if(NOT RESULT EQUAL 0)
-  message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
-          "Error checking current index with 'git diff-index --name-only HEAD --':\n."
-          "${OUTPUT}\n"
-          "Baseline images may be out of date.")
-  return()
-endif()
-
-string(STRIP "${OUTPUT}" OUTPUT)
-
-if(NOT "${OUTPUT}" STREQUAL "")
-  message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
-          "Current checkout is not clean. The following files have modifications:\n"
-          "${OUTPUT}\n"
-          "Baseline images may be out of date.")
-  return()
-endif()
-
-# 4) Get the current branch name of the source repo.
-execute_process(COMMAND
-  "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
-  WORKING_DIRECTORY "${SOURCE_DIR}"
-  RESULT_VARIABLE RESULT
-  ERROR_VARIABLE OUTPUT
-  OUTPUT_VARIABLE OUTPUT)
-
-if(NOT RESULT EQUAL 0)
-  message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
-          "Cannot determine current branch name of source directory. "
-          "Baseline images may be out of date.")
-  return()
-endif()
-
-string(STRIP "${OUTPUT}" BRANCH)
-
-# If BRANCH is "HEAD", we're not on a named branch. Just use master in that
-# case.
-if("${BRANCH}" STREQUAL "HEAD")
-  message("The current source directory at '${SOURCE_DIR}' is not on a named "
-          "branch. Using the 'master' branch of the testdata repo.")
-  set(BRANCH "master")
-endif()
-
-# 5) Update the remote branches available on the testdata repo.
-execute_process(COMMAND
-  "${GIT_EXECUTABLE}" fetch --update-shallow --depth=1
-  WORKING_DIRECTORY "${TESTDATA_DIR}"
-  RESULT_VARIABLE RESULT
-  ERROR_VARIABLE OUTPUT
-  OUTPUT_VARIABLE OUTPUT)
-
-if(NOT RESULT EQUAL 0)
-  message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
-          "Error updating remote branches with "
-          "'git fetch --update-shallow --depth=1':\n."
-          "${OUTPUT}\n"
-          "Baseline images may be out of date.")
-  return()
-endif()
-
 # bots merge master in and mess the following, always rechecking master
 # bots check out the correct branches
 # following keyword skips the branch checking
 if (CDAT_CHECKOUT_BASELINE_MATCHING_BRANCH)
+    # 2) Is TESTDATA_DIR a clone of TESTDATA_URL?
+    execute_process(COMMAND
+      "${GIT_EXECUTABLE}" config --get remote.origin.url
+      WORKING_DIRECTORY "${TESTDATA_DIR}"
+      RESULT_VARIABLE RESULT
+      ERROR_VARIABLE OUTPUT
+      OUTPUT_VARIABLE OUTPUT)
+
+    if(NOT RESULT EQUAL 0)
+      message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
+              "Directory exists and is not a git repository. "
+              "Baseline images may be out of date.")
+      return()
+    endif()
+
+    string(STRIP "${OUTPUT}" OUTPUT)
+
+    if(NOT "${TESTDATA_URL}" STREQUAL "${OUTPUT}")
+      message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
+              "Directory is a git clone of \"${OUTPUT}\", not \"${TESTDATA_URL}\". "
+              "Baseline images may be out of date.")
+      return()
+    endif()
+
+    # 3) Is the current testdata repo clean? Don't want to clobber any local mods.
+    # Update the index first:
+    execute_process(COMMAND
+      "${GIT_EXECUTABLE}" update-index -q --refresh
+      WORKING_DIRECTORY "${TESTDATA_DIR}"
+      RESULT_VARIABLE RESULT
+      ERROR_VARIABLE OUTPUT
+      OUTPUT_VARIABLE OUTPUT)
+
+    if(NOT RESULT EQUAL 0)
+      message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
+              "Error updating current index with 'git update-index -q --refresh':\n."
+              "${OUTPUT}\n"
+              "Baseline images may be out of date.")
+      return()
+    endif()
+
+    # Now check if the index is dirty:
+    execute_process(COMMAND
+      "${GIT_EXECUTABLE}" diff-index --name-only HEAD "--"
+      WORKING_DIRECTORY "${TESTDATA_DIR}"
+      RESULT_VARIABLE RESULT
+      ERROR_VARIABLE OUTPUT
+      OUTPUT_VARIABLE OUTPUT)
+
+    if(NOT RESULT EQUAL 0)
+      message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
+              "Error checking current index with 'git diff-index --name-only HEAD --':\n."
+              "${OUTPUT}\n"
+              "Baseline images may be out of date.")
+      return()
+    endif()
+
+    string(STRIP "${OUTPUT}" OUTPUT)
+
+    if(NOT "${OUTPUT}" STREQUAL "")
+      message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
+              "Current checkout is not clean. The following files have modifications:\n"
+              "${OUTPUT}\n"
+              "Baseline images may be out of date.")
+      return()
+    endif()
+
+    # 4) Get the current branch name of the source repo.
+    execute_process(COMMAND
+      "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
+      WORKING_DIRECTORY "${SOURCE_DIR}"
+      RESULT_VARIABLE RESULT
+      ERROR_VARIABLE OUTPUT
+      OUTPUT_VARIABLE OUTPUT)
+
+    if(NOT RESULT EQUAL 0)
+      message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
+              "Cannot determine current branch name of source directory. "
+              "Baseline images may be out of date.")
+      return()
+    endif()
+
+    string(STRIP "${OUTPUT}" BRANCH)
+
+    # If BRANCH is "HEAD", we're not on a named branch. Just use master in that
+    # case.
+    if("${BRANCH}" STREQUAL "HEAD")
+      message("The current source directory at '${SOURCE_DIR}' is not on a named "
+              "branch. Using the 'master' branch of the testdata repo.")
+      set(BRANCH "master")
+    endif()
+
+    # 5) Update the remote branches available on the testdata repo.
+    execute_process(COMMAND
+      "${GIT_EXECUTABLE}" fetch --update-shallow --depth=1
+      WORKING_DIRECTORY "${TESTDATA_DIR}"
+      RESULT_VARIABLE RESULT
+      ERROR_VARIABLE OUTPUT
+      OUTPUT_VARIABLE OUTPUT)
+
+    if(NOT RESULT EQUAL 0)
+      message("Cannot update uvcdat-testdata checkout at \"${TESTDATA_DIR}\". "
+              "Error updating remote branches with "
+              "'git fetch --update-shallow --depth=1':\n."
+              "${OUTPUT}\n"
+              "Baseline images may be out of date.")
+      return()
+    endif()
+
     message("[INFO] Trying to find matching branch on baseline repo")
     # 6) Check if the desired branch exists in TESTDATA_DIR's origin remote.
     execute_process(COMMAND
