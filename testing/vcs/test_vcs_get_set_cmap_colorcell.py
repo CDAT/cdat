802b04334246b5641fdf8587c82394964298b13e
Charles Doutriaux <doutriaux1@llnl.gov>
2015-09-29 09:46:50 -0700

    checks if obj colormap is None (undefined)
    fix #1570

--- testing/vcs/test_vcs_get_set_cmap_colorcell.py
+++ testing/vcs/test_vcs_get_set_cmap_colorcell.py
@@ -2,9 +2,19 @@ import vcs
 
 x=vcs.init()
 
+gm = x.createboxfill()
 cmap = x.createcolormap()
 
 rgb = cmap.getcolorcell(25)
+assert(rgb == [0,60,100])
+
+rgb = x.getcolorcell(25)
+assert(rgb == [0,60,100])
+
+rgb = x.getcolorcell(25,x)
+assert(rgb == [0,60,100])
+
+rgb = x.getcolorcell(25,gm)
 assert(rgb == [0,60,100])
 
 cmap.setcolorcell(25,56,23,29)
