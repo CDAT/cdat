6caef50d837e0d24d5b843feeab9b135c8b06424
Charles Doutriaux <doutriaux1@llnl.gov>
2015-10-27 09:04:17 -0700

    added function to load matplotlib colormaps
    removed duplicate code between vcs.colors and genutil.colors
    made color utilities function available from vcs.utils and vcs.colors

--- Packages/vcs/Lib/__init__.py
+++ Packages/vcs/Lib/__init__.py
@@ -29,6 +29,7 @@ prefix = cdat_info.get_prefix()
 sample_data = cdat_info.get_sampledata_path()
 cdat_info.pingPCMDIdb("cdat", "vcs")
 from utils import *  # noqa
+import colors
 import Canvas
 from vcshelp import *  # noqa
 from queries import *  # noqa
