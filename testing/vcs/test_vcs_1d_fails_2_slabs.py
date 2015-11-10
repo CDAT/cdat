b2682a6f26a4e05f081002dbe19c4d6d9307b6a8
Charles Doutriaux <doutriaux1@llnl.gov>
2015-11-09 16:22:41 -0800

    correct setting of failed

--- testing/vcs/test_vcs_1d_fails_2_slabs.py
+++ testing/vcs/test_vcs_1d_fails_2_slabs.py
@@ -1,10 +1,10 @@
 import vcs
 x = vcs.init()
 try:
-    x.xvsy([1,2,3,4],[1,2,3,4,5],bg=True)
-    failed = True
-except:
+    x.xvsy([1,2,3,4],[1,2,3,4,5],bg=False)
     failed = False
+except:
+    failed = True
 
 if failed is False:
     raise RuntimeError("plotting 1d with two slabs should have failed")
