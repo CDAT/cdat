af29cc237fd0a6ddc01ee7c139d9484bc9d125de
Charles Doutriaux <doutriaux1@llnl.gov>
2015-11-18 13:14:30 -0800

    fixed vcs pattern test

--- Packages/vcs/Lib/__init__.py
+++ Packages/vcs/Lib/__init__.py
@@ -235,7 +235,7 @@ canvaslist = []
 
 
 def init(gui=0, mode=1, pause_time=0, call_from_gui=0, size=None,
-         backend="vtk"):
+         backend="vtk", geometry=None):
     '''
  Function: init                 # Initialize, Construct a VCS Canvas Object
 
@@ -270,7 +270,8 @@ def init(gui=0, mode=1, pause_time=0, call_from_gui=0, size=None,
         pause_time=pause_time,
         call_from_gui=call_from_gui,
         size=size,
-        backend=backend)
+        backend=backend,
+        geometry=geometry)
     global canvaslist
     canvaslist.append(canvas)
     return canvas
