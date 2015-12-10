698d4fae7ccfbd208370121ef0bea5b6c9b6c51e
Charles Doutriaux <doutriaux1@llnl.gov>
2015-12-10 09:46:23 -0800

    Added a test for issue #1730
    will eventually fix #1730

---
+++ testing/vcs/test_vcs_meshfill_vertices_issue.py
@@ -0,0 +1,44 @@
+import numpy
+import vcs
+
+x=vcs.init()
+
+data_values = [ 25, 45, 55.]
+
+data_lon = [ 5., 10., 15.]
+data_lat = [ 5., 10., 15.]
+
+data_lon_vert = [
+        # Triangle (last one missing because traingle has only 3 vertices
+        [2.5,7.5,5.,1.e20],
+        # Square
+        [7.5,12.5,12.5,7.5],
+        # Diamond
+        [15.,17.5,15,12.5],
+        ]
+
+data_lat_vert = [
+        # triangle
+        [2.5,2.5,7.5,1.e20],
+        # square
+        [7.5,7.5,12.5,12.5],
+        # diamond
+        [12.5,15,17.5,15],
+        ]
+
+mesh = numpy.array([data_lat_vert,data_lon_vert])
+print "MESH SHAPE:",mesh.shape,mesh.dtype
+mesh = numpy.transpose(mesh,(1,0,2))
+print "MESH SHAPE:",mesh.shape
+mesh = numpy.ma.masked_greater(mesh,1.e19)
+
+print "Triangle lats:"
+print mesh[0][0]
+print "Triangle lons:"
+print mesh[0][1]
+m = x.createmeshfill()
+m.mesh = True
+
+x.plot(numpy.array(data_values,),mesh,m)
+x.png("mesh_miss")
+raw_input("press enter")
