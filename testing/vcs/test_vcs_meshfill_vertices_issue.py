05e0fd06e0fac2ca76979e496d80faebb0762e9e
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-12-11 23:17:05 -0500

    Fixed style issues

--- testing/vcs/test_vcs_meshfill_vertices_issue.py
+++ testing/vcs/test_vcs_meshfill_vertices_issue.py
@@ -10,7 +10,7 @@ x=vcs.init()
 
 x.setantialiasing(0)
 x.drawlogooff()
-x.setbgoutputdimensions(1200,1090,units="pixels")
+x.setbgoutputdimensions(1200, 1090, units="pixels")
 
 data_values = [ 25, 45, 55.]
 
@@ -18,13 +18,12 @@ data_lon = [ 5., 10., 15.]
 data_lat = [ 5., 10., 15.]
 
 data_lon_vert = [
-        # Triangle (last one missing because traingle has only 3 vertices
-        [2.5,7.5,5.,1.e20],
-        # Square
-        [7.5,12.5,12.5,7.5],
-        # Diamond
-        [15.,17.5,15,12.5],
-        ]
+    # Triangle (last one missing because traingle has only 3 vertices
+    [2.5,7.5,5.,1.e20],
+    # Square
+    [7.5,12.5,12.5,7.5],
+    # Diamond
+    [15.,17.5,15,12.5],]
 
 data_lat_vert = [
         # triangle
@@ -32,8 +31,7 @@ data_lat_vert = [
         # square
         [7.5,7.5,12.5,12.5],
         # diamond
-        [12.5,15,17.5,15],
-        ]
+        [12.5,15,17.5,15],]
 
 mesh = numpy.array([data_lat_vert,data_lon_vert])
 print "MESH SHAPE:",mesh.shape,mesh.dtype
@@ -54,5 +52,6 @@ m.mesh = True
 x.plot(numpy.array(data_values,),mesh,m,bg=True)
 x.png("test_vcs_meshfill_vertices_issue.png")
 src = sys.argv[1]
-ret = checkimage.check_result_image("test_vcs_meshfill_vertices_issue.png",src,checkimage.defaultThreshold)
+ret = checkimage.check_result_image("test_vcs_meshfill_vertices_issue.png",
+                                    src, checkimage.defaultThreshold)
 sys.exit(ret)
