04103c9c1b0e531a65ef61b69f7ea216955bb9e0
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-12-18 02:41:23 -0500

    Add CDAT_USE_SYSTEM_OPENSSL option for CMake build system

--- CMake/cdat_modules/openssl_pkg.cmake
+++ CMake/cdat_modules/openssl_pkg.cmake
@@ -1,20 +1,24 @@
-find_package (OpenSSL QUIET)
-if (OPENSSL_FOUND)
-  get_filename_component (OPENSSL_LIBRARY_DIR
-    "${OPENSSL_SSL_LIBRARY}" DIRECTORY)
-  message(STATUS "System OpenSSL found. "
-    "OpenSSL library directory: ${OPENSSL_LIBRARY_DIR}. "
-    "OpenSSL Version: ${OPENSSL_VERSION}")
-else (OPENSSL_FOUND)
+option(CDAT_USE_SYSTEM_OPENSSL "Use system OpenSSL, if found." ON)
+mark_as_advanced(CDAT_USE_SYSTEM_OPENSSL)
+if(CDAT_USE_SYSTEM_OPENSSL)
+  find_package(OpenSSL QUIET)
+  if(OPENSSL_FOUND)
+    get_filename_component(OPENSSL_LIBRARY_DIR
+      "${OPENSSL_SSL_LIBRARY}" DIRECTORY)
+    message(STATUS "System OpenSSL found. "
+      "OpenSSL library directory: ${OPENSSL_LIBRARY_DIR}. "
+      "OpenSSL Version: ${OPENSSL_VERSION}")
+  endif(OPENSSL_FOUND)
+endif(CDAT_USE_SYSTEM_OPENSSL)
 
+if(NOT CDAT_USE_SYSTEM_OPENSSL OR NOT OPENSSL_FOUND)
   set(OPENSSL_MAJOR_SRC 1)
   set(OPENSSL_MINOR_SRC 0)
   set(OPENSSL_PATCH_SRC 2e)
   set(OPENSSL_VERSION
     ${OPENSSL_MAJOR_SRC}.${OPENSSL_MINOR_SRC}.${OPENSSL_PATCH_SRC})
   
-  message(STATUS "System OpenSSL *NOT* found. "
-  "Compiling from source. Version: ${OPENSSL_VERSION}")
+  message(STATUS "Compiling OpenSSL from source. Version: ${OPENSSL_VERSION}")
 
   set(OPENSSL_URL "https://www.openssl.org/source")
   set(OPENSSL_GZ "openssl-${OPENSSL_VERSION}.tar.gz")
@@ -24,4 +28,4 @@ else (OPENSSL_FOUND)
   # We've reached here because we need OpenSSL.
   # Hence, defaulting to ON
   add_cdat_package(openssl "" "" ON)
-endif (OPENSSL_FOUND)
+endif(NOT CDAT_USE_SYSTEM_OPENSSL OR NOT OPENSSL_FOUND)
