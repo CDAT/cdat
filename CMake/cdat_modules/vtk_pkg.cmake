dc5bb634e56abd650926cd05ff5c828d915e43dd
Charles Doutriaux <doutriaux1@llnl.gov>
2016-01-08 07:57:38 -0800

    Testing against newer VTK proj4

--- CMake/cdat_modules/vtk_pkg.cmake
+++ CMake/cdat_modules/vtk_pkg.cmake
@@ -1,4 +1,4 @@
 set(VTK_SOURCE ${GIT_PROTOCOL}github.com/UV-CDAT/VTK.git )
 set(VTK_MD5)
-set(VTK_BRANCH uvcdat-2.4.0)
+set(VTK_BRANCH update_proj4_47_or_higher)
 add_cdat_package_dependent(VTK "" "" ON "CDAT_BUILD_GRAPHICS" OFF)
