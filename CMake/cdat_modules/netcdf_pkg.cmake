64bae4c17f97d5f117a35e37100e34b025441f5d
Paul J. Durack <durack1@llnl.gov>
2016-01-16 13:42:30 -0800

    Fix #1579 - rebase to current master

--- CMake/cdat_modules/netcdf_pkg.cmake
+++ CMake/cdat_modules/netcdf_pkg.cmake
@@ -1,9 +1,16 @@
 set(NC4_MAJOR_SRC 4)
 set(NC4_MINOR_SRC 4)
+<<<<<<< HEAD
 set(NC4_PATCH_SRC 0)
 set(NC4_URL ${LLNL_URL})
 set(NC4_GZ netcdf-c-${NC4_MAJOR_SRC}.${NC4_MINOR_SRC}.${NC4_PATCH_SRC}.tar.gz)
 set(NC4_MD5 4e3d3b05ab53d0ae6036a1a0fe1506e5)
+=======
+set(NC4_PATCH_SRC 0-rc5)
+set(NC4_URL ${LLNL_URL})
+set(NC4_GZ netcdf-c-${NC4_MAJOR_SRC}.${NC4_MINOR_SRC}.${NC4_PATCH_SRC}.tar.gz)
+set(NC4_MD5 a542254b7fc27258be9e8a11d74c97a0)
+>>>>>>> Update to latest 4.4.0-rc5 source, and update archive name
 
 set (nm NC4)
 string(TOUPPER ${nm} uc_nm)
