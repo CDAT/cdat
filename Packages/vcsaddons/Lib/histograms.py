ffe9cff7445b9d94f5c35021aec22f7d67b9b6d2
Sam Fries <fries2@llnl.gov>
2016-01-26 14:53:32 -0800

    Flake8'd

--- Packages/vcsaddons/Lib/histograms.py
+++ Packages/vcsaddons/Lib/histograms.py
@@ -1,31 +1,35 @@
 from core import VCSaddon
-import cdms2,MV2,vcs,vcsaddons
+import cdms2
+import MV2
+import vcs
+import vcsaddons
+
 
 class Ghg(VCSaddon):
-    def __init__(self,name=None,source='default',x=None,template = None):
-        self.g_name='Ghg'
-        self.g_type='histogram'
-        VCSaddon.__init__(self,name,source,x,template)
+
+    def __init__(self, name=None, source='default', x=None, template=None):
+        self.g_name = 'Ghg'
+        self.g_type = 'histogram'
+        VCSaddon.__init__(self, name, source, x, template)
         if source == 'default':
-            self.fillareastyles = ['solid',]
-            self.fillareaindices = [1,]
-            self.fillareacolors = [252,]
-            self.line = ['solid',]
-            self.linewidth=[1.0,]
-            self.linecolors=[241,]
+            self.fillareastyles = ['solid']
+            self.fillareaindices = [1]
+            self.fillareacolors = [252]
+            self.line = ['solid']
+            self.linewidth = [1.0]
+            self.linecolors = [241]
         else:
             gm = vcsaddons.gms[self.g_name][source]
-            self.fillareastyle= gm.fillareastyles
+            self.fillareastyle = gm.fillareastyles
             self.fillareaindices = gm.fillareaindices
             self.fillareacolors = gm.fillareacolors
             self.line = gm.line
             self.linewidth = gm.linewidth
             self.linecolors = gm.linecolors
-            
 
     def list(self):
         print '---------- Histogram (Ghg) member (attribute) listings ----------'
-        print 'Canvas Mode = ',self.x.mode
+        print 'Canvas Mode = ', self.x.mode
         VCSaddon.list(self)
         print 'fillareastyles = ', self.fillareastyles
         print 'fillareaindices = ', self.fillareaindices
@@ -33,26 +37,25 @@ class Ghg(VCSaddon):
         print 'line = ', self.line
         print 'linewidth = ', self.linewidth
         print 'linecolors = ', self.linecolors
-        
-    
-    def plot(self,data,template = None, bg=0, x=None):
+
+    def plot(self, data, template=None, bg=0, x=None):
         if x is None:
             x = self.x
         if template is None:
             template = self.template
-        elif isinstance(template,str):
+        elif isinstance(template, str):
             template = x.gettemplate(template)
         elif not vcs.istemplate(template):
             raise "Error did not know what to do with template: %s" % template
-        
-        if not isinstance(data,cdms2.tvariable.TransientVariable):
-            mode= cdms2.getAutoBounds()
+
+        if not isinstance(data, cdms2.tvariable.TransientVariable):
+            mode = cdms2.getAutoBounds()
             cdms2.setAutoBounds("on")
             data = MV2.array(data)
             data.getAxis(-1).getBounds()
             cdms2.setAutoBounds(mode)
 
-        while data.rank()>1:
+        while data.rank() > 1:
             data = data[0]
 
         # ok now we have a good x and a good data
@@ -61,26 +64,27 @@ class Ghg(VCSaddon):
         # create the primitive
         fill = x.createfillarea()
         line = x.createline()
-        fill.viewport = [template.data.x1,template.data.x2,template.data.y1,template.data.y2]
-        line.viewport = [template.data.x1,template.data.x2,template.data.y1,template.data.y2]
+        fill.viewport = [
+            template.data.x1, template.data.x2, template.data.y1, template.data.y2]
+        line.viewport = [
+            template.data.x1, template.data.x2, template.data.y1, template.data.y2]
         axb = data.getAxis(0).getBounds()
-        xmn,xmx = vcs.minmax(axb)
-        ymn,ymx = vcs.minmax(data)
-        
-        xmn,xmx,ymn,ymx = self.prep_plot(xmn,xmx,ymn,ymx)
-        
-        fill.worldcoordinate=[xmn,xmx,ymn,ymx]
-        line.worldcoordinate=[xmn,xmx,ymn,ymx]
-        
-        styles =[]
+        xmn, xmx = vcs.minmax(axb)
+        ymn, ymx = vcs.minmax(data)
+
+        xmn, xmx, ymn, ymx = self.prep_plot(xmn, xmx, ymn, ymx)
+
+        fill.worldcoordinate = [xmn, xmx, ymn, ymx]
+        line.worldcoordinate = [xmn, xmx, ymn, ymx]
+
+        styles = []
         cols = []
         indices = []
         lt = []
-        lw =[]
+        lw = []
         lc = []
         xs = []
         ys = []
-        
 
         for i in range(nbars):
             if i < len(self.fillareastyles):
@@ -96,21 +100,20 @@ class Ghg(VCSaddon):
             else:
                 indices.append(self.fillareaindices[-1])
             if i < len(self.line):
-                lt.append( self.line[i])
+                lt.append(self.line[i])
             else:
                 lt.append(self.line[-1])
             if i < len(self.linewidth):
-                lw.append( self.linewidth[i])
+                lw.append(self.linewidth[i])
             else:
                 lw.append(self.linewidth[-1])
             if i < len(self.line):
-                lc.append( self.linecolors[i])
+                lc.append(self.linecolors[i])
             else:
                 lc.append(self.linecolors[-1])
-            
-            xs.append( [axb[i][0],axb[i][1],axb[i][1],axb[i][0],axb[i][0]])
-            ys.append( [0,0,data[i],data[i],0])
 
+            xs.append([axb[i][0], axb[i][1], axb[i][1], axb[i][0], axb[i][0]])
+            ys.append([0, 0, data[i], data[i], 0])
 
         fill.style = styles
         fill.x = xs
@@ -123,13 +126,13 @@ class Ghg(VCSaddon):
         line.type = lt
         line.width = lw
         line.color = lc
-
+        fill.list()
         displays = []
-        displays.append(x.plot(fill,bg=bg))
-        displays.append(x.plot(line,bg=bg))
+        displays.append(x.plot(fill, bg=bg))
+        displays.append(x.plot(line, bg=bg))
 
-        x.worldcoordinate = fill.worldcoordinate 
-        dsp = template.plot(data,self,bg=bg)
+        x.worldcoordinate = fill.worldcoordinate
+        dsp = template.plot(data, self, bg=bg)
         for d in dsp:
             displays.append(d)
 
