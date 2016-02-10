8829b147a87128d8441b3327c1228a0a395f24e4
Charles Doutriaux <doutriaux1@llnl.gov>
2016-02-09 17:14:41 -0800

    got all gms to work I think

--- testing/vcs/test_vcs_read_old_scr.py
+++ testing/vcs/test_vcs_read_old_scr.py
@@ -3,44 +3,55 @@ import sys
 
 x=vcs.init()
 
-boxes = x.listelements("boxfill")
-isofs = x.listelements("isofill")
-
+Ns = {}
+for k in vcs.elements.keys():
+    Ns[k] = len(vcs.elements[k].keys())
 x.scriptrun(sys.argv[1])
-
-boxes2 = x.listelements("boxfill")
-isofs2 = x.listelements("isofill")
-
-tested = [(boxes,boxes2),(isofs,isofs2)]
-tested = [(isofs,isofs2)]
-for gm,gm2 in [(boxes,boxes2),(isofs,isofs2)]:
-    new = []
-    for e in gm2:
-        if not e in gm:
-            new.append(e)
-
-#    print "new ",new
-
+Ns2 = {}
+for k in vcs.elements.keys():
+    Ns2[k] = len(vcs.elements[k].keys())
+diffs = {'projection': 0, 'colormap': 53, 'isofill': 187, 'marker': 0, '3d_dual_scalar': 0, 'texttable': 4, '3d_scalar': 0, 'fillarea': 234, 'font': 0, '3d_vector': 0, '1d': 9, 'template': 43, 'textcombined': 0, 'textorientation': 3, 'xvsy': 0, 'xyvsy': 0, 'isoline': 113, 'boxfill': 239, 'fontNumber': 0, 'line': 21, 'meshfill': 0, 'yxvsx': 9, 'taylordiagram': 0, 'list': 26, 'display': 0, 'vector': 55, 'scatter': 0}
+for k in vcs.elements.keys():
+    assert(diffs[k] == Ns2[k]-Ns[k])
 
 gm = x.getisofill("pr_time_lat_1")
+assert(gm.ymtics1=="lat5")
+assert(gm.ext_2 is True)
+assert(gm.fillareacolors ==  [240, 240, 240, 28, 27, 26, 25, 23, 22, 21, 20, 19, 18, 16])
 gm = x.getboxfill("lon_lat_mjop05")
-gm = x.getisoline("div_anom")
-gm = x.getvector("lon_lat_IO_p1")
-x.getline("red_solid").list()
+assert(gm.xmtics1=="lon5")
+assert(gm.yticlabels1=="lat20")
+assert(gm.datawc_x1 == 30)
+assert(gm.datawc_x2 == 210.)
+assert(gm.datawc_y1 == -30)
+assert(gm.datawc_y2 == 30.)
+assert(gm.level_1==-0.05)
+assert(gm.level_2==0.05)
+assert(gm.color_1==18)
+assert(gm.color_2==219)
+gm = x.getline("red_solid")
+assert(gm.type == ['solid'])
+assert(gm.color == [242])
+assert(gm.width == [2.0])
+
 gm = x.getyxvsx("pr_lsfit_lat")
-gm.list()
-"""
-n=linear,xticlabels#1=*,xticlabels#2=*,yticlabels#1=*,
-   yticlabels#2=*,
-   datawc(-3,-3,3,3),
-   missing=1e+20,
-   range
-   (id=1,level1=0,level2=0.001,Tf=AMIP30)
-   (id=2,level1=0.001,level2=0.002,Tf=AMIP29)
-   (id=3,level1=0.002,level2=0.003,Tf=AMIP27)
-   (id=4,level1=0.003,level2=0.004,Tf=AMIP25)
-   (id=5,level1=0.004,level2=0.005,Tf=AMIP23)
-   (id=6,level1=0.005,level2=0.006,Tf=AMIP21)
-   (id=7,level1=0.006,level2=0.007,Tf=AMIP19)
-   (id=8,level1=0.007,level2=0.008,Tf=AMIP17)  )
-"""
+assert(gm.xmtics1=="lat5")
+assert(gm.linecolor==242)
+assert(gm.linewidth==2.)
+assert(gm.datawc_x1 == 30)
+assert(gm.datawc_x2 == -30.)
+assert(gm.datawc_y1 == -5.)
+assert(gm.datawc_y2 == 5.)
+gm = x.getisoline("div_anom")
+assert(gm.xmtics1=="lon5")
+assert(gm.xticlabels1=="lon15")
+assert(gm.line == ['dash', 'dash', 'dash', 'dash', 'solid', 'dash', 'dash', 'dash', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid'])
+assert(gm.linecolors ==  [241, 241, 241, 241, 242, 241, 241, 241, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+assert(gm.linewidths ==  [1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+gm = x.getvector("lon_lat_IO_5")
+assert(gm.xmtics1=="lon5")
+assert(gm.xticlabels1=="lon20")
+assert(gm.linecolor==242)
+assert(gm.linewidth==2.)
+assert(gm.scale==3)
+assert(gm.reference==5)
