c99ba989488b739b3e414722828fa3a8f5ad9645
Paul J. Durack <durack1@llnl.gov>
2016-02-17 19:03:39 -0800

    Fix #1838 - Correct idna md5

--- CMake/cdat_modules/idna_pkg.cmake
+++ CMake/cdat_modules/idna_pkg.cmake
@@ -5,7 +5,6 @@ set(IDNA_PATCH_SRC 0)
 set(IDNA_VERSION ${IDNA_MAJOR_SRC}.${IDNA_MINOR_SRC})
 set(IDNA_GZ idna-${IDNA_VERSION}.tar.gz)
 set(IDNA_SOURCE ${LLNL_URL}/${IDNA_GZ})
-set(IDNA_MD5 
-025bb71b3f9d2fad15d0ee53e48dc873)
+set(IDNA_MD5 9ef51e6e51ea91b6c62426856c8a5b7c)
 
 add_cdat_package_dependent(IDNA "" "" OFF "CDAT_BUILD_LEAN" OFF)
