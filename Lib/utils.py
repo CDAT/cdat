b4076e0ba4af7818cdc64867bbcf78653c20a46c
Charles Doutriaux <doutriaux1@llnl.gov>
2016-02-18 09:28:39 -0800

    Update utils.py

--- Lib/utils.py
+++ Lib/utils.py
@@ -174,8 +174,7 @@ def process_src_element(code):
     i = code.find("(")
     nm = code[:i]
     code = code[i + 1:-1]
-    # try:
-    if 1:
+    try:
         if typ == "Gfb":
             boxfill.process_src(nm, code)
         elif typ == "Gfi":
@@ -210,8 +209,8 @@ def process_src_element(code):
             marker.process_src(nm, code)
         elif typ == "C":
             colormap.process_src(nm, code)
-    # except Exception as err:
-    #    print "Processing error for %s,%s: %s" % (nm, typ, err)
+    except Exception as err:
+        print "Processing error for %s,%s: %s" % (nm, typ, err)
 
 
 def listelements(typ=None):
