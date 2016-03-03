0240924de2bfaa22cbe8867edbf00d8677d52a0f
Sam Fries <fries2@llnl.gov>
2016-03-03 09:15:01 -0800

    Mintics now properly support lists

--- testing/vcs/test_vcs_mintics.py
+++ testing/vcs/test_vcs_mintics.py
@@ -13,9 +13,14 @@ box = x.createboxfill()
 # Should ignore the string here
 box.xmtics1 = {i:"Test" for i in range(-180, 180, 15) if i % 30 != 0}
 box.ymtics1 = {i:"Test" for i in range(-90, 90, 5) if i % 10 != 0}
+box.xmtics2 = "lon15"
+box.ymtics2 = "lat5"
 template = x.createtemplate()
 template.xmintic1.priority = 1
 template.ymintic1.priority = 1
+template.xmintic2.priority = 1
+template.xmintic2.y2 += template.xmintic1.y1 - template.xmintic1.y2
+template.ymintic2.priority = 1
 x.plot(s, template, box, bg=1)
 fnm = "test_vcs_mintics.png"
 x.png(fnm)
