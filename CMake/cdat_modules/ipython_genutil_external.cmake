9a6bc2cc4992ff2f5df8b0e6d916169cae5ec272
Paul J. Durack <durack1@llnl.gov>
2016-03-04 14:21:51 -0800

    Fix #1539 - Added ipython_genutils dependency

---
+++ CMake/cdat_modules/ipython_genutil_external.cmake
@@ -0,0 +1,14 @@
+set(Ipython_genutils_source "${CMAKE_CURRENT_BINARY_DIR}/build/Ipython_genutils")
+
+ExternalProject_Add(Ipython_genutils
+  DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
+  SOURCE_DIR ${Ipython_genutils_source}
+  URL ${IPYTHON_GENUTILS_URL}/${IPYTHON_GENUTILS_GZ}
+  URL_MD5 ${IPYTHON_GENUTILS_MD5}
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
+  INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install ${PYTHON_EXTRA_PREFIX}
+  DEPENDS ${Ipython_genutils_deps}
+  ${ep_log_options}
+)
---
+++ CMake/cdat_modules/ipython_genutils_deps.cmake
@@ -0,0 +1,1 @@
+set(Ipython_genutils_deps ${ipython_pkg} ${python_pkg} ${setuptools_pkg})
