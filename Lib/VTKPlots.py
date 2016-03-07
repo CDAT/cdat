bbd390890e5f54f4e945d62fed6db794cd8429ef
Dan Lipsa <dan.lipsa@kitware.com>
2016-03-07 15:25:48 -0500

    BUG: calling render() from ModifiedEvent causes GetSize() to return previous size.
    This causes test_vcs_configurator_resize to fail.

--- Lib/VTKPlots.py
+++ Lib/VTKPlots.py
@@ -269,7 +269,6 @@ class VTKVCSBackend(object):
         self.showGUI(render=False)
         if self.renWin.GetSize() != (0, 0):
             self.scaleLogo()
-        self.renWin.Render()
         if restart_anim:
             self.canvas.configurator.start_animating()
 
