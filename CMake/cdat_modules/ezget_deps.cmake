71b216a15beb575b3558a3f7562bcaa918abb51a
Charles Doutriaux <doutriaux1@llnl.gov>
2016-03-07 16:58:27 -0800

    got cd77 ezget libdrs to go under Linux (RH6/Ubunutu

--- CMake/cdat_modules/ezget_deps.cmake
+++ CMake/cdat_modules/ezget_deps.cmake
@@ -1,1 +1,1 @@
-set(ezget_deps ${netcdf_pkg} ${libdrs_pkg})
+set(ezget_deps ${netcdf_pkg} ${libdrs_pkg} ${libcdms_pkg})
--- CMake/cdat_modules/ezget_external.cmake
+++ CMake/cdat_modules/ezget_external.cmake
@@ -1,38 +1,48 @@
-set(libcdms_source "${CMAKE_CURRENT_BINARY_DIR}/build/libcdms")
-set(libcdms_install "${cdat_EXTERNALS}")
-set(CONFIGURE_ARGS --srcdir=${libcdms_source}^^--enable-dap=^^--enable-drs=no^^--enable-hdf=no^^--enable-pp=yes^^--enable-ql=no^^--cache-file=/dev/null^^--prefix=${libcdms_install}^^--with-nclib=${cdat_EXTERNALS}/lib^^--with-ncinc=${cdat_EXTERNALS}/include^^--with-daplib=/lib^^--with-dapinc=/include^^--with-hdfinc=./include^^--with-hdflib=./lib^^--with-hdf5lib=${cdat_EXTERNALS}/lib^^--with-grib2lib=${cdat_EXTERNALS}/lib^^--with-jasperlib=${cdat_EXTERNALS}/lib^^--with-grib2inc=${cdat_EXTERNALS}/include^^--enable-grib2)
-file(MAKE_DIRECTORY ${cdat_EXTERNALS}/man/man3)
+set(ezget_source "${CMAKE_CURRENT_BINARY_DIR}/build/ezget")
+set(ezget_install "${cdat_EXTERNALS}")
+if (APPLE)
+    set(ezget_make_file ezget_Makefile.Mac.gfortran.in)
+else ()
+    set(ezget_make_file ezget_Makefile.Linux.gfortran.in)
+endif ()
 
-if(DEFINED GIT_CMD_STR_LIBCDMS )
-    message("[INFO] [libcdms] Installing ${nm} from ${GIT_CMD_STR_LIBCDMS}")
+configure_file(
+    ${cdat_CMAKE_SOURCE_DIR}/cdat_modules_extra/${ezget_make_file}
+    ${CMAKE_CURRENT_BINARY_DIR}/CMake/ezget_Makefile
+    )
+
+if(DEFINED GIT_CMD_STR_EZGET )
+    message("[INFO] [ezget] Installing ${nm} from ${GIT_CMD_STR_EZGET}")
     include(GetGitRevisionDescription)
     set(URL_STR )
     set(URL_MD5_STR )
 else()
-    message("[INFO] [libcdms] Installed ${nm} from tarball ${LIBCDMS_GZ}")
-    set(URL_STR URL ${LIBCDMS_URL}/${LIBCDMS_GZ})
-    set(URL_MD5_STR URL_MD5 ${LIBCDMS_MD5})
-    set(GIT_CMD_STR_LIBCDMS )
+    message("[INFO] [ezget] Installed ${nm} from tarball ${EZGET_GZ}")
+    set(URL_STR URL ${EZGET_URL}/${EZGET_GZ})
+    set(URL_MD5_STR URL_MD5 ${EZGET_MD5})
+    set(GIT_CMD_STR_EZGET )
     set(GIT_TAG )
 endif()
-set(LIBCDMS_MAKE_ARGS -j1)
-set(LIBCDMS_BUILD_ARGS -fPIC)
-ExternalProject_Add(libcdms
+set(EZGET_MAKE_ARGS -f^^${CMAKE_CURRENT_BINARY_DIR}/CMake/ezget_Makefile)
+set(EZGET_MAKE_INSTALL_ARGS -f^^${CMAKE_CURRENT_BINARY_DIR}/CMake/ezget_Makefile^^install)
+set(EZGET_BUILD_ARGS -fPIC)
+
+ExternalProject_Add(ezget
   LIST_SEPARATOR ^^
   DOWNLOAD_DIR ${CDAT_PACKAGE_CACHE_DIR}
-  SOURCE_DIR ${libcdms_source}
-  INSTALL_DIR ${libcdms_install}
+  SOURCE_DIR ${ezget_source}
+  INSTALL_DIR ${ezget_install}
   ${URL_STR}
   ${URL_MD5_STR}
-  ${GIT_CMD_STR_LIBCDMS}
+  ${GIT_CMD_STR_EZGET}
   ${GIT_TAG}
-  PATCH_COMMAND ${CMAKE_COMMAND} -E remove <SOURCE_DIR>/zconf.h
   BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -DADDITIONAL_CFLAGS=${LIBCDMS_BUILD_ARGS}  -DCONFIGURE_ARGS=${CONFIGURE_ARGS} -DINSTALL_DIR=<INSTALL_DIR> -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_configure_step.cmake
-  BUILD_COMMAND ${CMAKE_COMMAND} -DADDITIONAL_CFLAGS=${LIBCDMS_BUILD_ARGS} -Dmake=$(MAKE) -DBUILD_ARGS=${LIBCDMS_MAKE_ARGS} -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_make_step.cmake
-  DEPENDS ${libcdms_deps}
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${CMAKE_COMMAND} -DADDITIONAL_CFLAGS=${EZGET_BUILD_ARGS} -Dmake=$(MAKE) -DBUILD_ARGS=${EZGET_MAKE_ARGS} -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_make_step.cmake
+  INSTALL_COMMAND ${CMAKE_COMMAND} -DADDITIONAL_CFLAGS=${EZGET_BUILD_ARGS} -Dmake=$(MAKE) -DBUILD_ARGS=${EZGET_MAKE_INSTALL_ARGS} -DWORKING_DIR=<SOURCE_DIR> -P ${cdat_CMAKE_BINARY_DIR}/cdat_make_step.cmake
+  DEPENDS ${ezget_deps}
   ${ep_log_options}
 )
-if (DEFINED GIT_CMD_STR_LIBCDMS)
-  unset(GIT_CMD_STR_LIBCDMS)
+if (DEFINED GIT_CMD_STR_EZGET)
+  unset(GIT_CMD_STR_EZGET)
 endif()
