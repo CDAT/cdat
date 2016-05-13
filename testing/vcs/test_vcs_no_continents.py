569843aec62da106d9a7452983f49c999adcf4aa
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2016-05-13 07:34:01 -0400

    Updated to current master

--- testing/vcs/test_vcs_no_continents.py
+++ testing/vcs/test_vcs_no_continents.py
@@ -1,7 +1,4 @@
-import cdms2
-import os
-import sys
-import vcs
+import os, sys, cdms2, vcs, testing.regression as regression
 
 # Load the clt data:
 dataFile = cdms2.open(os.path.join(vcs.sample_data, "clt.nc"))
@@ -10,10 +7,7 @@ clt = clt(latitude=(-90.0, 90.0), longitude=(-180., 175.), squeeze=1,
           time=('1979-1-1 0:0:0.0', '1988-12-1 0:0:0.0'))
 
 # Initialize canvas:
-canvas = vcs.init()
-canvas.setantialiasing(0)
-canvas.setbgoutputdimensions(1200,1091,units="pixels")
-canvas.drawlogooff()
+canvas = regression.init()
 
 t1 = vcs.createtemplate()
 t1.scale(.5, "y")
@@ -24,15 +18,4 @@ t2.move(.5, 'y')
 canvas.plot(clt, t1, continents=0, bg=True)
 canvas.plot(clt, t2, continents=1, bg=True)
 
-# Load the image testing module:
-testingDir = os.path.join(os.path.dirname(__file__), "..")
-sys.path.append(testingDir)
-import checkimage
-
-# Create the test image and compare:
-baseline = sys.argv[1]
-testFile = "test_vcs_no_continents.png"
-canvas.png(testFile)
-ret = checkimage.check_result_image(testFile, baseline,
-                                    checkimage.defaultThreshold)
-sys.exit(ret)
+regression.run(canvas, "test_vcs_no_continents.png")
