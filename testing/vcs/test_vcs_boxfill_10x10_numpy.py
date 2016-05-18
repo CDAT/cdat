314fc2bf70fb9c410ba7c1200a1fcaec9dfbfdd5
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2016-05-18 11:53:42 -0400

    Fixed numpy test

--- testing/vcs/test_vcs_boxfill_10x10_numpy.py
+++ testing/vcs/test_vcs_boxfill_10x10_numpy.py
@@ -3,5 +3,5 @@ import vcs, numpy, os, sys, testing.regression as regression
 s = numpy.sin(numpy.arange(100))
 s = numpy.reshape(s,(10,10))
 x = regression.init()
-x.plot(s)
+x.plot(s, bg=1)
 regression.run(x, "test_vcs_boxfill_10x10_numpy.png")
