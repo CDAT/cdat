761fc9b1b6db5282a10d4eaf57ecf1ad844fd7ac
Charles Doutriaux <doutriaux1@llnl.gov>
2016-06-07 16:05:07 -0700

    major clean up

--- exsrc/pbmplus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PACKAGE="pbmplus"
-. ./prolog.sh
-d=`uname`
-if ( test "Linux" = "${d}" ) then
-( INSTALLBINARIES=${prefix}/Externals/bin; export INSTALLBINARIES; INSTALLMANUALS=${prefix}/Externals/man/mann ; export INSTALLMANUALS ; CC="gcc -ansi" ; export CC;cd pbmplus; make install )
-else
-( INSTALLBINARIES=${prefix}/Externals/bin; export INSTALLBINARIES ; INSTALLMANUALS=${prefix}/Externals/man/mann ; export INSTALLMANUALS ; cd pbmplus; make install )
-fi
