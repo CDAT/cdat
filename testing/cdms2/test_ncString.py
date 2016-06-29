7bd76dd4a26e5da4a948c59d9342fd6d3431e794
Denis Nadeau <dnadeau4@users.noreply.github.com>
2016-06-29 11:29:58 -0700

    Fix ncString test DataDir

--- testing/cdms2/test_ncString.py
+++ testing/cdms2/test_ncString.py
@@ -1,13 +1,13 @@
 import cdms2
 import os
 import sys
-baseline = sys.argv[1]
+datadir = sys.argv[1]
 
 from markError import NTIME,NLAT,NLON,x,clearError,markError,reportError
 clearError()
 
 try:
-    f = cdms2.open(baseline+"/../data/prcp_1951.nc")
+    f = cdms2.open(datadir+"/prcp_1951.nc")
     f.institution
     f['prcp'].long_name
 except:
