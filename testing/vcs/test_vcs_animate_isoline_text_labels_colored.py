de5202da8b486d7e16b1356bf187cfc9f26712a9
Denis Nadeau <dnadeau4@users.noreply.github.com>
2016-08-04 13:27:36 -0700

    conda fix for cdms2 and vcs and numpy

--- testing/vcs/test_vcs_animate_isoline_text_labels_colored.py
+++ testing/vcs/test_vcs_animate_isoline_text_labels_colored.py
@@ -1,4 +1,6 @@
 import vcs, numpy, cdms2, MV2, os, sys, time, testing.regression as regression
+import pdb
+pdb.set_trace()
 
 pth = os.path.join(os.path.dirname(__file__),"..")
 sys.path.append(pth)
