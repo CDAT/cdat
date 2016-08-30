9ba9b455416269906d9b0f98a14b44ef885511e8
Charles Doutriaux <doutriaux1@llnl.gov>
2016-08-30 11:52:49 -0700

    visus is deprecated

--- Packages/visus/src/visusscene/VisusTimeLoader.cpp
+++
@@ -1,9 +0,0 @@
-#include "VisusTimeLoader.h"
-
-int (* VisusTimeLoader::callback) (const VisusTime&) = NULL;
-
-int VisusTimeLoader::loadTimeStep(const VisusTime& current_time,
-                                  std::vector<unsigned char*>& data)
-{
-  return 1;
-}
