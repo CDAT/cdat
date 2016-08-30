f9ec15a5ceacb5c1cf82978e1df5bc91c802ec45
Charles Doutriaux <doutriaux1@llnl.gov>
2016-08-30 11:28:38 -0700

    3 repos splitted: cdtime, cdms2, regrid2

--- Packages/cdms2/Lib/error.py
+++
@@ -1,5 +0,0 @@
-"Error object for cdms module"
-
-class CDMSError (Exception):
-    def __init__ (self, args="Unspecified error from package cdms"):
-        self.args = (args,)
--- Packages/cdms2/MV2.py
+++
@@ -1,1 +0,0 @@
-from cdms2.MV2 import *
