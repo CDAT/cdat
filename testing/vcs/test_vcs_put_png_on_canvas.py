aa9dc08c762117efc5902663275f043608bb2fcd
Charles Doutriaux <doutriaux1@llnl.gov>
2016-09-27 13:35:28 -0700

    added tests for put_png_on_canvas

--- testing/vcs/test_vcs_put_png_on_canvas.py
+++ testing/vcs/test_vcs_put_png_on_canvas.py
@@ -1,20 +1,25 @@
 import vcs
 import sys
 import argparse
+import vcs.testing.regression as regression
+import os
 
 p =argparse.ArgumentParser()
 
-p.add_argument("-H","--fitToHeight",default=True,type=bool)
+p.add_argument("-H","--fitToHeight",default=True,action="store_false")
 p.add_argument("-u","--units",default="percent")
 p.add_argument("-x","--xoffset",default=0,type=float)
 p.add_argument("-y","--yoffset",default=0,type=float)
-p.add_argument("-f","--file")
-p.add_argument("-z","--zoom",default=1.)
+p.add_argument("-z","--zoom",default=1.,type=float)
+p.add_argument("-s","--source",default="./somefile.png")
 
 args = p.parse_args(sys.argv[1:])
+print args
 bg=False
-x=vcs.init(bg=bg,geometry=(814,606))
+x=vcs.init(bg=bg,geometry=(1200,800))
 x.open()
-x.put_png_on_canvas(args.file,args.zoom,args.xoffset,args.yoffset,args.units,args.fitToHeight)
+png = os.path.join(sys.prefix,"share","uvcdat","sample_data","BlueMarble.ppm")
+x.put_png_on_canvas(png,args.zoom,args.xoffset,args.yoffset,args.units,args.fitToHeight)
 fnm = "test_vcs_put_png_on_canvas_%s_%s_%s_%s_%s" % (args.zoom,args.xoffset,args.yoffset,args.units,args.fitToHeight)
 x.png(fnm)
+ret = regression.check_result_image(fnm+'.png',args.source,20.)
