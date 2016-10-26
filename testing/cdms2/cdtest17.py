15077e56881d742b3222cc456cf3806e003e4472
Charles Doutriaux <doutriaux1@llnl.gov>
2016-10-26 15:33:18 -0700

    got it to look into cdms repo to add tests (#2133)
    * got it to look into cdms repo to add tests
    
    * removed obsolete files

--- testing/cdms2/cdtest17.py
+++
@@ -1,8 +0,0 @@
-import sys,cdms2,cdat_info
-print 'Test 17: Slicing and keeping dims ...',
-f=cdms2.open(cdat_info.get_sampledata_path()+'/clt.nc')
-s=f("clt")
-s0=s[0]
-assert s0.getAxis(0).isLatitude() == True
-assert s0.getAxis(1).isLongitude() == True
-print 'OK'
