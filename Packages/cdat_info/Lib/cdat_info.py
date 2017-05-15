50b156c4f333d3e138283c9ca5fce7214446ac20
Charles Doutriaux <doutriaux1@llnl.gov>
2017-05-15 15:43:18 -0700

    fix #2178

--- Packages/cdat_info/Lib/cdat_info.py
+++ Packages/cdat_info/Lib/cdat_info.py
@@ -1,6 +1,8 @@
 import subprocess
 import pwd
 import version
+import time
+import json
 Version = version.__describe__
 ping_checked = False
 check_in_progress = False
@@ -10,8 +12,43 @@ def version():
     sp = Version.split("-")
     vnm = "-".join(sp[:-2])
     vlist = vnm.split(".") + sp[-2:]
+    # sometime first digit has "v" or some letter in front of it
+    while len(vlist[0])>0 and not vlist[0][0].isdigit():
+        vlist[0]=vlist[0][1:]
     return vlist
 
+def versions_compare(version1,version2):
+    """ Compare two version in list format"""
+    if len(version1)==0:
+        if len(version2)>0:  # other version has more stuff hence greater
+            return -1
+        else:  # no diff
+            return 0
+
+    if len(version2)==0: # orthe rverswion ran out of patching, v1 greater
+        return 1
+
+    v1 = str(version1[0])
+    v2 = str(version2[0])
+
+    # Trim leading non alpha char
+    while len(v1)>0 and not v1.isdigit():
+        v1= v1[1:]
+    if v1=="":
+        v1 = version1[0]
+    while len(v2)>0 and not v2.isdigit():
+        v2= v2[1:]
+    if v2=="":
+        v2 = version2[0]
+
+    if v1 == v2:
+        return versions_compare(version1[1:],version2[1:])
+    elif v1>v2:
+        return 1
+    else:
+        return -1
+
+    
 
 def get_drs_dirs():
     return [os.path.join(sys.prefix, "lib")]
@@ -50,6 +87,7 @@ def get_sampledata_path():
 def runCheck():
     import cdat_info
     import os
+    # Wait for other threads to be done
     if cdat_info.ping_checked is False:
         check_in_progress = True
         val = None
@@ -69,17 +107,33 @@ def runCheck():
                 os.path.expanduser("~"),
                 ".uvcdat",
                 ".anonymouslog")
+            # last time and version we asked for anonymous
+            last_time_checked = None
+            last_version_check = None
             if os.path.exists(fanom):
-                f = open(fanom)
-                for l in f.readlines():
-                    sp = l.strip().split("UVCDAT_ANONYMOUS_LOG:")
-                    if len(sp) > 1:
-                        try:
-                            val = eval(sp[1])
-                        except:
-                            pass
-                f.close()
-
+                try:
+                    with open(fanom) as f:
+                        data = json.load(f)
+                        val = data.get("log_anonymously",None)
+                        last_time_checked = data.get("last_time_checked",None)
+                        last_version_check = data.get("last_version_check",None)
+                except:
+                    with open(fanom) as f:
+                        for l in f.readlines():
+                            sp = l.strip().split()
+                            anon = sp[0].split("UVCDAT_ANONYMOUS_LOG:")
+                            if sp[0].lower().find("cdat_anonymous")>-1 and len(sp)>1:
+                                try:
+                                    val = eval(sp[1])
+                                except:
+                                    pass
+        if last_time_checked is None or last_version_check is None:
+            val = None
+        elif time.time()-last_time_checked>2592000:  # Approximately 3 mnoths
+            val = None
+        current = version()
+        if last_version_check is not None and versions_compare(current,last_version_check)>0: # we have a newer version
+            val = None
         reload(cdat_info)
         return val
 
@@ -90,11 +144,12 @@ def askAnonymous(val):
     while cdat_info.ping_checked is False and not val in [
             True, False]:  # couldn't get a valid value from env or file
         val2 = raw_input(
-            "Allow anonymous logging usage to help improve UV-CDAT? (you can also set the environment variable UVCDAT_ANONYMOUS_LOG to yes or     no) [yes/no]")
-        if val2.lower() in ['y', 'yes', 'ok']:
+            "Allow anonymous logging usage to help improve UV-CDAT? (you can also set the environment variable UVCDAT_ANONYMOUS_LOG to yes or     no) [yes]/no")
+        if val2.lower() in ['y', 'yes', 'ok', '1', 'true', '']:
             val = True
-        elif val2.lower() in ['n', 'no', 'not']:
+        elif val2.lower() in ['n', 'no', 'not', '0', 'false']:
             val = False
+        print "VAL:",val
         if val in [True, False]:  # store result for next time
             try:
                 fanom = os.path.join(
@@ -105,11 +160,13 @@ def askAnonymous(val):
                         os.path.join(
                             os.path.expanduser("~"),
                             ".uvcdat"))
-                f = open(fanom, "w")
-                print >>f, "#Store information about allowing UVCDAT anonymous logging"
-                print >>f, "# Need sto be True or False"
-                print >>f, "UVCDAT_ANONYMOUS_LOG: %s" % val
-                f.close()
+                data= { "log_anonymously": val,
+                        "last_time_checked": time.time(),
+                        "last_version_check": version()
+                        }
+                print "DATA:",data
+                with open(fanom, "w") as f:
+                    json.dump(data,f,indent=2)
             except Exception as err:
                 pass
     else:
@@ -123,6 +180,7 @@ def askAnonymous(val):
 def pingPCMDIdb(*args, **kargs):
     import cdat_info
     import os
+    # Wait for other threads
     while cdat_info.check_in_progress:
         reload(cdat_info)
     val = cdat_info.runCheck()
