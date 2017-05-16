bc8d871e9d7619167ec132e7b53c8bf943051832
Charles Doutriaux <doutriaux1@llnl.gov>
2017-05-16 09:02:53 -0700

    fix #2178 (#2179)
    * fix #2178
    
    * version diff logic improved
    
    * falke8ed
    
    * correct interval comment

--- Packages/cdat_info/Lib/cdat_info.py
+++ Packages/cdat_info/Lib/cdat_info.py
@@ -1,6 +1,16 @@
-import subprocess
 import pwd
+import threading
 import version
+import time
+import json
+import urllib2
+import cdat_info
+import hashlib
+import urllib
+import warnings
+import os
+import sys
+import requests
 Version = version.__describe__
 ping_checked = False
 check_in_progress = False
@@ -10,7 +20,46 @@ def version():
     sp = Version.split("-")
     vnm = "-".join(sp[:-2])
     vlist = vnm.split(".") + sp[-2:]
+    # sometime first digit has "v" or some letter in front of it
+    while len(vlist[0]) > 0 and not vlist[0][0].isdigit():
+        vlist[0] = vlist[0][1:]
     return vlist
+
+
+def versions_compare(version1, version2):
+    """ Compare two version in list format"""
+    if len(version1) == 0:
+        if len(version2) > 0:  # other version has more stuff hence greater
+            return -1
+        else:  # no diff
+            return 0
+
+    if len(version2) == 0:  # orthe rverswion ran out of patching, v1 greater
+        return 1
+
+    v1 = str(version1[0])
+    v2 = str(version2[0])
+    # Trim leading non alpha char
+    while len(v1) > 0 and not v1.isdigit():
+        v1 = v1[1:]
+    if v1 == "":
+        v1 = version1[0]
+    while len(v2) > 0 and not v2.isdigit():
+        v2 = v2[1:]
+    if v2 == "":
+        v2 = version2[0]
+
+    if v1 == v2:
+        return versions_compare(version1[1:], version2[1:])
+    elif v1.isdigit() and v2.isdigit():
+        if int(v1) > int(v2):
+            return 1
+        else:
+            return -1
+    elif v1 > v2:
+        return 1
+    else:
+        return -1
 
 
 def get_drs_dirs():
@@ -33,12 +82,10 @@ def get_version():
 
 
 def get_prefix():
-    import sys
     return sys.prefix
 
 
 def get_sampledata_path():
-    import os
     try:
         return os.path.join(os.environ["UVCDAT_SETUP_PATH"],
                             "share", "uvcdat", "sample_data")
@@ -48,10 +95,9 @@ def get_sampledata_path():
 
 
 def runCheck():
-    import cdat_info
-    import os
+    # Wait for other threads to be done
     if cdat_info.ping_checked is False:
-        check_in_progress = True
+        cdat_info.check_in_progress = True
         val = None
         envanom = os.environ.get("UVCDAT_ANONYMOUS_LOG", None)
         if envanom is not None:
@@ -60,40 +106,56 @@ def runCheck():
             elif envanom.lower() in ['false', 'no', 'n', 'not']:
                 val = False
             else:
-                import warnings
                 warnings.warn(
-                    "UVCDAT logging environment variable UVCDAT_ANONYMOUS_LOG should be set to 'True' or 'False', you have it set to '%s', will be ignored" %
+                    "UVCDAT logging environment variable UVCDAT_ANONYMOUS_LOG should be set to 'True' or 'False'" +
+                    ", you have it set to '%s', will be ignored" %
                     envanom)
         if val is None:  # No env variable looking in .uvcdat
             fanom = os.path.join(
                 os.path.expanduser("~"),
                 ".uvcdat",
                 ".anonymouslog")
+            # last time and version we asked for anonymous
+            last_time_checked = None
+            last_version_check = None
             if os.path.exists(fanom):
-                f = open(fanom)
-                for l in f.readlines():
-                    sp = l.strip().split("UVCDAT_ANONYMOUS_LOG:")
-                    if len(sp) > 1:
-                        try:
-                            val = eval(sp[1])
-                        except:
-                            pass
-                f.close()
-
+                try:
+                    with open(fanom) as f:
+                        data = json.load(f)
+                        val = data.get("log_anonymously", None)
+                        last_time_checked = data.get("last_time_checked", None)
+                        last_version_check = data.get(
+                            "last_version_check", None)
+                except BaseException:
+                    with open(fanom) as f:
+                        for l in f.readlines():
+                            sp = l.strip().split()
+                            if sp[0].lower().find("cdat_anonymous") > - \
+                                    1 and len(sp) > 1:
+                                try:
+                                    val = eval(sp[1])
+                                except BaseException:
+                                    pass
+        if (last_time_checked is None or last_version_check is None) and val is False:
+            val = None
+        elif time.time() - last_time_checked > 2592000 and val is False:  # Approximately 1 month
+            val = None
+        current = version()
+        if val is False and last_version_check is not None and versions_compare(
+                current, last_version_check) > 0:  # we have a newer version
+            val = None
         reload(cdat_info)
         return val
 
 
 def askAnonymous(val):
-    import cdat_info
-    import os
-    while cdat_info.ping_checked is False and not val in [
-            True, False]:  # couldn't get a valid value from env or file
+    while cdat_info.ping_checked is False and val not in [True, False]:  # couldn't get a valid value from env or file
         val2 = raw_input(
-            "Allow anonymous logging usage to help improve UV-CDAT? (you can also set the environment variable UVCDAT_ANONYMOUS_LOG to yes or     no) [yes/no]")
-        if val2.lower() in ['y', 'yes', 'ok']:
+            "Allow anonymous logging usage to help improve UV-CDAT" +
+            "(you can also set the environment variable UVCDAT_ANONYMOUS_LOG to yes or no)? [yes]/no: ")
+        if val2.lower() in ['y', 'yes', 'ok', '1', 'true', '']:
             val = True
-        elif val2.lower() in ['n', 'no', 'not']:
+        elif val2.lower() in ['n', 'no', 'not', '0', 'false']:
             val = False
         if val in [True, False]:  # store result for next time
             try:
@@ -105,24 +167,24 @@ def askAnonymous(val):
                         os.path.join(
                             os.path.expanduser("~"),
                             ".uvcdat"))
-                f = open(fanom, "w")
-                print >>f, "#Store information about allowing UVCDAT anonymous logging"
-                print >>f, "# Need sto be True or False"
-                print >>f, "UVCDAT_ANONYMOUS_LOG: %s" % val
-                f.close()
-            except Exception as err:
+                data = {"log_anonymously": val,
+                        "last_time_checked": time.time(),
+                        "last_version_check": version()
+                        }
+                with open(fanom, "w") as f:
+                    json.dump(data, f, indent=2)
+            except BaseException:
                 pass
     else:
         if cdat_info.ping_checked:
             val = cdat_info.ping
     cdat_info.ping = val
     cdat_info.ping_checked = True
-    check_in_progress = False
+    cdat_info.check_in_progress = False
 
 
 def pingPCMDIdb(*args, **kargs):
-    import cdat_info
-    import os
+    # Wait for other threads
     while cdat_info.check_in_progress:
         reload(cdat_info)
     val = cdat_info.runCheck()
@@ -133,48 +195,40 @@ def pingPCMDIdb(*args, **kargs):
     try:
         if not cdat_info.ping:
             return
-    except:
+    except BaseException:
         pass
     cdat_info.askAnonymous(val)
-    import threading
     kargs['target'] = pingPCMDIdbThread
     kargs['args'] = args
     try:
         t = threading.Thread(**kargs)
         t.start()
-    except:
+    except BaseException:
         pass
 
 
 def pingPCMDIdbThread(*args, **kargs):
-    import threading
     kargs['target'] = submitPing
     kargs['args'] = args
     t = threading.Thread(**kargs)
     try:
         t.start()
-        import time
         time.sleep(5)  # Lets wait 5 seconds top for this ping to work
         if t.isAlive():
             try:
                 t._Thread__stop()
-            except:
+            except BaseException:
                 pass
-    except:
+    except BaseException:
         pass
+
 
 def submitPing(source, action, source_version=None):
     try:
-        import urllib2
-        import sys
-        import os
-        import cdat_info
-        import hashlib
-        import urllib
         if source in ['cdat', 'auto', None]:
             source = cdat_info.SOURCE
         if cdat_info.ping:
-            if not source in actions_sent.keys():
+            if source not in actions_sent.keys():
                 actions_sent[source] = []
             elif action in actions_sent[source]:
                 return
@@ -197,10 +251,11 @@ def submitPing(source, action, source_version=None):
             urllib2.urlopen(
                 'http://uv-cdat.llnl.gov/UVCDATUsage/log/add/',
                 urllib.urlencode(data))
-    except Exception as err:
+    except BaseException:
         pass
 
-def download_sample_data_files(files_md5,path=None):
+
+def download_sample_data_files(files_md5, path=None):
     """Downloads sample data from a list of files
     Default download directory is os.environ["UVCDAT_SETUP_PATH"]
     then data will be downloaded to that path.
@@ -218,26 +273,26 @@ def download_sample_data_files(files_md5,path=None):
         vcs.sample_data directory.
     :type path: `str`_ or `None`_
     """
-    import requests
-    import hashlib
     if not os.path.exists(files_md5) or os.path.isdir(files_md5):
-        raise RuntimeError("Invalid file type for list of files: %s" % files_md5)
+        raise RuntimeError(
+            "Invalid file type for list of files: %s" %
+            files_md5)
     if path is None:
         path = get_sampledata_path()
     samples = open(files_md5).readlines()
     download_url_root = samples[0].strip()
-    if len(download_url_root.split())>1:
+    if len(download_url_root.split()) > 1:
         # Old style
         download_url_root = "http://uvcdat.llnl.gov/cdat/sample_data/"
         n0 = 0
     else:
-        n0=1
+        n0 = 1
     for sample in samples[n0:]:
         good_md5, name = sample.split()
         local_filename = os.path.join(path, name)
         try:
             os.makedirs(os.path.dirname(local_filename))
-        except:
+        except BaseException:
             pass
         attempts = 0
         while attempts < 3:
@@ -263,8 +318,7 @@ def download_sample_data_files(files_md5,path=None):
             else:
                 attempts += 1
 
-import os
-import sys
+
 os.environ["UVCDAT_SETUP_PATH"] = sys.prefix
 CDMS_INCLUDE_DAP = 'yes'
 CDMS_DAP_DIR = '.'
@@ -282,8 +336,7 @@ cdunif_include_directories = [
     sys.prefix + '/include/cdms'] + [
         sys.prefix + '/include',
         sys.prefix + '/lib/libffi-3.1/include',
-        '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/usr/include',
-    '/usr/X11R6/include'] + []
+    '/usr/X11R6/include']
 cdunif_library_directories = [sys.prefix + '/lib'] + get_drs_dirs(
 ) + [sys.prefix + '/lib'] + ["/usr/X11/lib", "/usr/X11R6/lib"]
 cdunif_libraries = ['cdms', 'netcdf'] + ['netcdf'] + \
